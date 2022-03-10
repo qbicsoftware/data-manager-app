9ee97d6ffbc9b74c5640481e2264a272f914761d
Sven Fillinger <sven.fillinger@mailbox.org>
2022-03-10 13:35:27 +0100

    Showcase multi-module use case

---
+++ domain/src/main/java/life/qbic/datamanagement/Example.java
@@ -0,0 +1,21 @@
+package life.qbic.datamanagement;
+
+import java.util.Random;
+
+public class Example {
+
+  private static final int NUMBER_UPPER_BOUND = Integer.MAX_VALUE;
+
+  public static Example create() {
+    return new Example();
+  }
+
+  private Example() {
+  }
+
+  public int spitOutNumber() {
+    Random random = new Random();
+    return random.nextInt(NUMBER_UPPER_BOUND);
+  }
+
+}
