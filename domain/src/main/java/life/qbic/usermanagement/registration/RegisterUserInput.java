4b79c7142c67a1e69e555f6d3c465cb2591601ee
Sven Fillinger <sven.fillinger@mailbox.org>
2022-05-02 09:45:31 +0200

    Address change requests

--- domain/src/main/java/life/qbic/usermanagement/registration/RegisterUserInput.java
+++ domain/src/main/java/life/qbic/usermanagement/registration/RegisterUserInput.java
@@ -9,8 +9,19 @@ import life.qbic.usermanagement.User;
  */
 public interface RegisterUserInput {
 
+  /**
+   * Register a new user.
+   *
+   * @param user the new user to register
+   * @since 1.0.0
+   */
   void register(User user);
 
+  /**
+   * Set the output the use case shall call, when finished.
+   * @param output the output to call when the registration has been performed
+   * @since 1.0.0
+   */
   void setOutput(RegisterUserOutput output);
 
 }
--- domain/src/main/java/life/qbic/usermanagement/registration/Registration.java
+++ domain/src/main/java/life/qbic/usermanagement/registration/Registration.java
@@ -1,7 +1,6 @@
 package life.qbic.usermanagement.registration;
 
 import life.qbic.usermanagement.User;
-import life.qbic.usermanagement.repository.UserDataStorage;
 import life.qbic.usermanagement.repository.UserRepository;
 
 /**
@@ -12,7 +11,7 @@ import life.qbic.usermanagement.repository.UserRepository;
  * In case a user with the provided email already exists, the registration will fail and calls the
  * failure output method.
  *
- * @since 1.5.0
+ * @since 1.0.0
  */
 public class Registration implements RegisterUserInput {
 
@@ -20,14 +19,48 @@ public class Registration implements RegisterUserInput {
 
   private final UserRepository userRepository;
 
+  /**
+   * Creates the registration use case.
+   * <p>
+   * Upon construction, a dummy output interface is created, that needs to be overridden by
+   * explicitly setting it via {@link Registration#setRegisterUserOutput(RegisterUserOutput)}.
+   * <p>
+   * The default output implementation just prints to std out on success and std err on failure,
+   * after the use case has been exectuted via {@link Registration#register(User)}.
+   *
+   * @param userRepository the user repository to save the new user to.
+   * @since 1.0.0
+   */
   public Registration(UserRepository userRepository) {
     this.userRepository = userRepository;
+    // Init a dummy output, until one is set by the client.
+    this.registerUserOutput = new RegisterUserOutput() {
+      @Override
+      public void onSuccess() {
+        System.out.println("Called dummy register success output.");
+      }
+
+      @Override
+      public void onFailure(String reason) {
+        System.err.println("Called dummy register failure output.");
+      }
+    };
   }
 
+  /**
+   * Sets an overrides the use case output.
+   *
+   * @param registerUserOutput an output interface implementation, so the use case can trigger the
+   *                           callback methods after its execution
+   * @since 1.0.0
+   */
   public void setRegisterUserOutput(RegisterUserOutput registerUserOutput) {
     this.registerUserOutput = registerUserOutput;
   }
 
+  /**
+   * @inheritDocs
+   */
   @Override
   public void register(User user) {
     userRepository.findByEmail(user.getEmail())
@@ -40,6 +73,9 @@ public class Registration implements RegisterUserInput {
             });
   }
 
+  /**
+   * @inheritDocs
+   */
   @Override
   public void setOutput(RegisterUserOutput output) {
     registerUserOutput = output;
