e454f718912c51b006ee25e4d1140fd2aedd0751
Jennifer Boedker <jennifer.boedker@qbic.uni-tuebingen.de>
2022-05-03 15:32:15 +0200

    Style new login

--- webapp/src/main/java/life/qbic/views/login/NewLoginLayout.java
+++ webapp/src/main/java/life/qbic/views/login/NewLoginLayout.java
@@ -1,14 +1,21 @@
 package life.qbic.views.login;
 
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.BeforeEnterEvent;
-import com.vaadin.flow.router.BeforeEnterObserver;
-import com.vaadin.flow.router.Route;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.router.*;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
+import life.qbic.views.ErrorMessage;
 import life.qbic.views.MainLayout;
+import life.qbic.views.register.UserRegistrationLayout;
+
+import java.util.List;
 
 /**
  * <b><class short description - 1 Line!></b>
@@ -17,20 +24,56 @@ import life.qbic.views.MainLayout;
  *
  * @since <version tag>
  */
+@PageTitle("Login")
 @Route(value = "login", layout = MainLayout.class)
 @AnonymousAllowed
+@CssImport("./styles/views/login/login-view.css")
 public class NewLoginLayout extends VerticalLayout implements BeforeEnterObserver {
 
+
+  private VerticalLayout contentLayout;
   public LoginForm loginForm;
+  public Span registerSpan;
 
   public NewLoginLayout() {
-    this.setSizeFull();
+    this.addClassName("grid");
+
+    initLayout();
+    styleLayout();
+  }
+
+  private void initLayout() {
+    contentLayout = new VerticalLayout();
     this.loginForm = new LoginForm();
-    add(loginForm);
     loginForm.setAction("login");
-    add(new Button("Sign up"));
+
+    createSpan();
+
+    contentLayout.add(loginForm, registerSpan);
+
+    add(contentLayout);
+  }
+
+  private void styleLayout() {
+    styleFormLayout();
+
+    setSizeFull();
     setAlignItems(FlexComponent.Alignment.CENTER);
     setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+  }
+
+
+  private void styleFormLayout() {
+    registerSpan.addClassName("p-l");
+    contentLayout.setWidthFull();
+    contentLayout.setPadding(false);
+    contentLayout.getElement().getClassList().addAll(
+            List.of("bg-base", "border", "border-contrast-10", "rounded-m", "box-border", "flex", "flex-col", "w-full", "text-s", "shadow-l", "min-width-300px", "max-width-15vw"));
+  }
+
+  private void createSpan() {
+    RouterLink routerLink = new RouterLink("REGISTER", UserRegistrationLayout.class);
+    registerSpan = new Span(new Text("Need an account? "), routerLink);
   }
 
   @Override
