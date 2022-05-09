4bd8199e7583a153f99be91c077fd7e8c3a31d56
Jennifer Boedker <jennifer.boedker@qbic.uni-tuebingen.de>
2022-05-09 09:05:07 +0200

    Add tests to encryption policy

---
+++ domain/src/test/groovy/life/qbic/usermanagement/policies/PasswordEncryptionPolicySpec.groovy
@@ -0,0 +1,37 @@
+package life.qbic.usermanagement.policies
+
+import spock.lang.Specification
+
+/**
+ * <b>Tests for the {@link PasswordEncryptionPolicy}</b>
+ *
+ * @since 1.0.0
+ */
+class PasswordEncryptionPolicySpec extends Specification {
+
+    def "A password can be matched with the correct salt and iteration"() {
+        when:
+        String encryptedPassword = PasswordEncryptionPolicy.create().encrypt(password)
+        boolean result = PasswordEncryptionPolicy.create().doPasswordsMatch(password as char[],encryptedPassword)
+
+        then:
+        result
+
+        where:
+        password = "12345678"
+    }
+
+    def "The encrypted password contains the number of iterations, a salt and the hashed password"() {
+        when:
+        String encryptedPassword = PasswordEncryptionPolicy.create().encrypt(password)
+        String[] passwordElements = encryptedPassword.split(":")
+
+        then:
+        passwordElements[0].startsWith("4242") // number of iterations
+        passwordElements[1].length() == 40 //contains a salt with length of 20 bytes
+        passwordElements[2].length() == 40 // contains a hash with length of 20 bytes
+
+        where:
+        password = "abcdefghihdeo"
+    }
+}
