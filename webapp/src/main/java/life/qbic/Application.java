7d5b75757761da3a167be99df0f68c4fec5bca03
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2022-05-23 15:46:53 +0200

    When UserRegistered SendEmail

--- webapp/src/main/java/life/qbic/Application.java
+++ webapp/src/main/java/life/qbic/Application.java
@@ -5,19 +5,20 @@ import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import life.qbic.apps.datamanager.notifications.MessageBusInterface;
+import life.qbic.apps.datamanager.notifications.MessageSubscriber;
 import life.qbic.domain.usermanagement.DomainRegistry;
 import life.qbic.domain.usermanagement.UserDomainService;
 import life.qbic.domain.usermanagement.registration.UserRegistered;
 import life.qbic.domain.usermanagement.repository.UserRepository;
+import life.qbic.usermanagement.registration.RegistrationEmailSender;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
 
 /**
  * The entry point of the Spring Boot application.
@@ -33,6 +34,7 @@ import java.io.ObjectInputStream;
     offlineResources = {"images/logo.png"})
 @NpmPackage(value = "line-awesome", version = "1.3.0")
 public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
+
   public static void main(String[] args) {
     var appContext = SpringApplication.run(Application.class, args);
 
@@ -41,23 +43,22 @@ public class Application extends SpringBootServletInitializer implements AppShel
     DomainRegistry.instance().registerService(new UserDomainService(userRepository));
 
     // Testing: subscribe to user register events in the message bus
+    MessageSubscriber whenUserRegisteredSendEmail = (message, messageParams) -> {
+      if (!messageParams.messageType.equals("UserRegistered")) {
+        return;
+      }
+      try {
+        UserRegistered userRegistered = deserialize(message);
+        RegistrationEmailSender registrationEmailSender = appContext.getBean(
+            RegistrationEmailSender.class);
+        registrationEmailSender.sendmail(userRegistered.userEmail(), userRegistered.userFullName());
+      } catch (IOException | ClassNotFoundException e) {
+        throw new RuntimeException(e);
+      }
+    };
+
     var messageBus = appContext.getBean(MessageBusInterface.class);
-    messageBus.subscribe(
-        (message, messageParameters) -> {
-          System.out.println("Receiving new message:");
-          System.out.println(
-              messageParameters.messageType + " [" + messageParameters.occurredOn + "]");
-          try {
-            UserRegistered event = deserialize(message);
-            System.out.println(event.occurredOn());
-            System.out.println(event.userId());
-            System.out.println(event.userEmail());
-            System.out.println(event.userFullName());
-          } catch (Exception e) {
-            System.out.println(e.getMessage());
-          }
-        },
-        "UserRegistered");
+    messageBus.subscribe(whenUserRegisteredSendEmail, "UserRegistered");
   }
 
   static UserRegistered deserialize(String event) throws IOException, ClassNotFoundException {
