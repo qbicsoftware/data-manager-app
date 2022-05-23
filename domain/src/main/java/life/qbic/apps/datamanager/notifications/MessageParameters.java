594c4792a9fbb787588e2d210a2852c616442ba9
Sven Fillinger <sven1103@svens-mbp.am10.uni-tuebingen.de>
2022-05-23 14:15:06 +0200

    Add JavaDocs for MessageParameters

--- domain/src/main/java/life/qbic/apps/datamanager/notifications/MessageParameters.java
+++ domain/src/main/java/life/qbic/apps/datamanager/notifications/MessageParameters.java
@@ -3,31 +3,32 @@ package life.qbic.apps.datamanager.notifications;
 import java.time.Instant;
 
 /**
- * <b><class short description - 1 Line!></b>
+ * <b>Message Parameters</b>
+ * <p>
+ * Provides quick access to important message properties, such as the message type, its id and when it the event
+ * it reports has happened.
  *
- * <p><More detailed description - When to use, what it solves, etc.></p>
- *
- * @since <version tag>
+ * @since 1.0.0
  */
 public final class MessageParameters {
 
-  public final String messageType;
+    public final String messageType;
 
-  public final String messageId;
+    public final String messageId;
 
-  public final String occuredOn;
+    public final String occuredOn;
 
-  private MessageParameters(String messageType, String messageId, String occuredOn) {
-    super();
-    this.messageId = messageId;
-    this.messageType = messageType;
-    this.occuredOn = occuredOn;
-  }
+    private MessageParameters(String messageType, String messageId, String occuredOn) {
+        super();
+        this.messageId = messageId;
+        this.messageType = messageType;
+        this.occuredOn = occuredOn;
+    }
 
-  public static MessageParameters durableTextParameters(String messageType, String messageId,
-      Instant occuredOn) {
-    String occuredOnString = occuredOn.toString();
-    return new MessageParameters(messageType, messageId, occuredOnString);
-  }
+    public static MessageParameters durableTextParameters(String messageType, String messageId,
+                                                          Instant occuredOn) {
+        String occuredOnString = occuredOn.toString();
+        return new MessageParameters(messageType, messageId, occuredOnString);
+    }
 
 }
