0de70dbfb4bb6e60dd8b8d3a7aa4265e8e5e89e9
Sven Fillinger <sven1103@svens-mbp.am10.uni-tuebingen.de>
2022-05-23 14:10:01 +0200

    Add JavaDocs

--- domain/src/main/java/life/qbic/domain/events/DomainEventProducer.java
+++ domain/src/main/java/life/qbic/domain/events/DomainEventProducer.java
@@ -4,57 +4,58 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * <b><class short description - 1 Line!></b>
+ * <b>Domain Event Producer</b>
+ * <p>
+ * Local domain event producer class. Can be used to observe certain domain event types and publish domain events
+ * within the domain.
  *
- * <p><More detailed description - When to use, what it solves, etc.></p>
- *
- * @since <version tag>
+ * @since 1.0.0
  */
 public class DomainEventProducer {
 
-  private static final ThreadLocal<List> subscribers = new ThreadLocal<>();
+    private static final ThreadLocal<List> subscribers = new ThreadLocal<>();
 
-  private static final ThreadLocal<Boolean> publishing = ThreadLocal.withInitial(
-      () -> Boolean.FALSE);
+    private static final ThreadLocal<Boolean> publishing = ThreadLocal.withInitial(
+            () -> Boolean.FALSE);
 
-  public static DomainEventProducer instance(){
-    return new DomainEventProducer();
-  }
-
-  public DomainEventProducer() {
-    super();
-  }
-
-  public <T extends DomainEvent> void subscribe(DomainEventSubscriber<T> subscriber) {
-    if (publishing.get()) {
-      return;
-    }
-    List<DomainEventSubscriber<T>> registeredSubscribers = subscribers.get();
-
-    if (registeredSubscribers == null) {
-      registeredSubscribers = new ArrayList<>();
-      subscribers.set(registeredSubscribers);
+    public static DomainEventProducer instance() {
+        return new DomainEventProducer();
     }
 
-    registeredSubscribers.add(subscriber);
-  }
-
-  public <T extends DomainEvent> void publish(final T domainEvent) {
-    if (publishing.get()) {
-      return;
+    public DomainEventProducer() {
+        super();
     }
-    try {
-      publishing.set(Boolean.TRUE);
-      List<DomainEventSubscriber<T>> registeredSubscribers = subscribers.get();
-      Class<? extends DomainEvent> domainEventType = domainEvent.getClass();
-      for (DomainEventSubscriber<T> subscriber : registeredSubscribers) {
-        if (subscriber.subscribedToEventType() == domainEventType) {
-          subscriber.handleEvent(domainEvent);
+
+    public <T extends DomainEvent> void subscribe(DomainEventSubscriber<T> subscriber) {
+        if (publishing.get()) {
+            return;
         }
-      }
-    } finally {
-      publishing.set(Boolean.FALSE);
+        List<DomainEventSubscriber<T>> registeredSubscribers = subscribers.get();
+
+        if (registeredSubscribers == null) {
+            registeredSubscribers = new ArrayList<>();
+            subscribers.set(registeredSubscribers);
+        }
+
+        registeredSubscribers.add(subscriber);
     }
-  }
+
+    public <T extends DomainEvent> void publish(final T domainEvent) {
+        if (publishing.get()) {
+            return;
+        }
+        try {
+            publishing.set(Boolean.TRUE);
+            List<DomainEventSubscriber<T>> registeredSubscribers = subscribers.get();
+            Class<? extends DomainEvent> domainEventType = domainEvent.getClass();
+            for (DomainEventSubscriber<T> subscriber : registeredSubscribers) {
+                if (subscriber.subscribedToEventType() == domainEventType) {
+                    subscriber.handleEvent(domainEvent);
+                }
+            }
+        } finally {
+            publishing.set(Boolean.FALSE);
+        }
+    }
 
 }
