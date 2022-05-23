2854693d624cea16a9398e217145bc5de8af07e4
Sven Fillinger <sven1103@svens-mbp.am10.uni-tuebingen.de>
2022-05-23 09:31:09 +0200

    Add more JavaDocs

--- webapp/src/main/java/life/qbic/messaging/Exchange.java
+++ webapp/src/main/java/life/qbic/messaging/Exchange.java
@@ -4,93 +4,129 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
 import life.qbic.apps.datamanager.notifications.MessageBusInterface;
 import life.qbic.apps.datamanager.notifications.MessageParameters;
 import life.qbic.apps.datamanager.notifications.MessageSubscriber;
 
 /**
- * <b><class short description - 1 Line!></b>
- *
- * <p><More detailed description - When to use, what it solves, etc.></p>
+ * <b>Exchange (messaging)</b>
+ * <p>
+ * The Exchange class is a simple implementation of the {@link MessageBusInterface} and can be used to broadcast
+ * messages out of the data manager and user management context.
+ * <p>
+ * Note: use this class for development purposes only and replace it with an implementation that utilizes
+ * a production grade messaging middleware, such as for example RabbitMQ or Apache Kafka.
  *
  * @since <version tag>
  */
 public class Exchange implements MessageBusInterface {
 
-  List<Topic> topics;
+    List<Topic> topics;
 
-  private static Exchange instance;
+    private static Exchange instance;
 
-  public static Exchange instance() {
-    if (instance == null) {
-      instance = new Exchange();
-    }
-    return instance;
-  }
-
-  protected Exchange() {
-    super();
-    topics = new ArrayList<>();
-  }
-
-  @Override
-  public void submit(String message, MessageParameters messageParameters) {
-    this.topics.forEach(it -> it.informAllSubscribers(message, messageParameters));
-  }
-
-  synchronized public void subscribe(MessageSubscriber subscriber, String notificationType) {
-    Topic matchingTopic = null;
-    for (Topic availableTopic : topics) {
-      if (availableTopic.matchesTopic(notificationType)) {
-        matchingTopic = availableTopic;
-        break;
-      }
-    }
-    if (matchingTopic == null) {
-      matchingTopic = new Topic(notificationType);
-      topics.add(matchingTopic);
-    }
-    matchingTopic.addSubscriber(subscriber);
-
-  }
-
-  static class Topic {
-
-    private final String topic;
-
-    private final Set<MessageSubscriber> subscribers;
-
-    static Topic create(String topic) {
-      return new Topic(topic);
+    /**
+     * Queries the current instance of the Exchange class.
+     *
+     * @return 1.0.0
+     */
+    public static Exchange instance() {
+        if (instance == null) {
+            instance = new Exchange();
+        }
+        return instance;
     }
 
-    protected Topic(String topic) {
-      super();
-      this.topic = topic;
-      subscribers = new HashSet<>();
+    protected Exchange() {
+        super();
+        topics = new ArrayList<>();
     }
 
-    synchronized void addSubscriber(MessageSubscriber subscriber) {
-      subscribers.add(subscriber);
+    /**
+     * Submits a message to the exchange. The topic is taken from the {@link MessageParameters#messageType} parameter,
+     * and all subscriber to this topic are informed.
+     *
+     * @param message           the message to publish via the exchange instance
+     * @param messageParameters some message parameters, such as the type (aka topic), the occuredOn timepoint and
+     *                          a unique message identifier.
+     */
+    @Override
+    synchronized public void submit(String message, MessageParameters messageParameters) {
+        this.topics.forEach(it -> it.informAllSubscribers(message, messageParameters));
     }
 
-    synchronized void removeSubscriber(MessageSubscriber subscriber) {
-      subscribers.remove(subscriber);
+    /**
+     * Subscribe to a topic in order to get informed, whenever a message with this topic is published
+     * over this Exchange instance.
+     *
+     * @param subscriber the subscriber callback reference. A subscriber can only subscribe once to a topic. Multiple
+     *                   calls will not overwrite the subscriber.
+     * @param topic      the topic to subscribe to
+     */
+    @Override
+    synchronized public void subscribe(MessageSubscriber subscriber, String topic) {
+        Topic matchingTopic = null;
+        for (Topic availableTopic : topics) {
+            if (availableTopic.matchesTopic(topic)) {
+                matchingTopic = availableTopic;
+                break;
+            }
+        }
+        if (matchingTopic == null) {
+            matchingTopic = new Topic(topic);
+            topics.add(matchingTopic);
+        }
+        matchingTopic.addSubscriber(subscriber);
+
     }
 
-    boolean matchesTopic(String topic) {
-      return this.topic.equalsIgnoreCase(topic);
-    }
+    /**
+     * Small helper class to handle topics and their subscribers.
+     */
+    static class Topic {
 
-    synchronized void informAllSubscribers(String message, MessageParameters messageParameters) {
-      if (messageParameters.messageType.equalsIgnoreCase(topic)) {
-        informSubscribers(message, messageParameters);
-      }
-    }
+        private final String topic;
 
-    private void informSubscribers(String message, MessageParameters messageParameters) {
-      subscribers.forEach(it -> it.receive(message, messageParameters));
+        private final Set<MessageSubscriber> subscribers;
+
+        /**
+         * Creates a new topic.
+         *
+         * @param topic the new message topic that clients can subscribe to
+         * @return a topic
+         */
+        static Topic create(String topic) {
+            return new Topic(topic);
+        }
+
+        protected Topic(String topic) {
+            super();
+            this.topic = topic;
+            subscribers = new HashSet<>();
+        }
+
+        synchronized void addSubscriber(MessageSubscriber subscriber) {
+            subscribers.add(subscriber);
+        }
+
+        synchronized void removeSubscriber(MessageSubscriber subscriber) {
+            subscribers.remove(subscriber);
+        }
+
+        boolean matchesTopic(String topic) {
+            return this.topic.equalsIgnoreCase(topic);
+        }
+
+        synchronized void informAllSubscribers(String message, MessageParameters messageParameters) {
+            if (messageParameters.messageType.equalsIgnoreCase(topic)) {
+                informSubscribers(message, messageParameters);
+            }
+        }
+
+        private void informSubscribers(String message, MessageParameters messageParameters) {
+            subscribers.forEach(it -> it.receive(message, messageParameters));
+        }
     }
-  }
 
 }
