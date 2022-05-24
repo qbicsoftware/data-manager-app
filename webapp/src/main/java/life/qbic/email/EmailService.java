86f099889198001d13ae50b0454428f5871dcbb6
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2022-05-24 13:01:07 +0200

    Remove unnecessary annotation

--- webapp/src/main/java/life/qbic/email/EmailService.java
+++ webapp/src/main/java/life/qbic/email/EmailService.java
@@ -1,15 +1,5 @@
 package life.qbic.email;
 
-import org.springframework.stereotype.Service;
-
-/**
- * <b>short description</b>
- *
- * <p>detailed description</p>
- *
- * @since <version tag>
- */
-@Service
 public interface EmailService {
 
   void send(Email email);
