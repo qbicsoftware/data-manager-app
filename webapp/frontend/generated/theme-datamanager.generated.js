8f5529569ebf5e1950ee6d73eca2f4e521d22cca
Tobias Koch <KochTobi@users.noreply.github.com>
2022-05-24 16:47:17 +0200

    As a User i want to be notified by email after registration (#20)
    * Implement email sending
    
    * When UserRegistered SendEmail
    
    * Introduce a thread for the message exchange
    
    * Add factory method with capacity arg
    
    * Add README section
    
    Co-authored-by: Sven Fillinger <sven.fillinger@mailbox.org>

--- webapp/frontend/generated/theme-datamanager.generated.js
+++ webapp/frontend/generated/theme-datamanager.generated.js
@@ -1,54 +1,57 @@
 import 'construct-style-sheets-polyfill';
-import stylesCss from 'themes/datamanager/styles.css?inline';
-import {badge, color, spacing, typography, utility} from '@vaadin/vaadin-lumo-styles';
 
 const createLinkReferences = (css, target) => {
-    // Unresolved urls are written as '@import url(text);' to the css
-    // [0] is the full match
-    // [1] matches the media query
-    // [2] matches the url
-    const importMatcher = /(?:@media\s(.+?))?(?:\s{)?\@import\surl\((.+?)\);(?:})?/g;
-
-    var match;
-    var styleCss = css;
-
-    // For each external url import add a link reference
-    while ((match = importMatcher.exec(css)) !== null) {
-        styleCss = styleCss.replace(match[0], "");
-        const link = document.createElement('link');
-        link.rel = 'stylesheet';
-        link.href = match[2];
-        if (match[1]) {
-            link.media = match[1];
-        }
-        // For target document append to head else append to target
-        if (target === document) {
-            document.head.appendChild(link);
-        } else {
-            target.appendChild(link);
-        }
+  // Unresolved urls are written as '@import url(text);' to the css
+  // [0] is the full match
+  // [1] matches the media query
+  // [2] matches the url
+  const importMatcher = /(?:@media\s(.+?))?(?:\s{)?\@import\surl\((.+?)\);(?:})?/g;
+  
+  var match;
+  var styleCss = css;
+  
+  // For each external url import add a link reference
+  while((match = importMatcher.exec(css)) !== null) {
+    styleCss = styleCss.replace(match[0], "");
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = match[2];
+    if (match[1]) {
+      link.media = match[1];
     }
-    ;
-    return styleCss;
+    // For target document append to head else append to target
+    if (target === document) {
+      document.head.appendChild(link);
+    } else {
+      target.appendChild(link);
+    }
+  };
+  return styleCss;
 };
 
 // target: Document | ShadowRoot
 export const injectGlobalCss = (css, target, first) => {
-    if (target === document) {
-        const hash = getHash(css);
-        if (window.Vaadin.theme.injectedGlobalCss.indexOf(hash) !== -1) {
-            return;
-        }
-        window.Vaadin.theme.injectedGlobalCss.push(hash);
+  if(target === document) {
+    const hash = getHash(css);
+    if (window.Vaadin.theme.injectedGlobalCss.indexOf(hash) !== -1) {
+      return;
     }
-    const sheet = new CSSStyleSheet();
-    sheet.replaceSync(createLinkReferences(css, target));
-    if (first) {
-        target.adoptedStyleSheets = [sheet, ...target.adoptedStyleSheets];
-    } else {
-        target.adoptedStyleSheets = [...target.adoptedStyleSheets, sheet];
-    }
+    window.Vaadin.theme.injectedGlobalCss.push(hash);
+  }
+  const sheet = new CSSStyleSheet();
+  sheet.replaceSync(createLinkReferences(css,target));
+  if (first) {
+    target.adoptedStyleSheets = [sheet, ...target.adoptedStyleSheets];
+  } else {
+    target.adoptedStyleSheets = [...target.adoptedStyleSheets, sheet];
+  }
 };
+import stylesCss from 'themes/datamanager/styles.css?inline';
+import { typography } from '@vaadin/vaadin-lumo-styles';
+import { color } from '@vaadin/vaadin-lumo-styles';
+import { spacing } from '@vaadin/vaadin-lumo-styles';
+import { badge } from '@vaadin/vaadin-lumo-styles';
+import { utility } from '@vaadin/vaadin-lumo-styles';
 
 window.Vaadin = window.Vaadin || {};
 window.Vaadin.theme = window.Vaadin.theme || {};
@@ -63,16 +66,16 @@ window.Vaadin.theme.injectedGlobalCss = [];
  * @returns {string} 32 bit (as 8 byte hex string)
  */
 function hashFnv32a(str) {
-    /*jshint bitwise:false */
-    let i, l, hval = 0x811c9dc5;
+  /*jshint bitwise:false */
+  let i, l, hval = 0x811c9dc5;
 
-    for (i = 0, l = str.length; i < l; i++) {
-        hval ^= str.charCodeAt(i);
-        hval += (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
-    }
+  for (i = 0, l = str.length; i < l; i++) {
+    hval ^= str.charCodeAt(i);
+    hval += (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
+  }
 
-    // Convert to 8 digit hex string
-    return ("0000000" + (hval >>> 0).toString(16)).substr(-8);
+  // Convert to 8 digit hex string
+  return ("0000000" + (hval >>> 0).toString(16)).substr(-8);
 }
 
 /**
@@ -83,23 +86,22 @@ function hashFnv32a(str) {
  * @returns {string} 64 bit (as 16 byte hex string)
  */
 function getHash(input) {
-    let h1 = hashFnv32a(input); // returns 32 bit (as 8 byte hex string)
-    return h1 + hashFnv32a(h1 + input);
+  let h1 = hashFnv32a(input); // returns 32 bit (as 8 byte hex string)
+  return h1 + hashFnv32a(h1 + input); 
 }
-
 export const applyTheme = (target) => {
-
-    injectGlobalCss(stylesCss.toString(), target);
-
-
-    if (!document['_vaadintheme_datamanager_componentCss']) {
-
-        document['_vaadintheme_datamanager_componentCss'] = true;
-    }
-    injectGlobalCss(typography.cssText, target, true);
-    injectGlobalCss(color.cssText, target, true);
-    injectGlobalCss(spacing.cssText, target, true);
-    injectGlobalCss(badge.cssText, target, true);
-    injectGlobalCss(utility.cssText, target, true);
+  
+  injectGlobalCss(stylesCss.toString(), target);
+    
+  
+  if (!document['_vaadintheme_datamanager_componentCss']) {
+    
+    document['_vaadintheme_datamanager_componentCss'] = true;
+  }
+  injectGlobalCss(typography.cssText, target, true);
+injectGlobalCss(color.cssText, target, true);
+injectGlobalCss(spacing.cssText, target, true);
+injectGlobalCss(badge.cssText, target, true);
+injectGlobalCss(utility.cssText, target, true);
 
 }
--- webapp/src/main/java/life/qbic/messaging/Exchange.java
+++ webapp/src/main/java/life/qbic/messaging/Exchange.java
@@ -1,13 +1,16 @@
 package life.qbic.messaging;
 
-import life.qbic.apps.datamanager.notifications.MessageBusInterface;
-import life.qbic.apps.datamanager.notifications.MessageParameters;
-import life.qbic.apps.datamanager.notifications.MessageSubscriber;
-
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Queue;
 import java.util.Set;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+import life.qbic.apps.datamanager.notifications.MessageBusInterface;
+import life.qbic.apps.datamanager.notifications.MessageParameters;
+import life.qbic.apps.datamanager.notifications.MessageSubscriber;
 
 /**
  * <b>Exchange (messaging)</b>
@@ -18,13 +21,20 @@ import java.util.Set;
  * <p>Note: use this class for development purposes only and replace it with an implementation that
  * utilizes a production grade messaging middleware, such as for example RabbitMQ or Apache Kafka.
  *
- * @since <version tag>
+ * @since 1.0.0
  */
 public class Exchange implements MessageBusInterface {
 
-  List<Topic> topics;
+  private static final int DEFAULT_CAPACITY = 100;
+  private final Queue<SubmissionTask> submissionTasks;
+
+  private final List<Topic> topics;
 
   private static Exchange instance;
+
+  private final ReentrantLock lock;
+
+  private Thread worker;
 
   /**
    * Queries the current instance of the Exchange class.
@@ -38,22 +48,54 @@ public class Exchange implements MessageBusInterface {
     return instance;
   }
 
-  protected Exchange() {
-    super();
+  /**
+   * Queries an instance of the Exchange class. If none exists, one will be created with the
+   * provided capacity for the messages the exchange instance can hold in its queue.
+   * <p>
+   * Note: if the instance already exists prior to this call, then the capacity argument will be
+   * ignored.
+   *
+   * @param capacity the capacity of the queue size
+   * @return an exchange instance
+   * @since 1.0.0
+   */
+  public static Exchange instance(int capacity) {
+    if (instance == null) {
+      instance = new Exchange(capacity);
+    }
+    return instance;
+  }
+
+  protected Exchange(int capacity) {
     topics = new ArrayList<>();
+    submissionTasks = new ArrayBlockingQueue<>(capacity);
+    lock = new ReentrantLock();
+    launchSubmissionTaskWorker();
+  }
+
+  protected Exchange() {
+    this(DEFAULT_CAPACITY);
+  }
+
+  private void launchSubmissionTaskWorker() {
+    worker = new SubmissionTaskWorker(submissionTasks, topics, lock);
+    worker.setName("Message Submission Worker");
+    worker.start();
   }
 
   /**
-   * Submits a message to the exchange. The topic is taken from the {@link
-   * MessageParameters#messageType} parameter, and all subscriber to this topic are informed.
+   * Submits a message to the exchange. The topic is taken from the
+   * {@link MessageParameters#messageType} parameter, and all subscriber to this topic are
+   * informed.
    *
-   * @param message the message to publish via the exchange instance
+   * @param message           the message to publish via the exchange instance
    * @param messageParameters some message parameters, such as the type (aka topic), the occuredOn
-   *     timepoint and a unique message identifier.
+   *                          timepoint and a unique message identifier.
    */
   @Override
-  public synchronized void submit(String message, MessageParameters messageParameters) {
-    this.topics.forEach(it -> it.informAllSubscribers(message, messageParameters));
+  public void submit(String message, MessageParameters messageParameters) {
+    SubmissionTask newTask = new SubmissionTask(message, messageParameters);
+    submissionTasks.add(newTask);
   }
 
   /**
@@ -61,11 +103,34 @@ public class Exchange implements MessageBusInterface {
    * over this Exchange instance.
    *
    * @param subscriber the subscriber callback reference. A subscriber can only subscribe once to a
-   *     topic. Multiple calls will not overwrite the subscriber.
-   * @param topic the topic to subscribe to
+   *                   topic. Multiple calls will not overwrite the subscriber.
+   * @param topic      the topic to subscribe to
    */
   @Override
-  public synchronized void subscribe(MessageSubscriber subscriber, String topic) {
+  public void subscribe(MessageSubscriber subscriber, String topic) {
+    try {
+      // We try to acquire the lock for 1 second. If the lock
+      // cannot be acquired within 1 second, we throw an exception
+      // so the client can try to subscribe again.
+      if (lock.tryLock(1000, TimeUnit.MILLISECONDS)) {
+        try {
+          tryToSubscribe(subscriber, topic);
+        } finally {
+          lock.unlock(); // release the lock afterwards
+        }
+      } else {
+        throw new RuntimeException("Subscription failed");
+      }
+    } catch (InterruptedException e) {
+      stop();
+    }
+  }
+
+  private void stop() {
+    this.worker.interrupt();
+  }
+
+  private void tryToSubscribe(MessageSubscriber messageSubscriber, String topic) {
     Topic matchingTopic = null;
     for (Topic availableTopic : topics) {
       if (availableTopic.matchesTopic(topic)) {
@@ -77,10 +142,12 @@ public class Exchange implements MessageBusInterface {
       matchingTopic = new Topic(topic);
       topics.add(matchingTopic);
     }
-    matchingTopic.addSubscriber(subscriber);
+    matchingTopic.addSubscriber(messageSubscriber);
   }
 
-  /** Small helper class to handle topics and their subscribers. */
+  /**
+   * Small helper class to handle topics and their subscribers.
+   */
   static class Topic {
 
     private final String topic;
@@ -103,11 +170,11 @@ public class Exchange implements MessageBusInterface {
       subscribers = new HashSet<>();
     }
 
-    synchronized void addSubscriber(MessageSubscriber subscriber) {
+    void addSubscriber(MessageSubscriber subscriber) {
       subscribers.add(subscriber);
     }
 
-    synchronized void removeSubscriber(MessageSubscriber subscriber) {
+    void removeSubscriber(MessageSubscriber subscriber) {
       subscribers.remove(subscriber);
     }
 
@@ -115,7 +182,7 @@ public class Exchange implements MessageBusInterface {
       return this.topic.equalsIgnoreCase(topic);
     }
 
-    synchronized void informAllSubscribers(String message, MessageParameters messageParameters) {
+    void informAllSubscribers(String message, MessageParameters messageParameters) {
       if (messageParameters.messageType.equalsIgnoreCase(topic)) {
         informSubscribers(message, messageParameters);
       }
@@ -124,5 +191,80 @@ public class Exchange implements MessageBusInterface {
     private void informSubscribers(String message, MessageParameters messageParameters) {
       subscribers.forEach(it -> it.receive(message, messageParameters));
     }
+  }
+
+  static class SubmissionTask {
+
+    String message;
+
+    MessageParameters messageParameters;
+
+    SubmissionTask(String message, MessageParameters messageParameters) {
+      this.message = message;
+      this.messageParameters = messageParameters;
+    }
+
+  }
+
+  static class SubmissionTaskWorker extends Thread {
+
+    Queue<SubmissionTask> tasks;
+
+    List<Topic> topics;
+
+    ReentrantLock lock;
+
+    SubmissionTaskWorker(Queue<SubmissionTask> tasks, List<Topic> topics,
+        ReentrantLock lock) {
+      this.tasks = tasks;
+      this.lock = lock;
+      this.topics = topics;
+    }
+
+    private void submit(String message, MessageParameters messageParameters) {
+      this.topics.forEach(it -> it.informAllSubscribers(message, messageParameters));
+    }
+
+    @Override
+    public void run() {
+      while (true) {
+        if (Thread.currentThread().isInterrupted()) {
+          cleanup();
+          return;
+        }
+        SubmissionTask currentTask = tasks.poll();
+        if (currentTask != null) {
+          try {
+            handleSubmission(currentTask);
+          } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+          }
+        }
+        try {
+          Thread.sleep(100);
+        } catch (InterruptedException e) {
+          // no need to do anything atm, we don't save the state of the working queue.
+          // we interrupt the current thread and wait for the cleanup
+          Thread.currentThread().interrupt();
+        }
+      }
+    }
+
+    private void cleanup() {
+      // do potential cleanup work when the worker receives
+      // an interrupted signal
+    }
+
+    private void handleSubmission(SubmissionTask currentTask) throws InterruptedException {
+      while (!lock.tryLock()) {
+        Thread.sleep(100);
+      }
+      try {
+        submit(currentTask.message, currentTask.messageParameters);
+      } finally {
+        lock.unlock();
+      }
+    }
+
   }
 }
