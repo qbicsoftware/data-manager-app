35a13673f6be435eac64c7a011bd0e8ad053aa0d
Sven Fillinger <sven.fillinger@mailbox.org>
2022-06-03 10:09:56 +0200

    Finish API

---
+++ domain/src/main/java/life/qbic/apps/datamanager/ApplicationException.java
@@ -0,0 +1,22 @@
+package life.qbic.apps.datamanager;
+
+import java.util.List;
+
+/**
+ * <b><class short description - 1 Line!></b>
+ *
+ * <p><More detailed description - When to use, what it solves, etc.></p>
+ *
+ * @since <version tag>
+ */
+public abstract class ApplicationException extends RuntimeException {
+
+  protected ApplicationException() {
+    super();
+  }
+
+  protected ApplicationException(String message) {
+    super(message);
+  }
+
+}
---
+++ domain/src/main/java/life/qbic/apps/datamanager/ConcreteEception.java
@@ -0,0 +1,14 @@
+package life.qbic.apps.datamanager;
+
+/**
+ * <b><class short description - 1 Line!></b>
+ *
+ * <p><More detailed description - When to use, what it solves, etc.></p>
+ *
+ * @since <version tag>
+ */
+public class ConcreteEception extends ApplicationException {
+
+  private ConcreteEception(){}
+
+}
