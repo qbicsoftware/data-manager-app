a60ee8a2747dd2481845abcbed21a86d62f906a7
Steffengreiner <steffen.greiner@qbic.uni-tuebingen.de>
2022-06-10 16:21:48 +0200

    Refactor value parameter name back to address in UserRegistrationService
    Co-authored-by: Tobias Koch <KochTobi@users.noreply.github.com>

--- domain/src/main/java/life/qbic/apps/datamanager/services/UserRegistrationService.java
+++ domain/src/main/java/life/qbic/apps/datamanager/services/UserRegistrationService.java
@@ -52,7 +52,7 @@ public final class UserRegistrationService {
    * client's responsibility to handle the raw password.
    *
    * @param fullName    the full name of the user
-   * @param email       the email value of the user
+   * @param email       the email address of the user
    * @param rawPassword the raw password provided by the user
    * @return a registration response with information about if the registration was successful or
    * not.
