61446009ad309848139fcdfb20562223e15ba757
Sven Fillinger <sven.fillinger@mailbox.org>
2022-06-21 15:03:35 +0200

    Add email format method

--- webapp/src/main/java/life/qbic/usermanagement/registration/EmailFactory.java
+++ webapp/src/main/java/life/qbic/usermanagement/registration/EmailFactory.java
@@ -27,6 +27,23 @@ public class EmailFactory {
         The QBiC team""", fullName, confirmationLink);
   }
 
+  private static String formatPasswordResetEmailContent(String fullName, String passwordResetLink) {
+    return String.format("""
+        Dear %s,
+                
+        we have received a password reset request. If you did not issue the reset, 
+        please contact us asap at support@qbic.zendesk.com.
+        
+        Please click the link below to set your new password:
+        
+        %s
+                
+        Need help? Contact us for further questions at support@qbic.zendesk.com
+                
+        Best regards,\s
+        The QBiC team""", fullName, passwordResetLink);
+  }
+
   public static Email registrationEmail(String from, Recipient to, String confirmationLink) {
     String content = formatRegistrationEmailContent(to.fullName(), confirmationLink);
     String subject = "Activate your Data Manager Account";
