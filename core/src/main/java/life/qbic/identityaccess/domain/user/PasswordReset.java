82aaf0c5212c4c04d861ad220daf794b7674ecb1
Sven Fillinger <sven.fillinger@mailbox.org>
2022-06-21 10:12:22 +0200

    Introduce model

---
+++ core/src/main/java/life/qbic/identityaccess/domain/user/PasswordReset.java
@@ -0,0 +1,52 @@
+package life.qbic.identityaccess.domain.user;
+
+import java.time.Instant;
+import life.qbic.shared.domain.events.DomainEvent;
+
+/**
+ * <b>Password Reset Domain Event</b>
+ *
+ * <p>Indicates that a user wants to set a new password.</p>
+ *
+ * @since 1.0.0
+ */
+public class PasswordReset extends DomainEvent {
+
+  private final UserId userId;
+
+  private final Instant occurredOn;
+
+  /**
+   * Creates a new {@link PasswordReset} domain event.
+   * <p>
+   * The instant of the event will be the time-point of the event instance creation.
+   *
+   * @param userId the id of the user for whom the password needs to be reset
+   * @return a new instance of a password reset domain event
+   * @since 1.0.0
+   */
+  public static PasswordReset create(UserId userId) {
+    return new PasswordReset(userId, Instant.now());
+  }
+
+  private PasswordReset(UserId userId, Instant occurredOn) {
+    super();
+    this.userId = userId;
+    this.occurredOn = occurredOn;
+  }
+
+  @Override
+  public Instant occurredOn() {
+    return occurredOn;
+  }
+
+  /**
+   * Queries the user id of the user for whom the password needs to be reset
+   *
+   * @return the affected user's id
+   * @since 1.0.0
+   */
+  public UserId userId() {
+    return userId;
+  }
+}
