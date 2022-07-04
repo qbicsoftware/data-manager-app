aee6feecbbb5efe7d54f46384d643cdfaa18a280
Steffengreiner <Steffen.Greiner@gmx.de>
2022-07-04 11:44:26 +0200

    Add documentation and adapt login to include successful email sending

--- webapp/src/main/java/life/qbic/usermanagement/EmailSubmissionService.java
+++ webapp/src/main/java/life/qbic/usermanagement/EmailSubmissionService.java
@@ -14,6 +14,7 @@ import javax.mail.internet.MimeMessage;
 import life.qbic.email.Email;
 import life.qbic.email.EmailService;
 import life.qbic.identityaccess.application.ApplicationException;
+import life.qbic.identityaccess.domain.user.EmailAddress.EmailValidationException;
 import life.qbic.shared.application.ApplicationResponse;
 import org.springframework.beans.factory.annotation.Value;
 
@@ -59,7 +60,8 @@ public class EmailSubmissionService implements EmailService {
       msg.setText(email.content());
       Transport.send(msg);
     } catch (MessagingException e) {
-      return ApplicationResponse.failureResponse(new EmailSendingException(e.getMessage(), email));
+      return ApplicationResponse.failureResponse(
+          new EmailSendingFailedException(e.getMessage(), email));
     }
     return ApplicationResponse.successResponse();
   }
@@ -67,27 +69,50 @@ public class EmailSubmissionService implements EmailService {
   @Override
   public void send(Email email) {
     if (email.mimeType().equals("text/plain")) {
-      try {
-        sendPlainEmail(email).ifSuccessOrElse(this::reportSuccess, response -> response.failures()
-            .forEach(exception -> log.error(exception.getMessage(), exception)));
-      } catch (Exception e) {
-        log.error("Email sending failed", e.getCause());
-      }
+      sendPlainEmail(email).ifSuccessOrElse(
+          successResponse -> reportSuccess(successResponse, email),
+          failureResponse -> reportFailure(failureResponse, email));
     }
   }
 
-  private void reportSuccess(ApplicationResponse applicationResponse) {
-    //ToDo Implement ApplicationEventPublisher with SendEmail Event?
+  private void reportSuccess(ApplicationResponse applicationResponse, Email email) {
+    String emailSendSuccessMessage = String.format(
+        "Email with subject '%s' successfully send to '%s'",
+        email.subject(), email.to().address());
+    log.info(emailSendSuccessMessage);
   }
 
-  public static class EmailSendingException extends ApplicationException {
+  private void reportFailure(ApplicationResponse applicationResponse, Email email) {
+    String emailSendFailureMessage = String.format(
+        "Email with subject '%s' could not be send to '%s'",
+        email.subject(), email.to().address());
+    applicationResponse.failures().forEach(exception -> {
+      log.error(emailSendFailureMessage);
+      log.error(exception.getMessage(), exception);
+    });
+  }
+
+  /**
+   * <h1>Exception that indicates violations during the email submission process</h1>
+   *
+   * <p>This exception is supposed to be thrown, if an email could not be send during the email
+   * submission process It's intention is to contain the email information for which the exception
+   * was thrown. This exception differs from {@link EmailValidationException}, which checks if a
+   * provided email address is semantically correct
+   * </p>
+   * <p>
+   * Example: A user provides an email address which is semantically correct but does not exist
+   *
+   * @since 1.0.0
+   */
+
+  public static class EmailSendingFailedException extends ApplicationException {
 
     @Serial
     private static final long serialVersionUID = -8023119236306814904L;
-
     private final Email email;
 
-    public EmailSendingException(String message, Email email) {
+    public EmailSendingFailedException(String message, Email email) {
       super(message);
       this.email = email;
     }
