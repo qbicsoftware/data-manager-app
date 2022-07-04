d5013e433342441a7c9098191a84cb42c4a691f2
Steffengreiner <steffen.greiner@qbic.uni-tuebingen.de>
2022-07-04 14:11:21 +0200

    Catch and log invalid EmailAddresses so EmailSubmissionservice stays responsive (#65)
    * Catch and log Invalid Email Addresses in ApplicationResponse with custom Exception
    
    * Add documentation and adapt login to include successful email sending

--- webapp/src/main/java/life/qbic/usermanagement/EmailSubmissionService.java
+++ webapp/src/main/java/life/qbic/usermanagement/EmailSubmissionService.java
@@ -1,5 +1,6 @@
 package life.qbic.usermanagement;
 
+import java.io.Serial;
 import java.util.Properties;
 import javax.mail.Authenticator;
 import javax.mail.Message;
@@ -12,6 +13,9 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import life.qbic.email.Email;
 import life.qbic.email.EmailService;
+import life.qbic.identityaccess.application.ApplicationException;
+import life.qbic.identityaccess.domain.user.EmailAddress.EmailValidationException;
+import life.qbic.shared.application.ApplicationResponse;
 import org.springframework.beans.factory.annotation.Value;
 
 /**
@@ -21,6 +25,8 @@ import org.springframework.beans.factory.annotation.Value;
  */
 public class EmailSubmissionService implements EmailService {
 
+  private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(
+      EmailSubmissionService.class);
   @Value("${spring.mail.password}")
   private String password;
 
@@ -33,7 +39,7 @@ public class EmailSubmissionService implements EmailService {
   @Value("${spring.mail.port}")
   private Integer smtpPort;
 
-  private void sendPlainEmail(Email email) {
+  private ApplicationResponse sendPlainEmail(Email email) {
     Properties props = new Properties();
     props.put("mail.smtp.auth", "true");
     props.put("mail.smtp.starttls.enable", "true");
@@ -52,17 +58,67 @@ public class EmailSubmissionService implements EmailService {
       msg.setRecipient(RecipientType.TO, new InternetAddress(email.to().address()));
       msg.setSubject(email.subject());
       msg.setText(email.content());
-
       Transport.send(msg);
     } catch (MessagingException e) {
-      throw new RuntimeException(e);
+      return ApplicationResponse.failureResponse(
+          new EmailSendingFailedException(e.getMessage(), email));
     }
+    return ApplicationResponse.successResponse();
   }
 
   @Override
   public void send(Email email) {
     if (email.mimeType().equals("text/plain")) {
-      sendPlainEmail(email);
+      sendPlainEmail(email).ifSuccessOrElse(
+          successResponse -> reportSuccess(successResponse, email),
+          failureResponse -> reportFailure(failureResponse, email));
+    }
+  }
+
+  private void reportSuccess(ApplicationResponse applicationResponse, Email email) {
+    String emailSendSuccessMessage = String.format(
+        "Email with subject '%s' successfully send to '%s'",
+        email.subject(), email.to().address());
+    log.info(emailSendSuccessMessage);
+  }
+
+  private void reportFailure(ApplicationResponse applicationResponse, Email email) {
+    String emailSendFailureMessage = String.format(
+        "Email with subject '%s' could not be send to '%s'",
+        email.subject(), email.to().address());
+    applicationResponse.failures().forEach(exception -> {
+      log.error(emailSendFailureMessage);
+      log.error(exception.getMessage(), exception);
+    });
+  }
+
+  /**
+   * <h1>Exception that indicates violations during the email submission process</h1>
+   *
+   * <p>This exception is supposed to be thrown, if an email could not be send during the email
+   * submission process It's intention is to contain the email information for which the exception
+   * was thrown. This exception differs from {@link EmailValidationException}, which checks if a
+   * provided email address is semantically correct
+   * </p>
+   * <p>
+   * Example: A user provides an email address which is semantically correct but does not exist
+   *
+   * @since 1.0.0
+   */
+
+  public static class EmailSendingFailedException extends ApplicationException {
+
+    @Serial
+    private static final long serialVersionUID = -8023119236306814904L;
+    private final Email email;
+
+    public EmailSendingFailedException(String message, Email email) {
+      super(message);
+      this.email = email;
+    }
+
+    public Email getEmail() {
+      return email;
     }
   }
 }
