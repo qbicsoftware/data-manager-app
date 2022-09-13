a92dfc30ef53fb925069d7790cb458943d9b868e
Sven Fillinger <sven.fillinger@mailbox.org>
2022-09-13 09:57:10 +0200

    Add javadocs

--- mariadb-connector/src/main/java/life/qbic/finance/persistence/OfferRepository.java
+++ mariadb-connector/src/main/java/life/qbic/finance/persistence/OfferRepository.java
@@ -4,8 +4,24 @@ import java.util.List;
 import life.qbic.projectmanagement.finances.offer.Offer;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * JPA repository for {@link Offer} queries.
+ *
+ * @since 1.0.0
+ */
 public interface OfferRepository extends CrudRepository<Offer, Long> {
 
-  List<Offer> findByProjectTitleContainingIgnoreCaseOrOfferIdContainingIgnoreCase(String projectTitle, String offerId);
+  /**
+   * Case-insensitive search for offers that contain a given project title or offer id character
+   * sequence
+   *
+   * @param projectTitle the project title character sequence contained in the project title of an
+   *                     offer
+   * @param offerId      the offer id character sequence contained in the offer id of an offer
+   * @return matching search results
+   * @since 1.0.0
+   */
+  List<Offer> findByProjectTitleContainingIgnoreCaseOrOfferIdContainingIgnoreCase(
+      String projectTitle, String offerId);
 
 }
--- mariadb-connector/src/main/java/life/qbic/finance/persistence/SimpleOfferSearchService.java
+++ mariadb-connector/src/main/java/life/qbic/finance/persistence/SimpleOfferSearchService.java
@@ -1,27 +1,27 @@
 package life.qbic.finance.persistence;
 
 import java.util.List;
-import life.qbic.projectmanagement.finances.offer.OfferId;
 import life.qbic.projectmanagement.finances.offer.Offer;
-import life.qbic.projectmanagement.finances.offer.ProjectTitle;
 import life.qbic.projectmanagement.finances.offer.api.OfferSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
- * <b><class short description - 1 Line!></b>
+ * <b>Simple Offer Search Service</b>
+ * <p>
+ * Basic implementation of the {@link OfferSearchService} interface.
  *
- * <p><More detailed description - When to use, what it solves, etc.></p>
- *
- * @since <version tag>
+ * @since 1.0.0
  */
 @Service
 public class SimpleOfferSearchService implements OfferSearchService {
+
   private final OfferRepository offerRepository;
 
   @Override
   public List<Offer> findByProjectTitleOrOfferId(String projectTitle, String offerId) {
-    return offerRepository.findByProjectTitleContainingIgnoreCaseOrOfferIdContainingIgnoreCase(projectTitle, offerId);
+    return offerRepository.findByProjectTitleContainingIgnoreCaseOrOfferIdContainingIgnoreCase(
+        projectTitle, offerId);
   }
 
   @Autowired
