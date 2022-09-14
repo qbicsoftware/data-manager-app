ecdcf76b2116e27efab45ede6a83e89ceb29c9c8
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2022-09-14 10:09:27 +0200

    move to source directory

---
+++ projectmanagement/src/main/java/life/qbic/projectmanagement/application/ProjectCreationServiceImpl.java
@@ -0,0 +1,16 @@
+package life.qbic.projectmanagement.application;
+
+/**
+ * <b>short description</b>
+ *
+ * <p>detailed description</p>
+ *
+ * @since <version tag>
+ */
+public class ProjectCreationServiceImpl implements ProjectCreationService {
+
+  @Override
+  public ProjectCreationResponse createProject(String title) {
+    return null;
+  }
+}
