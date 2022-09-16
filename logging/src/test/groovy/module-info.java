3d3823f87f0adb6a6f2f66fe86398b50d0b03e93
Sven F <9976560+sven1103@users.noreply.github.com>
2022-09-16 15:56:29 +0200

    Remove module-info files (#87)
    To save us from the pain we experience with it.

--- logging/src/test/groovy/module-info.java
+++
@@ -1,8 +0,0 @@
-open module life.qbic.logging {
-  requires org.slf4j;
-  requires logback.core;
-  requires org.spockframework.core;
-  requires org.apache.groovy;
-  requires java.desktop;
-  requires life.qbic.logging.subscription.api;
-}
--- subscription-api/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module life.qbic.logging.subscription.api {
-  uses life.qbic.logging.subscription.api.Subscriber;
-
-  exports life.qbic.logging.subscription.api;
-
-}
