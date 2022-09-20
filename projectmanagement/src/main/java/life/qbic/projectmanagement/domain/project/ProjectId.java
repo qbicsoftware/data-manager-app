bfbdfcbc0a7fbf774412f68cde4da05f06d6b8d4
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2022-09-20 14:01:43 +0200

    Change from record to class
    Needed to allow JPA reflective access

--- projectmanagement/src/main/java/life/qbic/projectmanagement/domain/project/ProjectId.java
+++ projectmanagement/src/main/java/life/qbic/projectmanagement/domain/project/ProjectId.java
@@ -3,25 +3,30 @@ package life.qbic.projectmanagement.domain.project;
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
+import java.util.StringJoiner;
 import java.util.UUID;
 
 
 /**
  * The unique identifier of a project
- *
- * @param uuid
  */
-public record ProjectId(UUID uuid) implements Serializable {
+public final class ProjectId implements Serializable {
 
   @Serial
   private static final long serialVersionUID = 7904987287799381970L;
 
-  public ProjectId {
-    Objects.requireNonNull(uuid);
-  }
+  private UUID uuid;
 
   private ProjectId() {
     this(UUID.randomUUID());
+  }
+
+  private ProjectId(UUID uuid) {
+    if (Objects.isNull(uuid)) {
+      throw new IllegalArgumentException("uuid must be provided");
+    }
+    Objects.requireNonNull(uuid);
+    this.uuid = uuid;
   }
 
   public static ProjectId create() {
@@ -30,5 +35,31 @@ public record ProjectId(UUID uuid) implements Serializable {
 
   public static ProjectId of(UUID uuid) {
     return new ProjectId(uuid);
+  }
+
+  @Override
+  public String toString() {
+    return new StringJoiner(", ", ProjectId.class.getSimpleName() + "[", "]")
+        .add("uuid=" + uuid)
+        .toString();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    ProjectId projectId = (ProjectId) o;
+
+    return uuid.equals(projectId.uuid);
+  }
+
+  @Override
+  public int hashCode() {
+    return uuid.hashCode();
   }
 }
