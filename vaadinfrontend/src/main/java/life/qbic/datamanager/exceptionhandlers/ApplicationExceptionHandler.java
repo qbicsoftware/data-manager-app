8f1978f791c45d77fb180f1e486537d0dac9e9e6
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2022-09-22 18:21:09 +0200

    Add JavaDoc

--- vaadinfrontend/src/main/java/life/qbic/datamanager/exceptionhandlers/ApplicationExceptionHandler.java
+++ vaadinfrontend/src/main/java/life/qbic/datamanager/exceptionhandlers/ApplicationExceptionHandler.java
@@ -4,13 +4,17 @@ import com.vaadin.flow.component.UI;
 import life.qbic.application.commons.ApplicationException;
 
 /**
- * <b>short description</b>
- *
- * <p>detailed description</p>
- *
- * @since <version tag>
+ * Handles an application exception and presents it to the output.
  */
 public interface ApplicationExceptionHandler {
 
+  /**
+   * Handles an application exception and presents it to the user. Does not log the exception as it
+   * is assumed that the exception does not provide a complete stacktrace.
+   *
+   * @param ui        the UI in which the exception should be presented
+   * @param exception the exception
+   * @since 1.0.0
+   */
   void handle(UI ui, ApplicationException exception);
 }
