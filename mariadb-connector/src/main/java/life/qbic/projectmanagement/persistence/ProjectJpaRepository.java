bc701eca5cf3a3024f457c29dedd7c7857d2cc56
Sven Fillinger <sven.fillinger@mailbox.org>
2022-10-04 13:58:09 +0200

    Cleanup

--- mariadb-connector/src/main/java/life/qbic/projectmanagement/persistence/ProjectJpaRepository.java
+++ mariadb-connector/src/main/java/life/qbic/projectmanagement/persistence/ProjectJpaRepository.java
@@ -17,10 +17,12 @@ import java.util.List;
  *
  * <p>Implementation for the {@link ProjectRepository} interface.
  *
- * <p>This class serves as an adapter and proxies requests to an JPA implementation to interact with
+ * <p>This class serves as an adapter and proxies requests to an JPA implementation to interact
+ * with
  * persistent {@link Project} data in the storage layer.
  *
- * <p>The actual JPA implementation is done by {@link QbicProjectRepo}, which is injected as dependency
+ * <p>The actual JPA implementation is done by {@link QbicProjectRepo}, which is injected as
+ * dependency
  * upon creation.
  *
  * @since 1.0.0
@@ -34,14 +36,15 @@ public class ProjectJpaRepository implements ProjectRepository {
   private final ProjectPreviewRepository projectPreviewRepo;
 
   @Autowired
-  public ProjectJpaRepository(QbicProjectRepo projectRepo, ProjectPreviewRepository projectPreviewRepo) {
+  public ProjectJpaRepository(QbicProjectRepo projectRepo,
+      ProjectPreviewRepository projectPreviewRepo) {
     this.projectRepo = projectRepo;
     this.projectPreviewRepo = projectPreviewRepo;
   }
 
   @Override
   public void add(Project project) {
-    if(doesProjectExistWithId(project.getId())) {
+    if (doesProjectExistWithId(project.getId())) {
       throw new ProjectExistsException();
     }
     projectRepo.save(project);
@@ -54,9 +57,8 @@ public class ProjectJpaRepository implements ProjectRepository {
 
   @Override
   public List<ProjectPreview> query(String filter, int offset, int limit) {
-    var result = projectPreviewRepo.findByProjectTitleContainingIgnoreCase(filter, new OffsetBasedRequest(offset, limit)).getContent();
-    return result;
-
+    return projectPreviewRepo.findByProjectTitleContainingIgnoreCase(filter,
+        new OffsetBasedRequest(offset, limit)).getContent();
   }
 
   private boolean doesProjectExistWithId(ProjectId id) {
