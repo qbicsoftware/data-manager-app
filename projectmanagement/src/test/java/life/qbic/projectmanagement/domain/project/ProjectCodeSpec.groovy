e6e9967e74c93803df4e52e474ffd757b3eb10ed
Sven Fillinger <sven.fillinger@mailbox.org>
2022-10-10 09:45:23 +0200

    Update JDs

--- projectmanagement/src/test/java/life/qbic/projectmanagement/domain/project/ProjectCodeSpec.groovy
+++ projectmanagement/src/test/java/life/qbic/projectmanagement/domain/project/ProjectCodeSpec.groovy
@@ -2,13 +2,6 @@ package life.qbic.projectmanagement.domain.project
 
 import spock.lang.Specification
 
-/**
- * <b><class short description - 1 Line!></b>
- *
- * <p><More detailed description - When to use, what it solves, etc.></p>
- *
- * @since <version tag>
- */
 class ProjectCodeSpec extends Specification {
 
     def "Calling the random factory method creates a new random code"() {
