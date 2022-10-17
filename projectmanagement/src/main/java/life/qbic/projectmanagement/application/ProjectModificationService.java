8a43ee37707bc9db0b42bc838b2485584a366988
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2022-10-17 09:12:22 +0200

    sketch application service

---
+++ projectmanagement/src/main/java/life/qbic/projectmanagement/application/ProjectModificationService.java
@@ -0,0 +1,22 @@
+package life.qbic.projectmanagement.application;
+
+/**
+ * <b>short description</b>
+ *
+ * <p>detailed description</p>
+ *
+ * @since <version tag>
+ */
+public class ProjectModificationService {
+
+  public void linkOfferToProject(String offerIdentifier, String projectIdentifier) {
+    /*
+     TODO:
+     - offer := query offer / verify that offer exists
+     - project := query project
+     - project.linkOffer(offer.id())
+     - projectRepository.save(project)
+     */
+  }
+
+}
