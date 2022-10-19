4a69bb3a7b6da73f4e7a7facd407d941f0a172d4
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2022-10-19 15:32:44 +0200

    move offer links to ProjectLinkingService

--- projectmanagement/src/main/java/life/qbic/projectmanagement/application/ProjectInformationService.java
+++ projectmanagement/src/main/java/life/qbic/projectmanagement/application/ProjectInformationService.java
@@ -1,13 +1,11 @@
 package life.qbic.projectmanagement.application;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import life.qbic.logging.api.Logger;
 import life.qbic.logging.service.LoggerFactory;
 import life.qbic.projectmanagement.application.api.ProjectPreviewLookup;
-import life.qbic.projectmanagement.domain.project.OfferIdentifier;
 import life.qbic.projectmanagement.domain.project.Project;
 import life.qbic.projectmanagement.domain.project.ProjectId;
 import life.qbic.projectmanagement.domain.project.repository.ProjectRepository;
@@ -62,11 +60,6 @@ public class ProjectInformationService {
   public Optional<Project> find(ProjectId projectId) {
     log.debug("Search for project with id: " + projectId.toString());
     return projectRepository.find(projectId);
-  }
-
-  public List<OfferIdentifier> queryLinkedOffers(ProjectId projectId) {
-    return projectRepository.find(projectId).map(Project::linkedOffers).orElse(
-        Collections.emptyList());
   }
 
 
