6258713f92c013545ca6fc4bad769482a9e933ea
Sven Fillinger <sven.fillinger@mailbox.org>
2023-01-19 15:33:26 +0100

    Add JDs for condition

--- projectmanagement/src/main/java/life/qbic/projectmanagement/domain/project/experiment/Condition.java
+++ projectmanagement/src/main/java/life/qbic/projectmanagement/domain/project/experiment/Condition.java
@@ -8,11 +8,29 @@ import java.util.Random;
 import java.util.stream.Collectors;
 
 /**
- * <b><class short description - 1 Line!></b>
+ * <b>Condition</b>
+ * <p>
+ * Conditions represent different linear combinations of experimental variable level(s). For example
+ * you want to compare different genotypes of a specimen, or different treatments. Let's assume you
+ * have a experimental design with two variables:
+ * <ul>
+ * <li>(1) the genotype (wildtype vs. mutant) </li>
+ * <li>(2) a treatment a solvent with different concentrations (0 mmol/L and 150 mmol/L)</li>
+ * </ul>
+ * <p>
+ * So in total there will be four conditions:
  *
- * <p><More detailed description - When to use, what it solves, etc.></p>
+ * <ul>
+ *   <li>(1) wildtype + 0 mmol/L</li>
+ *   <li>(2) wildtype + 150 mmol/L</li>
+ *   <li>(3) mutant + 0 mmol/L</li>
+ *   <li>(4) mutant + 150 mmol/L</li>
+ * </ul>
+ * <p>
+ * Conditions in an experimental design can be defined via {@link ExperimentalDesign#createCondition(VariableLevel[])}.
+ * <p>
  *
- * @since <version tag>
+ * @since 1.0.0
  */
 public class Condition {
 
@@ -20,7 +38,14 @@ public class Condition {
 
   private final Long id;
 
-
+  /**
+   * Creates a new instance of a condition object with one or more {@link VariableLevel} defining
+   * the level of an experimental variable.
+   *
+   * @param definedVariables the linear combination of experimental variable levels
+   * @return the condition
+   * @since 1.0.0
+   */
   @SafeVarargs
   public static Condition create(VariableLevel<ExperimentalValue>... definedVariables) {
     Arrays.stream(definedVariables).forEach(Objects::requireNonNull);
@@ -29,8 +54,9 @@ public class Condition {
       throw new IllegalArgumentException("Please define at least one variable level.");
     }
 
-    int distinctExperimentVariables = Arrays.stream(definedVariables).map(VariableLevel::experimentalVariable).collect(
-        Collectors.toSet()).size();
+    int distinctExperimentVariables = Arrays.stream(definedVariables)
+        .map(VariableLevel::experimentalVariable).collect(
+            Collectors.toSet()).size();
     if (distinctExperimentVariables < definedVariables.length) {
       throw new IllegalArgumentException(
           "Variable levels are not from distinct experimental variables.");
@@ -40,13 +66,20 @@ public class Condition {
   }
 
 
-
   @SafeVarargs
   private Condition(Long id, VariableLevel<ExperimentalValue>... definedVariables) {
     this.id = id;
     this.definedVariables = Arrays.stream(definedVariables).toList();
   }
 
+  /**
+   * Queries the {@link ExperimentalValue} of an experimental variable. If the condition does not
+   * contain a value for the experimental variable, the result will be empty.
+   *
+   * @param experimentalVariable the experimental variable to look for
+   * @return the value if present, else empty
+   * @since 1.0.0
+   */
   public Optional<ExperimentalValue> valueOf(
       ExperimentalVariable<ExperimentalValue> experimentalVariable) {
     return definedVariables.stream().filter(
@@ -55,10 +88,22 @@ public class Condition {
         .map(VariableLevel::experimentalValue).findAny();
   }
 
+  /**
+   * Lists all available experimental variables in the condition.
+   *
+   * @return A list of the experimental variables
+   * @since 1.0.0
+   */
   public List<ExperimentalVariable<ExperimentalValue>> experimentalVariables() {
     return definedVariables.stream().map(VariableLevel::experimentalVariable).toList();
   }
 
+  /**
+   * The identifier of the condition.
+   *
+   * @return
+   * @since 1.0.0
+   */
   public Long id() {
     return this.id;
   }
