2620493b64d1327828d7c3b54803025047b4cae6
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2023-01-23 15:34:04 +0100

    extend records for vocabularies

--- projectmanagement/src/main/java/life/qbic/projectmanagement/domain/project/vocabulary/Analyte.java
+++ projectmanagement/src/main/java/life/qbic/projectmanagement/domain/project/vocabulary/Analyte.java
@@ -1,3 +1,22 @@
 package life.qbic.projectmanagement.domain.project.vocabulary;
 
-public record Analyte(String name) { }
+import java.util.Objects;
+import life.qbic.projectmanagement.domain.project.ProjectManagementDomainException;
+
+public record Analyte(String label) {
+
+  public Analyte {
+    Objects.requireNonNull(label);
+    if (label.isEmpty()) {
+      throw new ProjectManagementDomainException("Vocabulary label for Analyte is empty.");
+    }
+  }
+
+  public static Analyte create(String label) {
+    return new Analyte(label);
+  }
+
+  public String value() {
+    return this.label();
+  }
+}
--- projectmanagement/src/main/java/life/qbic/projectmanagement/domain/project/vocabulary/Species.java
+++ projectmanagement/src/main/java/life/qbic/projectmanagement/domain/project/vocabulary/Species.java
@@ -1,3 +1,21 @@
 package life.qbic.projectmanagement.domain.project.vocabulary;
 
-public record Species(String name) { }
+import java.util.Objects;
+import life.qbic.projectmanagement.domain.project.ProjectManagementDomainException;
+
+public record Species(String label) {
+  public Species {
+    Objects.requireNonNull(label);
+    if (label.isEmpty()) {
+      throw new ProjectManagementDomainException("Vocabulary label for Species is empty.");
+    }
+  }
+
+  public static Species create(String label) {
+    return new Species(label);
+  }
+
+  public String value() {
+    return this.label();
+  }
+}
