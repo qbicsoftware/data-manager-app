4735363156055e9bcd61da43e6ee799f0805e82a
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2023-03-10 14:52:42 +0100

    simplify exception handling in project view page

--- vaadinfrontend/src/main/java/life/qbic/datamanager/views/project/view/ProjectViewPage.java
+++ vaadinfrontend/src/main/java/life/qbic/datamanager/views/project/view/ProjectViewPage.java
@@ -3,25 +3,19 @@ package life.qbic.datamanager.views.project.view;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.router.BeforeEvent;
-import com.vaadin.flow.router.ErrorParameter;
-import com.vaadin.flow.router.HasErrorParameter;
-import com.vaadin.flow.router.HasUrlParameter;
-import com.vaadin.flow.router.InternalServerError;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.RouteAlias;
 import java.io.Serial;
 import javax.annotation.security.PermitAll;
-import life.qbic.application.commons.ApplicationException;
 import life.qbic.datamanager.views.MainLayout;
 import life.qbic.datamanager.views.project.view.components.ExperimentalDesignDetailComponent;
 import life.qbic.datamanager.views.project.view.components.ProjectDetailsComponent;
 import life.qbic.datamanager.views.project.view.components.ProjectLinksComponent;
 import life.qbic.logging.api.Logger;
 import life.qbic.logging.service.LoggerFactory;
+import life.qbic.projectmanagement.application.ProjectManagementException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -33,8 +27,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Route(value = "projects/:projectId?", layout = MainLayout.class)
 @PermitAll
 @CssImport("./styles/views/project/project-view.css")
-public class ProjectViewPage extends Div implements
-    BeforeEnterObserver, HasErrorParameter<ApplicationException> {
+public class ProjectViewPage extends Div implements BeforeEnterObserver {
 
   @Serial
   private static final long serialVersionUID = 3402433356187177105L;
@@ -45,7 +38,8 @@ public class ProjectViewPage extends Div implements
   private final transient ProjectViewHandler handler;
 
   public ProjectViewPage(@Autowired ProjectDetailsComponent projectDetailsComponent, @Autowired
-  ProjectLinksComponent projectLinksComponent, @Autowired ExperimentalDesignDetailComponent experimentalDesignDetailComponent) {
+  ProjectLinksComponent projectLinksComponent,
+      @Autowired ExperimentalDesignDetailComponent experimentalDesignDetailComponent) {
     handler = new ProjectViewHandler(projectDetailsComponent,
         projectLinksComponent, experimentalDesignDetailComponent);
     add(projectDetailsComponent);
@@ -54,19 +48,24 @@ public class ProjectViewPage extends Div implements
     initNavbar(projectDetailsComponent, experimentalDesignDetailComponent);
     add(navBar);
     setPageStyles();
-    setComponentStyles(projectDetailsComponent, projectLinksComponent, experimentalDesignDetailComponent);
+    setComponentStyles(projectDetailsComponent, projectLinksComponent,
+        experimentalDesignDetailComponent);
     log.debug(
         String.format("New instance for project view (#%s) created with detail component (#%s)",
             System.identityHashCode(this), System.identityHashCode(projectDetailsComponent)));
   }
 
-  private void initNavbar(ProjectDetailsComponent projectDetailsComponent, ExperimentalDesignDetailComponent experimentalDesignDetailComponent) {
+  private void initNavbar(ProjectDetailsComponent projectDetailsComponent,
+      ExperimentalDesignDetailComponent experimentalDesignDetailComponent) {
     navBar.add(switchComponentsButton);
-    switchComponentsButton.addClickListener(clickEvent -> switchDetailsComponents(projectDetailsComponent, experimentalDesignDetailComponent));
+    switchComponentsButton.addClickListener(
+        clickEvent -> switchDetailsComponents(projectDetailsComponent,
+            experimentalDesignDetailComponent));
   }
 
-  private void switchDetailsComponents(ProjectDetailsComponent projectDetailsComponent, ExperimentalDesignDetailComponent experimentalDesignDetailComponent){
-    if(this.getChildren().toList().contains(projectDetailsComponent)){
+  private void switchDetailsComponents(ProjectDetailsComponent projectDetailsComponent,
+      ExperimentalDesignDetailComponent experimentalDesignDetailComponent) {
+    if (this.getChildren().toList().contains(projectDetailsComponent)) {
       add(experimentalDesignDetailComponent);
       remove(projectDetailsComponent);
     } else if (this.getChildren().toList().contains(experimentalDesignDetailComponent)) {
@@ -80,7 +79,8 @@ public class ProjectViewPage extends Div implements
   }
 
   public void setComponentStyles(ProjectDetailsComponent projectDetailsComponent,
-      ProjectLinksComponent projectLinksComponent,  ExperimentalDesignDetailComponent experimentalDesignDetailComponent) {
+      ProjectLinksComponent projectLinksComponent,
+      ExperimentalDesignDetailComponent experimentalDesignDetailComponent) {
     projectDetailsComponent.setStyles("project-details-component");
     projectLinksComponent.setStyles("project-links-component");
     //Todo Determine if we want to have seperate styles for each component
@@ -90,24 +90,10 @@ public class ProjectViewPage extends Div implements
   @Override
   public void beforeEnter(BeforeEnterEvent beforeEnterEvent) {
     beforeEnterEvent.getRouteParameters().get("projectId")
-        .ifPresentOrElse(handler::routeParameter, () -> {
-          throw new ApplicationException() {
-            @Override
-            public ErrorCode errorCode() {
-              return ErrorCode.INVALID_PROJECT_CODE;
-            }
-
-            @Override
-            public ErrorParameters errorParameters() {
-              return ErrorParameters.create();
-            }
-          };
-        });
-  }
-
-  @Override
-  public int setErrorParameter(BeforeEnterEvent beforeEnterEvent,
-      ErrorParameter<ApplicationException> errorParameter) {
-    return 0;
+        .ifPresentOrElse(
+            handler::routeParameter,
+            () -> {
+              throw new ProjectManagementException("no project id provided");
+            });
   }
 }
