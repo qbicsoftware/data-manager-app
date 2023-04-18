9c6566701670945acef704dd5f0e12792b9e2475
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2023-04-18 14:16:35 +0200

    Remove dead code

--- vaadinfrontend/src/main/java/life/qbic/datamanager/views/general/Container.java
+++ vaadinfrontend/src/main/java/life/qbic/datamanager/views/general/Container.java
@@ -1,13 +1,5 @@
 package life.qbic.datamanager.views.general;
 
-/**
- * TODO!
- * <b>short description</b>
- *
- * <p>detailed description</p>
- *
- * @since <version tag>
- */
 public class Container<T> {
 
   private T item;
--- vaadinfrontend/src/main/java/life/qbic/datamanager/views/projects/project/experiments/experiment/Tag.java
+++ vaadinfrontend/src/main/java/life/qbic/datamanager/views/projects/project/experiments/experiment/Tag.java
@@ -3,14 +3,6 @@ package life.qbic.datamanager.views.projects.project.experiments.experiment;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.theme.lumo.LumoUtility.FontSize;
 
-/**
- * TODO!
- * <b>short description</b>
- *
- * <p>detailed description</p>
- *
- * @since <version tag>
- */
 public class Tag extends Span {
 
   public Tag(String text) {
