e0b60188802ce71e34ae22d4da719dbfc0cf331b
Sven Fillinger <sven.fillinger@mailbox.org>
2023-04-21 09:15:17 +0200

    Test sample assignment

--- projectmanagement/src/test/groovy/life/qbic/projectmanagement/domain/project/sample/SampleSpec.groovy
+++ projectmanagement/src/test/groovy/life/qbic/projectmanagement/domain/project/sample/SampleSpec.groovy
@@ -29,6 +29,23 @@ class SampleSpec extends Specification {
         response.code().equals(Sample.SampleAddResponse.ResponseCode.SUCCESSFUL)
         unAssignedSample.assignedBatch().get().equals(batchId)
 
+    }
+
+    def "If a sample is already assigned to a batch, the assignment is not overwritten and a failure response is returned"() {
+        given:
+        SampleOrigin sampleOrigin = new SampleOrigin(Species.create("Homo Sapiens"), Specimen.create("Skin"), Analyte.create("DNA"))
+        Sample unAssignedSample = Sample.create("My test sample", ExperimentId.create(), 1_000L, new BiologicalReplicateLabel("biol repl. 1"), sampleOrigin)
+        Batch batch = Batch.create("Test Batch")
+        Batch anotherBatch = Batch.create("Another Batch")
+
+        when:
+        unAssignedSample.assignToBatch(batch)
+        def response = unAssignedSample.assignToBatch(anotherBatch)
+
+        then:
+        response.code().equals(Sample.SampleAddResponse.ResponseCode.ALREADY_IN_BATCH)
+        unAssignedSample.assignedBatch().get().equals(batch.batchId())
+
 
     }
 
