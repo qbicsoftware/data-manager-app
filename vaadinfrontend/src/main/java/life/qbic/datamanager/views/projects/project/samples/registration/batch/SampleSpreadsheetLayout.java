dea6869b5db8a041ff16a27ef9e33c1a74e8bedf
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2023-07-04 17:03:19 +0200

    use error notification for invalid sample batch information

--- vaadinfrontend/src/main/java/life/qbic/datamanager/views/projects/project/samples/registration/batch/SampleSpreadsheetLayout.java
+++ vaadinfrontend/src/main/java/life/qbic/datamanager/views/projects/project/samples/registration/batch/SampleSpreadsheetLayout.java
@@ -13,7 +13,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import life.qbic.application.commons.Result;
-import life.qbic.datamanager.views.notifications.InformationMessage;
+import life.qbic.datamanager.views.notifications.ErrorMessage;
 import life.qbic.datamanager.views.notifications.StyledNotification;
 import life.qbic.datamanager.views.projects.project.samples.registration.batch.SampleRegistrationSpreadsheet.InvalidSpreadsheetRow;
 import life.qbic.datamanager.views.projects.project.samples.registration.batch.SampleRegistrationSpreadsheet.NGSRowDTO;
@@ -141,7 +141,7 @@ class SampleSpreadsheetLayout extends Div {
     }
 
     private void displayInputInvalidMessage(String invalidationReason) {
-      InformationMessage infoMessage = new InformationMessage(
+      ErrorMessage infoMessage = new ErrorMessage(
           "Incomplete or erroneous metadata found",
           invalidationReason);
       StyledNotification notification = new StyledNotification(infoMessage);
