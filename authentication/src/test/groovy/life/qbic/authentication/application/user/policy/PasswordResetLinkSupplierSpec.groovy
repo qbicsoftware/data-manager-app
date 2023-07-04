b5aaaabe5f87221e8376e458143f63b323132b04
Sven Fillinger <sven.fillinger@mailbox.org>
2023-07-04 10:57:15 +0200

    Fix tests

--- authentication/src/test/groovy/life/qbic/authentication/application/user/policy/PasswordResetLinkSupplierSpec.groovy
+++ authentication/src/test/groovy/life/qbic/authentication/application/user/policy/PasswordResetLinkSupplierSpec.groovy
@@ -7,7 +7,7 @@ class PasswordResetLinkSupplierSpec extends Specification {
 
     def "when a registration link is requested, the url contains the correct path and some token"() {
         given:
-        def linkSupplier = new PasswordResetLinkSupplier("https", "hostname", 8080, "endpoint", "confirmation-parameter")
+        def linkSupplier = new PasswordResetLinkSupplier("https", "hostname", 8080,"", "endpoint", "confirmation-parameter")
         expect:
         linkSupplier.passwordResetUrl("some-user") == "https://hostname:8080/endpoint?confirmation-parameter=some-user"
     }
