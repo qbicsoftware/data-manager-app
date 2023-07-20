c19f9c10c9bed98ba33168718ae06993d495e5a1
Sven Fillinger <sven.fillinger@mailbox.org>
2023-07-20 14:31:32 +0200

    Save work

--- vaadinfrontend/src/main/java/life/qbic/datamanager/views/projects/project/experiments/experiment/components/ExperimentalGroupsDialog.java
+++ vaadinfrontend/src/main/java/life/qbic/datamanager/views/projects/project/experiments/experiment/components/ExperimentalGroupsDialog.java
@@ -1,8 +1,14 @@
 package life.qbic.datamanager.views.projects.project.experiments.experiment.components;
 
+import com.vaadin.flow.component.ComponentEvent;
 import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.ItemLabelGenerator;
 import com.vaadin.flow.component.combobox.MultiSelectComboBox;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.textfield.NumberField;
+import jakarta.validation.constraints.Min;
 import java.io.Serial;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -26,6 +32,10 @@ public class ExperimentalGroupsDialog extends DialogWindow {
   private final Collection<VariableLevel> experimentalVariables;
 
   private final List<ComponentEventListener<CancelEvent<ExperimentalGroupsDialog>>> cancelListeners = new ArrayList<>();
+
+  private final ExperimentalGroupEntry experimentalGroupEntry = new ExperimentalGroupEntry();
+
+  private final Div experimentalGroupsCollection = new Div();
 
   private static final ItemLabelGenerator<VariableLevel> VARIABLE_LEVEL_ITEM_LABEL_GENERATOR = it -> String.format(
       "%s: %s", it.variableName().value(),
@@ -55,15 +65,11 @@ public class ExperimentalGroupsDialog extends DialogWindow {
   }
 
   private void layoutComponent() {
-    MultiSelectComboBox<VariableLevel> multiSelectComboBox = new MultiSelectComboBox<>();
-    multiSelectComboBox.setItems(experimentalVariables);
-    multiSelectComboBox.setItemLabelGenerator(VARIABLE_LEVEL_ITEM_LABEL_GENERATOR);
-    multiSelectComboBox.setWidthFull();
-    multiSelectComboBox.setLabel("Condition");
-    multiSelectComboBox.addClassName("combo-box");
-    multiSelectComboBox.addClassName("chip-badge");
-    multiSelectComboBox.setAllowCustomValue(false);
-    add(multiSelectComboBox);
+    addNewGroupEntry();
+    var addNewGroupIcon = new Icon(VaadinIcon.PLUS);
+    addNewGroupIcon.addClickListener(listener -> addNewGroupEntry());
+    add(experimentalGroupsCollection);
+    add(addNewGroupIcon);
 
     addClassName("experiment-group-dialog");
     setConfirmButtonLabel("Add");
@@ -71,6 +77,27 @@ public class ExperimentalGroupsDialog extends DialogWindow {
     getFooter().add(cancelButton, confirmButton);
   }
 
+  private void addNewGroupEntry() {
+    var groupEntry = experimentalGroupEntry.empty();
+    groupEntry.setItems(experimentalVariables);
+    experimentalGroupsCollection.add(groupEntry);
+    groupEntry.registerForRemoveEvents(event -> removeExperimentalGroupEntry(event.getSource()));
+  }
+
+  void removeExperimentalGroupEntry(ExperimentalGroupEntry entry) {
+    experimentalGroupsCollection.getChildren().filter(entry::equals)
+        .findAny().ifPresent(experimentalGroupsCollection::remove);
+    refreshGroupEntries();
+  }
+
+  private void refreshGroupEntries() {
+    if (experimentalGroupsCollection.getChildren().toList().isEmpty()) {
+      var groupEntry = experimentalGroupEntry.empty();
+      groupEntry.setItems(experimentalVariables);
+      experimentalGroupsCollection.add(groupEntry);
+      groupEntry.registerForRemoveEvents(event -> removeExperimentalGroupEntry(event.getSource()));
+    }
+  }
 
   public static ExperimentalGroupsDialog empty(Collection<VariableLevel> experimentalVariables) {
     return new ExperimentalGroupsDialog(experimentalVariables);
@@ -81,5 +108,75 @@ public class ExperimentalGroupsDialog extends DialogWindow {
     return null;
   }
 
+  private class ExperimentalGroupEntry extends Div {
 
+    @Serial
+    private static final long serialVersionUID = -1387021927263833261L;
+    private MultiSelectComboBox<VariableLevel> variableComboBox;
+
+    private final List<ComponentEventListener<RemoveEvent>> removeEventListeners = new ArrayList<>();
+
+    @Min(1)
+    private NumberField sampleSize;
+    private ExperimentalGroupEntry() {
+      super();
+      variableComboBox = new MultiSelectComboBox<>();
+      sampleSize = new NumberField();
+      sampleSize.addClassName("number-field");
+      sampleSize.setLabel("Biological Replicates");
+      sampleSize.setStepButtonsVisible(true);
+      sampleSize.setStep(1);
+      sampleSize.setMin(1);
+      sampleSize.setValue(1.0);
+      sampleSize.setErrorMessage("Please specify a valid number of replicates");
+      //variableComboBox.setItems(experimentalVariables);
+      variableComboBox.setItemLabelGenerator(VARIABLE_LEVEL_ITEM_LABEL_GENERATOR);
+      variableComboBox.setWidthFull();
+      variableComboBox.setLabel("Condition");
+      variableComboBox.addClassName("combo-box");
+      variableComboBox.addClassName("chip-badge");
+      variableComboBox.setAllowCustomValue(false);
+      add(variableComboBox);
+      add(sampleSize);
+      var deleteIcon = new Icon(VaadinIcon.CLOSE_SMALL);
+      deleteIcon.addClickListener(listener -> notifyListenersAboutRemove());
+      add(deleteIcon);
+      addClassName("experimental-group-entry");
+    }
+
+    ExperimentalGroupEntry empty() {
+      return new ExperimentalGroupEntry();
+    }
+
+    void setItems(Collection<VariableLevel> availableVariableLevels) {
+      variableComboBox.setItems(availableVariableLevels);
+    }
+
+    void registerForRemoveEvents(ComponentEventListener<RemoveEvent> listener){
+      removeEventListeners.add(listener);
+    }
+
+    private void notifyListenersAboutRemove() {
+      var removeEvent = new RemoveEvent(this, true);
+      removeEventListeners.forEach(listener -> listener.onComponentEvent(removeEvent));
+    }
+  }
+
+  class RemoveEvent extends ComponentEvent<ExperimentalGroupEntry> {
+
+    @Serial
+    private static final long serialVersionUID = 2934203596212238997L;
+
+    /**
+     * Creates a new event using the given source and indicator whether the event originated from the
+     * client side or the server side.
+     *
+     * @param source     the source component
+     * @param fromClient <code>true</code> if the event originated from the client
+     *                   side, <code>false</code> otherwise
+     */
+    public RemoveEvent(ExperimentalGroupEntry source, boolean fromClient) {
+      super(source, fromClient);
+    }
+  }
 }
