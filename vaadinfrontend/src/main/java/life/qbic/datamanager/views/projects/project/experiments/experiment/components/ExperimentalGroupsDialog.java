02f1f68f8153a6857753b42b616502efb68abe04
Tobias Koch <kochtobi@users.noreply.github.com>
2023-08-03 09:49:44 +0200

    Exchange group input by existing component

--- vaadinfrontend/src/main/java/life/qbic/datamanager/views/projects/project/experiments/experiment/components/ExperimentalGroupsDialog.java
+++ vaadinfrontend/src/main/java/life/qbic/datamanager/views/projects/project/experiments/experiment/components/ExperimentalGroupsDialog.java
@@ -1,15 +1,11 @@
 package life.qbic.datamanager.views.projects.project.experiments.experiment.components;
 
-import com.vaadin.flow.component.ComponentEvent;
 import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.ItemLabelGenerator;
-import com.vaadin.flow.component.combobox.MultiSelectComboBox;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.textfield.NumberField;
-import jakarta.validation.constraints.Min;
 import java.io.Serial;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -18,6 +14,7 @@ import java.util.Objects;
 import life.qbic.datamanager.views.general.CancelEvent;
 import life.qbic.datamanager.views.general.ConfirmEvent;
 import life.qbic.datamanager.views.general.DialogWindow;
+import life.qbic.datamanager.views.projects.project.experiments.experiment.ExperimentalGroupInput;
 import life.qbic.projectmanagement.application.ExperimentValueFormatter;
 import life.qbic.projectmanagement.domain.project.experiment.VariableLevel;
 
@@ -36,7 +33,7 @@ public class ExperimentalGroupsDialog extends DialogWindow {
       ExperimentValueFormatter.format(it.experimentalValue()));
   @Serial
   private static final long serialVersionUID = 1657697182040756406L;
-  private final Collection<VariableLevel> experimentalVariables;
+  private final Collection<VariableLevel> experimentalVariableLevels;
   private final List<ComponentEventListener<CancelEvent<ExperimentalGroupsDialog>>> cancelListeners = new ArrayList<>();
   private final Div experimentalGroupsCollection = new Div();
   private final Div content = new Div();
@@ -44,30 +41,29 @@ public class ExperimentalGroupsDialog extends DialogWindow {
   private final boolean editMode;
   private final List<ComponentEventListener<ConfirmEvent<ExperimentalGroupsDialog>>> confirmListeners = new ArrayList<>();
 
-  private ExperimentalGroupsDialog(Collection<VariableLevel> experimentalVariables,
+  private ExperimentalGroupsDialog(Collection<VariableLevel> experimentalVariableLevels,
       boolean editMode) {
     super();
     this.editMode = editMode;
-    this.experimentalVariables = Objects.requireNonNull(experimentalVariables);
+    this.experimentalVariableLevels = Objects.requireNonNull(experimentalVariableLevels);
     layoutComponent();
     configureComponent();
   }
 
-  private ExperimentalGroupsDialog(Collection<VariableLevel> experimentalVariables,
+  private ExperimentalGroupsDialog(Collection<VariableLevel> experimentalVariableLevels,
       Collection<ExperimentalGroupContent> experimentalGroupContents, boolean editMode) {
-    this(experimentalVariables, editMode);
+    this(experimentalVariableLevels, editMode);
     this.experimentalGroupsCollection.removeAll();
-    addEntries(experimentalVariables, experimentalGroupContents);
+    addEntries(experimentalVariableLevels, experimentalGroupContents);
   }
 
-  private void addEntries(Collection<VariableLevel> experimentalVariables,
+  private void addEntries(Collection<VariableLevel> experimentalVariableLevels,
       Collection<ExperimentalGroupContent> experimentalGroupContents) {
     experimentalGroupContents.stream().map(group -> {
-      var groupEntry = new ExperimentalGroupEntry();
-      groupEntry.setAvailableVariableLevels(experimentalVariables);
+      var groupEntry = new ExperimentalGroupInput(experimentalVariableLevels);
       groupEntry.setCondition(group.variableLevels());
-      groupEntry.setSampleSize(group.size());
-      groupEntry.registerForRemoveEvents(
+      groupEntry.setReplicateCount(group.size());
+      groupEntry.addRemoveEventListener(
           listener -> experimentalGroupsCollection.remove(groupEntry));
       return groupEntry;
     }).forEach(experimentalGroupsCollection::add);
@@ -82,9 +78,9 @@ public class ExperimentalGroupsDialog extends DialogWindow {
     return new ExperimentalGroupsDialog(experimentalVariables, experimentalGroupContents, true);
   }
 
-  private static ExperimentalGroupContent convert(ExperimentalGroupEntry experimentalGroupEntry) {
-    return new ExperimentalGroupContent(experimentalGroupEntry.sampleSize.getValue().intValue(),
-        experimentalGroupEntry.condition.getSelectedItems());
+  private static ExperimentalGroupContent convert(ExperimentalGroupInput experimentalGroupInput) {
+    return new ExperimentalGroupContent(experimentalGroupInput.getReplicateCount(),
+        experimentalGroupInput.getCondition());
   }
 
   private void configureComponent() {
@@ -142,13 +138,12 @@ public class ExperimentalGroupsDialog extends DialogWindow {
   }
 
   private void addNewGroupEntry() {
-    var groupEntry = new ExperimentalGroupEntry();
-    groupEntry.setAvailableVariableLevels(experimentalVariables);
+    var groupEntry = new ExperimentalGroupInput(experimentalVariableLevels);
     experimentalGroupsCollection.add(groupEntry);
-    groupEntry.registerForRemoveEvents(event -> removeExperimentalGroupEntry(event.getSource()));
+    groupEntry.addRemoveEventListener(event -> removeExperimentalGroupEntry(event.getSource()));
   }
 
-  void removeExperimentalGroupEntry(ExperimentalGroupEntry entry) {
+  void removeExperimentalGroupEntry(ExperimentalGroupInput entry) {
     experimentalGroupsCollection.getChildren().filter(entry::equals)
         .findAny().ifPresent(experimentalGroupsCollection::remove);
     refreshGroupEntries();
@@ -156,10 +151,9 @@ public class ExperimentalGroupsDialog extends DialogWindow {
 
   private void refreshGroupEntries() {
     if (experimentalGroupsCollection.getChildren().toList().isEmpty()) {
-      var groupEntry = new ExperimentalGroupEntry();
-      groupEntry.setAvailableVariableLevels(experimentalVariables);
+      var groupEntry = new ExperimentalGroupInput(experimentalVariableLevels);
       experimentalGroupsCollection.add(groupEntry);
-      groupEntry.registerForRemoveEvents(event -> removeExperimentalGroupEntry(event.getSource()));
+      groupEntry.addRemoveEventListener(event -> removeExperimentalGroupEntry(event.getSource()));
     }
   }
 
@@ -172,92 +166,11 @@ public class ExperimentalGroupsDialog extends DialogWindow {
    */
   public Collection<ExperimentalGroupContent> experimentalGroups() {
     return this.experimentalGroupsCollection.getChildren()
-        .filter(component -> component.getClass().equals(ExperimentalGroupEntry.class))
-        .map(experimentalGroupEntry -> convert((ExperimentalGroupEntry) experimentalGroupEntry))
+        .filter(component -> component.getClass().equals(ExperimentalGroupInput.class))
+        .map(experimentalGroupEntry -> convert((ExperimentalGroupInput) experimentalGroupEntry))
         .toList();
   }
 
   public record ExperimentalGroupContent(int size, Collection<VariableLevel> variableLevels) {}
 
-  class ExperimentalGroupEntry extends Div {
-
-    @Serial
-    private static final long serialVersionUID = -1387021927263833261L;
-    private final List<ComponentEventListener<RemoveEvent>> removeEventListeners = new ArrayList<>();
-    private final MultiSelectComboBox<VariableLevel> condition = new MultiSelectComboBox<>();
-    @Min(1)
-    private final NumberField sampleSize = new NumberField();
-
-    private ExperimentalGroupEntry() {
-      super();
-      addClassName("experimental-group-entry");
-
-      configureNumberFied();
-      configureConditionField();
-
-      var deleteIcon = new Icon(VaadinIcon.CLOSE_SMALL);
-      deleteIcon.addClickListener(listener -> notifyListenersAboutRemove());
-
-      add(condition, sampleSize, deleteIcon);
-    }
-
-    private void configureConditionField() {
-      condition.setItems(new ArrayList<>());
-      condition.setItemLabelGenerator(VARIABLE_LEVEL_ITEM_LABEL_GENERATOR);
-      condition.setWidthFull();
-      condition.setLabel("Condition");
-      condition.addClassName("combo-box");
-      condition.addClassName("chip-badge");
-      condition.setAllowCustomValue(false);
-    }
-
-    private void configureNumberFied() {
-      sampleSize.addClassName("number-field");
-      sampleSize.setLabel("Biological Replicates");
-      sampleSize.setStepButtonsVisible(true);
-      sampleSize.setStep(1);
-      sampleSize.setMin(1);
-      sampleSize.setValue(1.0);
-      sampleSize.setErrorMessage("Please specify a valid number of replicates");
-    }
-
-    void setAvailableVariableLevels(Collection<VariableLevel> availableVariableLevels) {
-      condition.setItems(availableVariableLevels);
-    }
-
-    void setCondition(Collection<VariableLevel> condition) {
-      this.condition.setValue(condition);
-    }
-
-    void setSampleSize(int size) {
-      sampleSize.setValue((double) size);
-    }
-
-    void registerForRemoveEvents(ComponentEventListener<RemoveEvent> listener) {
-      removeEventListeners.add(listener);
-    }
-
-    private void notifyListenersAboutRemove() {
-      var removeEvent = new RemoveEvent(this, true);
-      removeEventListeners.forEach(listener -> listener.onComponentEvent(removeEvent));
-    }
-  }
-
-  class RemoveEvent extends ComponentEvent<ExperimentalGroupEntry> {
-
-    @Serial
-    private static final long serialVersionUID = 2934203596212238997L;
-
-    /**
-     * Creates a new event using the given source and indicator whether the event originated from
-     * the client side or the server side.
-     *
-     * @param source     the source component
-     * @param fromClient <code>true</code> if the event originated from the client
-     *                   side, <code>false</code> otherwise
-     */
-    public RemoveEvent(ExperimentalGroupEntry source, boolean fromClient) {
-      super(source, fromClient);
-    }
-  }
 }
