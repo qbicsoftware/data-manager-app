6d1379a26ffd918eda01bebc84c4c125ec1779cc
Tobias Koch <kochtobi@users.noreply.github.com>
2023-08-14 13:10:51 +0200

    create the dialog

--- vaadinfrontend/src/main/java/life/qbic/datamanager/views/projects/project/experiments/experiment/components/ExperimentalGroupsExistDialog.java
+++ vaadinfrontend/src/main/java/life/qbic/datamanager/views/projects/project/experiments/experiment/components/ExperimentalGroupsExistDialog.java
@@ -2,6 +2,10 @@ package life.qbic.datamanager.views.projects.project.experiments.experiment.comp
 
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import life.qbic.datamanager.views.general.DialogWindow;
 
 /**
@@ -17,8 +21,28 @@ public class ExperimentalGroupsExistDialog extends DialogWindow {
 
   public ExperimentalGroupsExistDialog() {
     addClassName("experimental-groups-exist-dialog");
-    Div content = new Div(new Text("Hello World!"));
+    removeClassName("dialog-window"); //FIXME
+
+    Div content = new Div();
+    content.add(
+        new Div(new Text(
+            "Editing experimental variables requires all experimental groups to be deleted.")),
+        new Div(new Text("You have "), new Span("X"), new Text(" experimental groups.")),
+        new Div(new Text("Please delete them to edit the variables.")));
     content.addClassName("content");
+
+    H2 title = new H2("Cannot edit variables");
+    title.addClassName("title");
+
+    Icon errorIcon = new Icon(VaadinIcon.CLOSE_CIRCLE);
+    errorIcon.setClassName("error-icon");
+
+    setConfirmButtonLabel("Go to Experimental Groups");
+    setCancelButtonLabel("Cancel");
+
+    getHeader().add(errorIcon, title);
+    getFooter().add(cancelButton, confirmButton);
+
     add(content);
   }
 }
