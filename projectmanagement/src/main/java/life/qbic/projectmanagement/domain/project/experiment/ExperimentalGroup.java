33fa05d01c1c6fd12f58b599a534a6a0a6072b2d
Steffengreiner <Steffen.Greiner@gmx.de>
2023-08-29 13:05:20 +0200

    Fall back to default generationtype AUTO strategy and remove postfilter on sampleId since it's not implemented yet

--- projectmanagement/src/main/java/life/qbic/projectmanagement/domain/project/experiment/ExperimentalGroup.java
+++ projectmanagement/src/main/java/life/qbic/projectmanagement/domain/project/experiment/ExperimentalGroup.java
@@ -3,7 +3,6 @@ package life.qbic.projectmanagement.domain.project.experiment;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
@@ -29,7 +28,7 @@ public class ExperimentalGroup {
   private List<BiologicalReplicate> biologicalReplicates;
   private Condition condition;
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue
   private Long experimentalGroupId;
   private int sampleSize;
 
