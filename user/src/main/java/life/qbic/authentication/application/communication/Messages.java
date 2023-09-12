c38974fe09dd9c791a561048497f9a710694a55c
Sven Fillinger <sven.fillinger@mailbox.org>
2023-09-12 15:26:36 +0200

    Refactor auth related notifications

---
+++ user/src/main/java/life/qbic/authentication/application/communication/Messages.java
@@ -0,0 +1,43 @@
+package life.qbic.authentication.application.communication;
+
+import life.qbic.domain.concepts.communication.Email;
+import life.qbic.domain.concepts.communication.Recipient;
+
+/**
+ * Creates registration messages.
+ */
+public class Messages {
+
+  private Messages() {
+  }
+
+  public static String formatRegistrationEmailContent(String fullName, String confirmationLink) {
+    return String.format("""
+        Dear %s,
+                
+        Thank you for registering to QBiCs Data Management Portal.
+                
+        Please click the link below to confirm your registration:
+                
+        %s
+                
+        Need help? Contact us for further questions at support@qbic.zendesk.com
+        """, fullName, confirmationLink);
+  }
+
+  public static String formatPasswordResetEmailContent(String fullName, String passwordResetLink) {
+    return String.format("""
+        Dear %s,
+                
+        we have received a password reset request. If you did not issue the reset, 
+        please contact us asap at support@qbic.zendesk.com.
+                
+        Please click the link below to set your new password:
+                
+        %s
+                
+        Need help? Contact us for further questions at support@qbic.zendesk.com
+        """, fullName, passwordResetLink);
+  }
+
+}
--- user/src/main/java/life/qbic/authentication/application/user/policy/WhenPasswordResetNotifyUser.java
+++ user/src/main/java/life/qbic/authentication/application/user/policy/WhenPasswordResetNotifyUser.java
@@ -3,7 +3,7 @@ package life.qbic.authentication.application.user.policy;
 import life.qbic.authentication.application.communication.Messages;
 import life.qbic.authentication.domain.user.concept.User;
 import life.qbic.authentication.domain.user.concept.UserId;
-import life.qbic.authentication.domain.user.event.UserRegistered;
+import life.qbic.authentication.domain.user.event.PasswordResetRequested;
 import life.qbic.authentication.domain.user.repository.UserRepository;
 import life.qbic.domain.concepts.DomainEvent;
 import life.qbic.domain.concepts.DomainEventDispatcher;
@@ -18,52 +18,55 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
- * When a user registered, email the user a confirmation link to the provided email address.
+ * When a password reset request reset event occurred, email the user with a link to reset their
+ * password.
  *
  * @since 1.0.0
  */
 @Component
-public class WhenUserRegisteredSendConfirmationEmail implements
-    DomainEventSubscriber<UserRegistered> {
+public class WhenPasswordResetNotifyUser implements
+    DomainEventSubscriber<PasswordResetRequested> {
 
   private final CommunicationService communicationService;
 
   private final JobScheduler jobScheduler;
-  private final UserRepository userRepository;
-  private final EmailConfirmationLinkSupplier emailConfirmationLinkSupplier;
 
-  public WhenUserRegisteredSendConfirmationEmail(
-      @Autowired CommunicationService communicationService,
-      @Autowired JobScheduler jobScheduler, @Autowired UserRepository userRepository,
-      @Autowired EmailConfirmationLinkSupplier emailConfirmationLinkSupplier) {
+  private final UserRepository userRepository;
+  private final PasswordResetLinkSupplier passwordResetLinkSupplier;
+
+  public WhenPasswordResetNotifyUser(@Autowired CommunicationService communicationService,
+      @Autowired UserRepository userRepository,
+      @Autowired JobScheduler jobScheduler,
+      @Autowired PasswordResetLinkSupplier passwordResetLinkSupplier) {
     this.communicationService = communicationService;
-    this.userRepository = userRepository;
-    this.emailConfirmationLinkSupplier = emailConfirmationLinkSupplier;
-    DomainEventDispatcher.instance().subscribe(this);
     this.jobScheduler = jobScheduler;
+    this.userRepository = userRepository;
+    this.passwordResetLinkSupplier = passwordResetLinkSupplier;
+    DomainEventDispatcher.instance().subscribe(this);
   }
 
   @Override
   public Class<? extends DomainEvent> subscribedToEventType() {
-    return UserRegistered.class;
+    return PasswordResetRequested.class;
   }
 
   @Override
-  public void handleEvent(UserRegistered event) {
-    this.jobScheduler.enqueue(() -> notifyUserWithConfirmationInstructions(event.userId()));
+  public void handleEvent(PasswordResetRequested event) {
+    jobScheduler.enqueue(
+        () -> notifyUserWithInstructions(event.userId().get()));
   }
 
-  @Job(name = "Notify user with account confirmation instructions")
-  public void notifyUserWithConfirmationInstructions(String userId) {
+  @Job(name = "Notify user with password reset instructions")
+  public void notifyUserWithInstructions(String userId) {
     userRepository.findById(UserId.from(userId)).ifPresentOrElse(this::notifyUser, () -> {
       throw new RuntimeException("User with id %s not found".formatted(userId));
     });
   }
 
   private void notifyUser(User user) {
-    communicationService.send(new Subject("Please confirm your email address"),
+    communicationService.send(new Subject("Please reset your password"),
         new Recipient(user.emailAddress().get(), user.fullName().get()),
-        new Content(Messages.formatRegistrationEmailContent(user.fullName().get(),
-            emailConfirmationLinkSupplier.emailConfirmationUrl(user.id().get()))));
+        new Content(Messages.formatPasswordResetEmailContent(user.fullName().get(),
+            passwordResetLinkSupplier.passwordResetUrl(user.id().get()))));
   }
 }
