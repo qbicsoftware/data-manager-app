cb39c4de3c3c688bb29a8a94ce9208cd13fd4923
Steffengreiner <Steffen.Greiner@gmx.de>
2023-10-09 14:41:08 +0200

    Refactor component to make method purposes more clear and adhere to SRP

--- vaadinfrontend/src/main/java/life/qbic/datamanager/views/navigation/ProjectSideNavigationComponent.java
+++ vaadinfrontend/src/main/java/life/qbic/datamanager/views/navigation/ProjectSideNavigationComponent.java
@@ -10,7 +10,6 @@ import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.menubar.MenuBar;
-import com.vaadin.flow.component.sidenav.SideNav;
 import com.vaadin.flow.component.sidenav.SideNavItem;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
@@ -23,6 +22,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import life.qbic.datamanager.views.AppRoutes.Projects;
 import life.qbic.datamanager.views.Context;
 import life.qbic.datamanager.views.projects.overview.ProjectOverviewPage;
@@ -54,16 +54,12 @@ public class ProjectSideNavigationComponent extends Div implements
     BeforeEnterObserver {
 
   private static final Logger log = getLogger(ProjectSideNavigationComponent.class);
-  private final Div projectSection = new Div();
-  private final SideNavItem projectSectionHeader = new SideNavItem("");
-  private final Div experimentSection = new Div();
-  private final SideNavItem experimentSectionHeader = new SideNavItem("");
+  private final Div content = new Div();
   private final transient ProjectInformationService projectInformationService;
   private final transient ExperimentInformationService experimentInformationService;
   public static final String PROJECT_ID_ROUTE_PARAMETER = "projectId";
   public static final String EXPERIMENT_ID_ROUTE_PARAMETER = "experimentId";
   private transient Context context = new Context();
-  private final MenuBar projectSelectMenu = new MenuBar();
 
   public ProjectSideNavigationComponent(
       @Autowired ProjectInformationService projectInformationService,
@@ -73,9 +69,8 @@ public class ProjectSideNavigationComponent extends Div implements
     this.addClassName("project-navigation-drawer");
     this.projectInformationService = projectInformationService;
     this.experimentInformationService = experimentInformationService;
-    projectSection.addClassName("project-section");
-    experimentSection.addClassName("experiment-section");
-    add(projectSection, experimentSection);
+    content.addClassName("content");
+    add(content);
     log.debug(
         "New instance for ProjectSideNavigationComponent {} was created",
         System.identityHashCode(this));
@@ -83,75 +78,91 @@ public class ProjectSideNavigationComponent extends Div implements
 
   @Override
   public void beforeEnter(BeforeEnterEvent beforeEnterEvent) {
-    resetSections();
+    resetContent();
     String projectId = beforeEnterEvent.getRouteParameters()
         .get(PROJECT_ID_ROUTE_PARAMETER).orElseThrow();
     ProjectId parsedProjectId = ProjectId.parse(projectId);
     this.context = new Context().with(parsedProjectId);
     beforeEnterEvent.getRouteParameters().get(EXPERIMENT_ID_ROUTE_PARAMETER)
         .ifPresent(experimentId -> this.context = context.with(ExperimentId.parse(experimentId)));
-    setContext();
+    loadProjectInformation();
   }
 
-  private void setContext() {
-    initializeProjectSection(context.projectId().orElseThrow());
-    initializeExperimentSection(context.projectId().orElseThrow());
+  private void loadProjectInformation() {
+    Project project = loadProjectFromId();
+    List<Experiment> experiments = loadExperimentsForProject(project);
+    ProjectPreview projectPreview = generatePreviewFromProject(project);
+    generateNavBarContent(projectPreview, experiments);
   }
 
-  private void initializeProjectSection(ProjectId projectId) {
-    projectSectionHeader.setLabel("PROJECT");
-    projectSectionHeader.setPrefixComponent(VaadinIcon.NOTEBOOK.create());
-    projectSectionHeader.addClassName("primary");
-    SideNav projectSideNav = new SideNav();
-    projectSideNav.addClassName("project-side-nav");
-    String projectInformationPath = String.format(Projects.PROJECT_INFO, projectId.value());
-    String projectAccessPath = String.format(Projects.ACCESS, projectId.value());
-    SideNavItem projectInformationItem = new SideNavItem("PROJECT INFORMATION",
-        projectInformationPath, VaadinIcon.DEINDENT.create());
-    SideNavItem projectAccessItem = new SideNavItem("PROJECT ACCESS MANAGEMENT", projectAccessPath,
-        VaadinIcon.USERS.create());
-    projectSideNav.addItem(projectInformationItem);
-    projectSideNav.addItem(projectAccessItem);
-    projectSection.addComponentAsFirst(projectSectionHeader);
-    MenuBar projectSelect = createProjectSelect(projectId);
-    projectSection.add(projectSelect, generateLineDivider(), projectSideNav);
+  private Project loadProjectFromId() {
+    return projectInformationService.find(context.projectId().orElseThrow()).orElseThrow();
   }
 
-  private MenuBar createProjectSelect(ProjectId projectId) {
-    projectSelectMenu.removeAll();
-    projectSelectMenu.addClassNames("project-select-menu");
-    String projectTitle = retrieveCurrentlySelectedProject(projectId).projectTitle();
-    Span currentlySelectedProject = new Span(projectTitle);
-    currentlySelectedProject.addClassName("selected-project-title");
-    Icon dropdownIcon = VaadinIcon.CHEVRON_DOWN_SMALL.create();
-    dropdownIcon.addClassName(IconSize.SMALL);
-    Span selectedProject = new Span(currentlySelectedProject, dropdownIcon);
-    selectedProject.addClassName("selected-project");
-    MenuItem item = projectSelectMenu.addItem(selectedProject, projectTitle);
-    generateProjectMenuOptions(item);
-    return projectSelectMenu;
+  private List<Experiment> loadExperimentsForProject(Project project) {
+    return project.experiments().stream()
+        .map(experimentInformationService::find).filter(Optional::isPresent).map(Optional::get)
+        .toList();
   }
 
-  private ProjectPreview retrieveCurrentlySelectedProject(ProjectId projectId) {
-    Project project = projectInformationService.find(projectId).orElseThrow();
+  private ProjectPreview generatePreviewFromProject(Project project) {
     return projectInformationService.queryPreview(project.getProjectIntent().projectTitle().title(),
         0, 1, new ArrayList<>()).stream().findFirst().orElseThrow();
   }
 
-  private void generateProjectMenuOptions(MenuItem menuItem) {
-    SubMenu subMenu = menuItem.getSubMenu();
+  private void generateNavBarContent(ProjectPreview preview, List<Experiment> experiments) {
+    Div projectSection = createProjectSection(preview.projectTitle());
+    Div experimentSection = createExperimentSection(experiments);
+    content.add(projectSection, experimentSection);
+  }
+
+  private Div createProjectSection(String projectTitle) {
+    Div projectSection = new Div();
+    projectSection.add(createProjectHeader(), createProjectSelection(projectTitle),
+        generateSectionDivider(), createProjectItems());
+    projectSection.addClassName("project-section");
+    return projectSection;
+  }
+
+  private SideNavItem createProjectHeader() {
+    SideNavItem projectHeader = new SideNavItem("PROJECT");
+    projectHeader.setLabel("PROJECT");
+    projectHeader.setPrefixComponent(VaadinIcon.NOTEBOOK.create());
+    projectHeader.addClassName("primary");
+    return projectHeader;
+  }
+
+  private MenuBar createProjectSelection(String projectTitle) {
+    MenuBar projectSelection = new MenuBar();
+    projectSelection.addClassNames("project-selection-menu");
+    Span selectedProjectTitle = new Span(projectTitle);
+    selectedProjectTitle.addClassName("selected-project-title");
+    Icon dropdownIcon = VaadinIcon.CHEVRON_DOWN_SMALL.create();
+    dropdownIcon.addClassName(IconSize.SMALL);
+    Span dropDownField = new Span(selectedProjectTitle, dropdownIcon);
+    dropDownField.addClassName("dropdown-field");
+    MenuItem item = projectSelection.addItem(dropDownField, projectTitle);
+    addSelectionOptions(item);
+    return projectSelection;
+  }
+
+  private void addSelectionOptions(MenuItem menuItem) {
+    SubMenu projectSelectionSubMenu = menuItem.getSubMenu();
     Span recentProjectsHeader = new Span("Recent Projects");
     recentProjectsHeader.addClassName("recent-projects-header");
     Span projectOverviewRouteComponent = new Span("Go To Projects");
-    subMenu.addItem(projectOverviewRouteComponent, event -> routeToProjectOverview());
-    subMenu.add(generateLineDivider());
-    subMenu.add(recentProjectsHeader);
+    projectSelectionSubMenu.addItem(projectOverviewRouteComponent,
+        event -> routeToProjectOverview());
+    projectSelectionSubMenu.add(generateSectionDivider());
+    projectSelectionSubMenu.add(recentProjectsHeader);
     retrieveLastModifiedProjects().forEach(
-        preview -> {
-          MenuItem recentProject = subMenu.addItem(String.format("%s - %s", preview.projectCode(),
-              preview.projectTitle()), event -> routeToProject(preview.projectId()));
-          recentProject.addClassName("transparent-icon");
-        });
+        preview -> addRecentProjectItem(projectSelectionSubMenu, preview));
+  }
+
+  private void addRecentProjectItem(SubMenu subMenu, ProjectPreview preview) {
+    MenuItem recentProject = subMenu.addItem(String.format("%s - %s", preview.projectCode(),
+        preview.projectTitle()), event -> routeToProject(preview.projectId()));
+    recentProject.addClassName("transparent-icon");
   }
 
   private List<ProjectPreview> retrieveLastModifiedProjects() {
@@ -160,36 +171,56 @@ public class ProjectSideNavigationComponent extends Div implements
     return projectInformationService.queryPreview("", 0, 3, sortOrders);
   }
 
-  private void initializeExperimentSection(ProjectId projectId) {
-    SideNav experimentSideNav = new SideNav();
-    experimentSideNav.addClassName("experiment-side-nav");
-    experimentSectionHeader.removeAll();
-    experimentSectionHeader.setLabel("EXPERIMENTS");
-    experimentSectionHeader.setPrefixComponent(VaadinIcon.FLASK.create());
-    experimentSectionHeader.addClassName("primary");
-    List<Experiment> experiments = experimentInformationService.findAllForProject(projectId);
-    experiments.forEach(
-        experiment -> experimentSectionHeader.addItem(createExperimentItem(projectId, experiment)));
-    experimentSideNav.addItem(experimentSectionHeader);
-    experimentSection.addComponentAsFirst(experimentSideNav);
-    experimentSectionHeader.setExpanded(true);
+  private Div createProjectItems() {
+    Div projectItems = new Div();
+    projectItems.add(createProjectSummary(), createProjectUsers());
+    projectItems.addClassName("project-items");
+    return projectItems;
   }
 
-  private SideNavItem createExperimentItem(ProjectId projectId, Experiment experiment) {
-    String experimentPath = String.format(Projects.EXPERIMENT, projectId.value(),
+  private SideNavItem createProjectSummary() {
+    String projectSummaryPath = String.format(Projects.PROJECT_INFO,
+        context.projectId().orElseThrow());
+    return new SideNavItem("SUMMARY",
+        projectSummaryPath, VaadinIcon.DEINDENT.create());
+  }
+
+  private SideNavItem createProjectUsers() {
+    String projectUsersPath = String.format(Projects.ACCESS, context.projectId().orElseThrow());
+    return new SideNavItem("USERS", projectUsersPath,
+        VaadinIcon.USERS.create());
+  }
+
+  private Div createExperimentSection(List<Experiment> experimentsList) {
+    Div experimentSection = new Div();
+    SideNavItem experiments = new SideNavItem("");
+    experiments.setLabel("EXPERIMENTS");
+    experiments.setPrefixComponent(VaadinIcon.FLASK.create());
+    experimentsList.forEach(
+        experiment -> experiments.addItem(createItemFromExperiment(experiment)));
+    experiments.setExpanded(true);
+    experiments.addClassName("experiment-section");
+    experiments.addClassName("primary");
+    experimentSection.add(experiments);
+    return experimentSection;
+  }
+
+  private SideNavItem createItemFromExperiment(Experiment experiment) {
+    String experimentPath = String.format(Projects.EXPERIMENT, context.projectId().orElseThrow(),
         experiment.experimentId().value());
     SideNavItem sideNavItem = new SideNavItem(experiment.getName(), experimentPath);
     sideNavItem.addClassName("hoverable");
     return sideNavItem;
   }
 
-  private Hr generateLineDivider() {
-    return new Hr();
+  private Span generateSectionDivider() {
+    Span sectionDivider = new Span(new Hr());
+    sectionDivider.addClassName("section-divider");
+    return sectionDivider;
   }
 
-  private void resetSections() {
-    projectSection.removeAll();
-    experimentSection.removeAll();
+  private void resetContent() {
+    content.removeAll();
   }
 
   private void routeToProject(ProjectId projectId) {
