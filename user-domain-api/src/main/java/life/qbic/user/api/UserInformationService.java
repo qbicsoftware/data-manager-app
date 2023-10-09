b7aca05c2aa9d16f4032972ae143477a8e5f6635
Sven Fillinger <sven.fillinger@mailbox.org>
2023-10-09 11:02:32 +0200

    Provide jds

--- user-domain-api/src/main/java/life/qbic/user/api/UserInformationService.java
+++ user-domain-api/src/main/java/life/qbic/user/api/UserInformationService.java
@@ -3,16 +3,34 @@ package life.qbic.user.api;
 import java.util.Optional;
 
 /**
- * <b><interface short description - 1 Line!></b>
+ * <b>User information service</b>
  *
- * <p><More detailed description - When to use, what it solves, etc.></p>
+ * <p>This Java interface serves as an OHS offering clients from other domains
+ * to query user information, without exposing any internal domain model details outside the context
+ * boundary of the user domain.</p>
  *
- * @since <version tag>
+ * @since 1.0.0
  */
 public interface UserInformationService {
 
+  /**
+   * Looks up a user based on a given email address. Since the email address is always beloning to
+   * one user and one user only, the search result is either successful or empty.
+   *
+   * @param emailAddress the email address of a user to lookup
+   * @return user information if a user with the given email address exists, else empty
+   * @since 1.0.0
+   */
   Optional<UserInfo> findByEmail(String emailAddress);
 
+  /**
+   * Looks up a user by their user id. The user id is guaranteed to be unique, so either it is
+   * successful or empty.
+   *
+   * @param userId the user id to look up the corresponding user entry
+   * @return user information if a user with the given email exists, else empty
+   * @since 1.0.0
+   */
   Optional<UserInfo> findById(String userId);
 
 }
