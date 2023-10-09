fd55a0489efab3f4160a447dbd260996cf9444d4
Sven F <9976560+sven1103@users.noreply.github.com>
2023-10-09 13:22:26 +0200

    Refactor modules to match domain boundaries (#397)
    More clearer separation of concerns and context boundaries of 
    project management and user domain.
    ---------
    Co-authored-by: steffengreiner <steffen.greiner@qbic.uni-tuebingen.de>
    Co-authored-by: wow-such-code <andreas.friedrich@uni-tuebingen.de>

---
+++ projectmanagement-domain/src/main/java/life/qbic/projectmanagement/application/authorization/authorities/aspects/CanCreateProject.java
@@ -0,0 +1,11 @@
+package life.qbic.projectmanagement.application.authorization.authorities.aspects;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+/**
+ * Limits the activity to users with authority to create a project
+ */
+@PreAuthorize("hasAuthority('project:create')")
+public @interface CanCreateProject {
+
+}
