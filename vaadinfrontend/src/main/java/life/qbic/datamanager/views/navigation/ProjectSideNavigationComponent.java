05a8df72765b9f59f595bd7abc1c657a157c76b1
Steffengreiner <Steffen.Greiner@gmx.de>
2023-10-09 18:46:38 +0200

    Add Navigation Event triggering to reduce nesting

--- vaadinfrontend/src/main/java/life/qbic/datamanager/views/navigation/ProjectSideNavigationComponent.java
+++ vaadinfrontend/src/main/java/life/qbic/datamanager/views/navigation/ProjectSideNavigationComponent.java
@@ -3,6 +3,8 @@ package life.qbic.datamanager.views.navigation;
 import static org.slf4j.LoggerFactory.getLogger;
 
 import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ComponentEvent;
 import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.contextmenu.MenuItem;
 import com.vaadin.flow.component.contextmenu.SubMenu;
@@ -20,7 +22,7 @@ import com.vaadin.flow.router.RouteParameters;
 import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
 import com.vaadin.flow.theme.lumo.LumoUtility.IconSize;
-import java.util.ArrayList;
+import java.io.Serial;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
@@ -85,11 +87,6 @@ public class ProjectSideNavigationComponent extends Div implements
         .toList();
   }
 
-  private static ProjectPreview generatePreviewFromProject(Project project) {
-    return projectInformationService.queryPreview(project.getProjectIntent().projectTitle().title(),
-        0, 1, new ArrayList<>()).stream().findFirst().orElseThrow();
-  }
-
   private static SideNavItem createProjectHeader() {
     SideNavItem projectHeader = new SideNavItem("PROJECT");
     projectHeader.setLabel("PROJECT");
@@ -118,7 +115,7 @@ public class ProjectSideNavigationComponent extends Div implements
     return projectInformationService.find(id).orElseThrow();
   }
 
-  private static Div createProjectSection(String projectId, String projectTitle) {
+  private Div createProjectSection(String projectId, String projectTitle) {
     Div projectSection = new Div();
     projectSection.add(createProjectHeader(), createProjectSelection(projectTitle),
         generateSectionDivider(), createProjectItems(projectId));
@@ -171,7 +168,8 @@ public class ProjectSideNavigationComponent extends Div implements
     return sideNavItem;
   }
 
-  private static MenuBar createProjectSelection(String projectTitle) {
+  //ToDo Make static and move NavigationEventTriggering to director class
+  private MenuBar createProjectSelection(String projectTitle) {
     MenuBar projectSelection = new MenuBar();
     projectSelection.addClassNames("project-selection-menu");
     Span selectedProjectTitle = new Span(projectTitle);
@@ -185,30 +183,34 @@ public class ProjectSideNavigationComponent extends Div implements
     return projectSelection;
   }
 
-  private static void addSelectionOptions(MenuItem menuItem) {
+  //ToDo Make static and move NavigationEventTriggering to director class
+  private void addSelectionOptions(MenuItem menuItem) {
     SubMenu projectSelectionSubMenu = menuItem.getSubMenu();
     Span recentProjectsHeader = new Span("Recent Projects");
     recentProjectsHeader.addClassName("recent-projects-header");
     Span projectOverviewRouteComponent = new Span("Go To Projects");
     projectSelectionSubMenu.addItem(projectOverviewRouteComponent,
-        event -> routeToProjectOverview());
+        event -> fireNavigationEvent(event.getSource(), Optional.empty(), event.isFromClient()));
     projectSelectionSubMenu.add(generateSectionDivider());
     projectSelectionSubMenu.add(recentProjectsHeader);
     retrieveLastModifiedProjects().forEach(
         preview -> addRecentProjectItem(projectSelectionSubMenu, preview));
   }
 
+  //ToDo Make static and move NavigationEventTriggering to director class
   private void addRecentProjectItem(SubMenu subMenu, ProjectPreview preview) {
     MenuItem recentProject = subMenu.addItem(String.format("%s - %s", preview.projectCode(),
             preview.projectTitle()),
-        (ComponentEventListener<ClickEvent<MenuItem>>) menuItemClickEvent -> ProjectSideNavigationComponent::this::routeToProject));
+        (ComponentEventListener<ClickEvent<MenuItem>>) menuItemClickEvent -> fireNavigationEvent(
+            menuItemClickEvent.getSource(), Optional.of(preview.projectId()),
+            menuItemClickEvent.isFromClient()));
     recentProject.addClassName("transparent-icon");
   }
 
-  private void routeToProject() {
+  private void routeToProject(ProjectId projectId) {
     RouteParameters routeParameters = new RouteParameters(
-        new RouteParam(PROJECT_ID_ROUTE_PARAMETER, context.projectId);
-    currentUI.navigate(ProjectInformationMain.class, routeParameters);
+        new RouteParam(PROJECT_ID_ROUTE_PARAMETER, projectId.value()));
+    getUI().ifPresent(ui -> ui.navigate(ProjectInformationMain.class, routeParameters));
     log.debug("Routing to ProjectInformation page for project " + projectId.value());
   }
 
@@ -223,14 +225,53 @@ public class ProjectSideNavigationComponent extends Div implements
         .ifPresent(experimentId -> context = context.with(ExperimentId.parse(experimentId)));
     var project = loadProject(parsedProjectId);
     List<Experiment> experiments = loadExperimentsForProject(project);
+    //Todo generate director class handling building of each component section
     content.add(
         createProjectSection(parsedProjectId.value(),
             project.getProjectIntent().projectTitle().title()),
         createExperimentSection(parsedProjectId.value(), experiments));
+    addListener(ProjectNavigationEvent.class, this::addNavigationListener);
+  }
+
+  private void fireNavigationEvent(Component component, Optional<ProjectId> projectId,
+      boolean fromClient) {
+    var projectNavigationEvent = new ProjectNavigationEvent(component, projectId, fromClient);
+    fireEvent(projectNavigationEvent);
+  }
+
+  private void addNavigationListener(ProjectNavigationEvent projectNavigationEvent) {
+    projectNavigationEvent.projectId().ifPresentOrElse(this::routeToProject,
+        this::routeToProjectOverview);
   }
 
   private void routeToProjectOverview() {
     getUI().ifPresent(ui -> ui.navigate(ProjectOverviewPage.class));
     log.debug("Routing to ProjectOverview page");
+  }
+
+  private static class ProjectNavigationEvent extends ComponentEvent<Component> {
+
+    @Serial
+    private static final long serialVersionUID = 7399764169934605506L;
+    private final Optional<ProjectId> projectId;
+
+    /**
+     * Creates a new event using the given source and indicator whether the event originated from
+     * the client side or the server side.
+     *
+     * @param source     the source component
+     * @param projectId  the {@link ProjectId} of the project to be navigated
+     * @param fromClient <code>true</code> if the event originated from the client
+     *                   side, <code>false</code> otherwise
+     */
+    public ProjectNavigationEvent(Component source, Optional<ProjectId> projectId,
+        boolean fromClient) {
+      super(source, fromClient);
+      this.projectId = projectId;
+    }
+
+    Optional<ProjectId> projectId() {
+      return projectId;
+    }
   }
 }
