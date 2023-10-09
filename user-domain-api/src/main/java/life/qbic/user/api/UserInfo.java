2a2e291867e48b61b012b025e7787b4c7ebe37fb
Sven Fillinger <sven.fillinger@mailbox.org>
2023-10-09 10:54:10 +0200

    Provide JD

--- user-domain-api/src/main/java/life/qbic/user/api/UserInfo.java
+++ user-domain-api/src/main/java/life/qbic/user/api/UserInfo.java
@@ -1,11 +1,11 @@
 package life.qbic.user.api;
 
 /**
- * <b><record short description - 1 Line!></b>
+ * <b>User Information DTO</b>
  *
- * <p><More detailed description - When to use, what it solves, etc.></p>
+ * <p>DTO to exchange user information between the user domain and the client calling a service</p>
  *
- * @since <version tag>
+ * @since 1.0.0
  */
 public record UserInfo(String id, String fullName, String emailAddress, String encryptedPassword, boolean isActive) {
 
