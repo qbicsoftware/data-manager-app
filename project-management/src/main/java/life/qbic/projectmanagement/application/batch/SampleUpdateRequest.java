0526b1eb0fd7e8ba0363bff7799a75b163139553
Steffengreiner <Steffen.Greiner@gmx.de>
2023-11-28 10:53:06 +0100

    Add JD to SampleUpdateRequest
    Co-authored-by: KochTobi <tobias.koch@qbic.uni-tuebingen.de>

--- project-management/src/main/java/life/qbic/projectmanagement/application/batch/SampleUpdateRequest.java
+++ project-management/src/main/java/life/qbic/projectmanagement/application/batch/SampleUpdateRequest.java
@@ -9,15 +9,26 @@ import life.qbic.projectmanagement.domain.model.sample.AnalysisMethod;
 import life.qbic.projectmanagement.domain.model.sample.SampleId;
 
 /**
- * <class short description - One Line!>
- *
- * <More detailed description - When to use, what it solves, etc.>
- *
- * @since <version tag>
- *
+ * Sample update request.
+ * <p>
+ * Serves as a parameter object for sample update.
+ * @param sampleId            the {@link SampleId} of the sample to be updated
+ * @param sampleInformation   the {@link SampleInformation} containing the content of the sample to be updated
  */
 public record SampleUpdateRequest(SampleId sampleId, SampleInformation sampleInformation) {
 
+  /**
+   * Sample update request.
+   * <p>
+   * @param sampleLabel          a human-readable semantic descriptor of the sample
+   * @param analysisMethod       analysis method to be performed
+   * @param biologicalReplicate  the biological replicate the sample has been taken from
+   * @param experimentalGroup    the experimental group the sample is part of
+   * @param species              the species the sample belongs to
+   * @param specimen             the specimen the sample belongs to.
+   * @param analyte              the analyte the sample belongs to
+   * @param comment              comment relating to the sample
+   */
   public record SampleInformation(String sampleLabel, AnalysisMethod analysisMethod,
                                   BiologicalReplicate biologicalReplicate,
                                   ExperimentalGroup experimentalGroup, Species species,
