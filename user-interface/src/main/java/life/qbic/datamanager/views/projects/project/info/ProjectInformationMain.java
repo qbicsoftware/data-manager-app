16fba7402e7c00d3b3600ecc85bd7de52d6bddc2
Tobias Koch <kochtobi@users.noreply.github.com>
2023-12-12 11:46:16 +0100

    Move offer list from ProjectInformationMain.java to ProjectSupportComponent.java
    Moved handling of offer related events and logic from `ProjectInformationMain` to `ProjectSupportComponent`. This includes methods for downloading, deleting, and uploading offers as well as refreshing offers. This refactor aims to simplify and clarify the code structure by assigning feature-related code to more relevant classes.
    
    Co-authored-by: wow-such-code <andreas.friedrich@uni-tuebingen.de>

--- user-interface/src/main/java/life/qbic/datamanager/views/projects/project/info/ProjectInformationMain.java
+++ user-interface/src/main/java/life/qbic/datamanager/views/projects/project/info/ProjectInformationMain.java
@@ -14,28 +14,19 @@ import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
 import jakarta.annotation.security.PermitAll;
 import java.io.Serial;
-import java.util.List;
 import life.qbic.application.commons.ApplicationException;
 import life.qbic.application.commons.Result;
 import life.qbic.datamanager.security.UserPermissions;
 import life.qbic.datamanager.views.Context;
 import life.qbic.datamanager.views.general.MainComponent;
-import life.qbic.datamanager.views.general.OfferDownload;
 import life.qbic.datamanager.views.notifications.StyledNotification;
 import life.qbic.datamanager.views.notifications.SuccessMessage;
 import life.qbic.datamanager.views.projects.project.ProjectMainLayout;
 import life.qbic.datamanager.views.projects.project.experiments.ExperimentInformationMain;
 import life.qbic.datamanager.views.projects.project.experiments.experiment.create.ExperimentAddDialog;
-import life.qbic.datamanager.views.projects.project.info.OfferList.DeleteOfferClickEvent;
-import life.qbic.datamanager.views.projects.project.info.OfferList.DownloadOfferClickEvent;
-import life.qbic.datamanager.views.projects.project.info.OfferList.OfferInfo;
-import life.qbic.datamanager.views.projects.project.info.OfferList.UploadOfferClickEvent;
-import life.qbic.datamanager.views.projects.purchase.UploadPurchaseDialog;
 import life.qbic.logging.api.Logger;
 import life.qbic.projectmanagement.application.AddExperimentToProjectService;
 import life.qbic.projectmanagement.application.OntologyTermInformationService;
-import life.qbic.projectmanagement.application.purchase.OfferDTO;
-import life.qbic.projectmanagement.application.purchase.ProjectPurchaseService;
 import life.qbic.projectmanagement.domain.model.experiment.ExperimentId;
 import life.qbic.projectmanagement.domain.model.project.Project;
 import life.qbic.projectmanagement.domain.model.project.ProjectId;
@@ -61,40 +52,30 @@ public class ProjectInformationMain extends MainComponent implements BeforeEnter
   private static final Logger log = logger(ProjectInformationMain.class);
   private final ProjectContentComponent projectContentComponent;
   private final ProjectSupportComponent projectSupportComponent;
-  private final OfferList offerList;
   private final transient AddExperimentToProjectService addExperimentToProjectService;
   private final transient OntologyTermInformationService ontologyTermInformationService;
   private final UserPermissions userPermissions;
   public static final String PROJECT_ID_ROUTE_PARAMETER = "projectId";
   public static final String EXPERIMENT_ID_ROUTE_PARAMETER = "experimentId";
   private Context context;
-  private final OfferDownload offerDownload;
-  private final ProjectPurchaseService projectPurchaseService;
 
   public ProjectInformationMain(
       @Autowired ProjectContentComponent projectContentComponent,
       @Autowired ProjectSupportComponent projectSupportComponent,
       @Autowired UserPermissions userPermissions,
       @Autowired AddExperimentToProjectService addExperimentToProjectService,
-      @Autowired OntologyTermInformationService ontologyTermInformationService,
-      @Autowired ProjectPurchaseService projectPurchaseService) {
+      @Autowired OntologyTermInformationService ontologyTermInformationService) {
     super(projectContentComponent, projectSupportComponent);
     requireNonNull(userPermissions, "userPermissions must not be null");
     requireNonNull(projectContentComponent);
     requireNonNull(projectSupportComponent);
     requireNonNull(addExperimentToProjectService);
     requireNonNull(ontologyTermInformationService);
-    requireNonNull(projectPurchaseService, "projectPurchaseService must not be null");
-    this.projectPurchaseService = projectPurchaseService;
     this.projectContentComponent = projectContentComponent;
     this.projectSupportComponent = projectSupportComponent;
     this.userPermissions = userPermissions;
     this.addExperimentToProjectService = addExperimentToProjectService;
     this.ontologyTermInformationService = ontologyTermInformationService;
-    this.offerDownload = new OfferDownload(
-        (projectId, offerId) -> this.projectPurchaseService.getOfferWithContent(projectId, offerId)
-            .orElseThrow());
-    this.offerList = setupOfferList();
 
     addClassName("project");
     addListeners();
@@ -105,55 +86,6 @@ public class ProjectInformationMain extends MainComponent implements BeforeEnter
         System.identityHashCode(projectContentComponent),
         projectSupportComponent.getClass().getSimpleName(),
         System.identityHashCode(projectSupportComponent)));
-    add(offerDownload, offerList);
-
-  }
-
-  private OfferList setupOfferList() {
-    OfferList component = new OfferList();
-    component.addDeleteOfferClickListener(this::onDeleteOfferClicked);
-    component.addDownloadOfferClickListener(this::onDownloadOfferClicked);
-    component.addUploadOfferClickListener(
-        event -> onUploadOfferClicked(event, projectPurchaseService,
-            context.projectId().orElseThrow().value()));
-    return component;
-  }
-
-  private void onDownloadOfferClicked(DownloadOfferClickEvent downloadOfferClickEvent) {
-    offerDownload.trigger(context.projectId().orElseThrow().value(),
-        downloadOfferClickEvent.offerId());
-  }
-
-  private void onDeleteOfferClicked(DeleteOfferClickEvent deleteOfferClickEvent) {
-    projectPurchaseService.deleteOffer(context.projectId().orElseThrow().value(),
-        deleteOfferClickEvent.offerId());
-    deleteOfferClickEvent.getSource().remove(deleteOfferClickEvent.offerId());
-    offerDownload.removeHref();
-  }
-
-  private void onUploadOfferClicked(UploadOfferClickEvent uploadOfferClickEvent,
-      ProjectPurchaseService projectPurchaseService,
-      String projectId) {
-    UploadPurchaseDialog dialog = new UploadPurchaseDialog();
-    dialog.addConfirmListener(confirmEvent -> {
-      List<OfferDTO> offerDTOs = confirmEvent.getSource().purchaseItems().stream()
-          .map(it -> new OfferDTO(it.signed(), it.fileName(), it.content()))
-          .toList();
-      projectPurchaseService.addPurchases(projectId, offerDTOs);
-      refreshOffers(projectPurchaseService, projectId, uploadOfferClickEvent.getSource());
-      confirmEvent.getSource().close();
-    });
-    dialog.addCancelListener(cancelEvent -> cancelEvent.getSource().close());
-    dialog.open();
-  }
-
-  private static void refreshOffers(ProjectPurchaseService projectPurchaseService, String projectId,
-      OfferList offerList) {
-    List<OfferInfo> offers = projectPurchaseService.linkedOffers(projectId)
-        .stream()
-        .map(offer -> new OfferInfo(offer.id(), offer.getFileName(), offer.isSigned()))
-        .toList();
-    offerList.setOffers(offers);
   }
 
   /**
@@ -181,7 +113,6 @@ public class ProjectInformationMain extends MainComponent implements BeforeEnter
 
   private void setContext(Context context) {
     this.context = context;
-    refreshOffers(projectPurchaseService, context.projectId().orElseThrow().value(), offerList);
     projectContentComponent.setContext(context);
     projectSupportComponent.setContext(context);
   }
