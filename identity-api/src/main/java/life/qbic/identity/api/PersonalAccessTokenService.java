ec192d18c509304dbdbf561ff7eab30a9211d619
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2024-02-08 10:05:13 +0100

    Add identity api for personal access token generation

---
+++ identity-api/src/main/java/life/qbic/identity/api/PersonalAccessTokenService.java
@@ -0,0 +1,33 @@
+package life.qbic.identity.api;
+
+import java.util.Collection;
+
+/**
+ * <b>Personal Access PersonalAccessToken Service</b>
+ *
+ * <p>An identity domain service interface that enables the creation and query of
+ * personal access token for users.</p>
+ *
+ * @since 1.0.0
+ */
+public interface PersonalAccessTokenService {
+
+  /**
+   * Creates a personal access token for a user with the given id.
+   *
+   * @param userId the user id of the user the token shall be created for
+   * @return the raw token after successful creation
+   * @since 1.0.0
+   */
+  RawToken create(String userId) throws UnknownUserIdException;
+
+  /**
+   * Lists all personal access token entries for user with a given id.
+   *
+   * @param userId the user's id the service shall search for existing tokens
+   * @return a collection of personal access token entries associated with a user id
+   * @since 1.0.0
+   */
+  Collection<PersonalAccessToken> find(String userId);
+
+}
