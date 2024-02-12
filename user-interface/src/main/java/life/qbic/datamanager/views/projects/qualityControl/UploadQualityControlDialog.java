29c70a511c92054a0fc0593e73ceca7db814b893
Steffengreiner <Steffen.Greiner@gmx.de>
2024-02-12 12:35:35 +0100

    add minor improvements

--- user-interface/src/main/java/life/qbic/datamanager/views/projects/qualityControl/UploadQualityControlDialog.java
+++ user-interface/src/main/java/life/qbic/datamanager/views/projects/qualityControl/UploadQualityControlDialog.java
@@ -35,7 +35,7 @@ public class UploadQualityControlDialog extends DialogWindow {
 
   private static final Logger log = logger(UploadQualityControlDialog.class);
   private static final String VAADIN_FILENAME_EVENT = "event.detail.file.name";
-  private static final int MAX_FILE_SIZE_BYTES = 1024 * 1024 * 5; // 14 MiB
+  private static final int MAX_FILE_SIZE_BYTES = 1024 * 1024 * 25; // 14 MiB
   @Serial
   private static final long serialVersionUID = 6602134795666762831L;
   private final Upload upload;
