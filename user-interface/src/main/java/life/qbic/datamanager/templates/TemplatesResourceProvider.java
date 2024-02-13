4ed5b99717bb0f42999973ee377ec47775023757
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2024-02-13 10:40:51 +0100

    Add resouce povider

---
+++ user-interface/src/main/java/life/qbic/datamanager/templates/TemplatesResourceProvider.java
@@ -0,0 +1,14 @@
+package life.qbic.datamanager.templates;
+
+/**
+ * <b><class short description - 1 Line!></b>
+ *
+ * <p><More detailed description - When to use, what it solves, etc.></p>
+ *
+ * @since <version tag>
+ */
+public class TemplatesResourceProvider {
+
+
+
+}
