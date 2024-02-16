8f555ee054b97cc6a1d4e7966a72b8eb570e9580
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2024-02-16 15:27:34 +0100

    DM-407 Provide sample measurement information

--- project-management/src/main/java/life/qbic/projectmanagement/application/measurement/validation/NGSValidator.java
+++ project-management/src/main/java/life/qbic/projectmanagement/application/measurement/validation/NGSValidator.java
@@ -1,6 +1,10 @@
 package life.qbic.projectmanagement.application.measurement.validation;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 import life.qbic.projectmanagement.domain.model.measurement.NGSMeasurement;
 
 /**
@@ -12,6 +16,14 @@ import life.qbic.projectmanagement.domain.model.measurement.NGSMeasurement;
  */
 public class NGSValidator implements Validator<NGSMeasurement> {
 
+  private static final Set<String> NGS_PROPERTIES = new HashSet<>();
+
+  static {
+    NGS_PROPERTIES.addAll(Arrays.asList("qbic sample id", "organism id", "facility", "instrument",
+        "sequencing read type", "library kit", "flow cell", "run protocol", "index i5", "index i7",
+        "note"));
+  }
+
   @Override
   public ValidationResult validate(NGSMeasurement measurementMetadata) {
     // TODO implement property validation
@@ -19,6 +31,18 @@ public class NGSValidator implements Validator<NGSMeasurement> {
   }
 
   public boolean isNGS(Collection<String> properties) {
-    return false;
+    if (properties.isEmpty()) {
+      return false;
+    }
+    if (properties.size() != NGS_PROPERTIES.size()) {
+      return false;
+    }
+    for (String ngsProperty : NGS_PROPERTIES) {
+      var propertyFound = properties.stream().filter(property -> Objects.equals(property, ngsProperty)).findAny();
+      if (propertyFound.isEmpty()) {
+        return false;
+      }
+    }
+    return true;
   }
 }
--- project-management/src/test/groovy/life/qbic/projectmanagement/application/measurement/validation/NGSValidatorSpec.groovy
+++ project-management/src/test/groovy/life/qbic/projectmanagement/application/measurement/validation/NGSValidatorSpec.groovy
@@ -6,12 +6,51 @@ class NGSValidatorSpec extends Specification {
 
     def "Given a valid NGS measurement metadata property collection, pass the validation"() {
         given:
-        def validNGSproperties = [""]
+        def validNGSproperties = ["qbic sample id", "organism id", "facility", "instrument", "sequencing read type", "library kit", "flow cell", "run protocol", "index i5", "index i7", "note"]
 
+        when:
+        def ngsValidator = new NGSValidator();
+        def isNGSMetadata = ngsValidator.isNGS(validNGSproperties)
 
+        then:
+        isNGSMetadata
 
+    }
 
+    def "Missing properties for the NGS metadata collection must result in an unsuccessful validation"() {
+        given:
+        def missingProperties= ["organism id", "facility", "instrument", "sequencing read type", "library kit", "flow cell", "run protocol", "index i5", "index i7", "note"]
 
+        when:
+        def ngsValidator = new NGSValidator();
+        def isNGSMetadata = ngsValidator.isNGS(missingProperties)
+
+        then:
+        !isNGSMetadata
+    }
+
+    def "Providing no properties for the NGS metadata collection must result in an unsuccessful validation"() {
+        given:
+        def missingProperties= []
+
+        when:
+        def ngsValidator = new NGSValidator();
+        def isNGSMetadata = ngsValidator.isNGS(missingProperties)
+
+        then:
+        !isNGSMetadata
+    }
+
+    def "A complete property set must be valid no matter the letter casing style"() {
+        given:
+        def chaosCasing= ["QbiC SaMpLe ID", "Organism ID", "FACiLity", "Instrument", "sequencing read type", "library kit", "flow cell", "run protocol", "index i5", "index i7", "note"]
+
+        when:
+        def ngsValidator = new NGSValidator();
+        def isNGSMetadata = ngsValidator.isNGS(chaosCasing)
+
+        then:
+        isNGSMetadata
     }
 
 }
