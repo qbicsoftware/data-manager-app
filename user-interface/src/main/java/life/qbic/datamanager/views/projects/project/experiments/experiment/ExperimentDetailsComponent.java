4b374c69df6e2886541d92be50aeb93d05ef4e2d
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2024-04-30 14:58:34 +0200

    use java's natural order comparator

--- user-interface/src/main/java/life/qbic/datamanager/views/projects/project/experiments/experiment/ExperimentDetailsComponent.java
+++ user-interface/src/main/java/life/qbic/datamanager/views/projects/project/experiments/experiment/ExperimentDetailsComponent.java
@@ -492,8 +492,9 @@ public class ExperimentDetailsComponent extends PageArea {
     // We load the experimental groups of the experiment and render them as cards
     List<ExperimentalGroup> groups = experimentInformationService.experimentalGroupsFor(
         context.experimentId().orElseThrow());
+    Comparator<String> natOrder = Comparator.naturalOrder();
     List<ExperimentalGroupCard> experimentalGroupsCards = groups.stream()
-        .sorted((g1, g2) -> new HumanReadableComparator().compare(g1.name(), g2.name()))
+        .sorted((g1, g2) -> natOrder.compare(g1.name(), g2.name()))
         .map(ExperimentalGroupCard::new)
         .toList();
     experimentalGroupsCollection.setContent(experimentalGroupsCards);
@@ -538,8 +539,9 @@ public class ExperimentDetailsComponent extends PageArea {
     this.experimentalVariables.removeAll();
     // We load the experimental variables of the experiment and render them as cards
     List<ExperimentalVariable> variables = experiment.variables();
+    Comparator<String> natOrder = Comparator.naturalOrder();
     List<ExperimentalVariableCard> experimentalVariableCards = variables.stream()
-        .sorted((var1, var2) -> new HumanReadableComparator().compare(var1.name().value(), var2.name().value()))
+        .sorted((var1, var2) -> natOrder.compare(var1.name().value(), var2.name().value()))
         .map(ExperimentalVariableCard::new).toList();
 
     experimentalVariableCollection.setContent(experimentalVariableCards);
@@ -565,51 +567,5 @@ public class ExperimentDetailsComponent extends PageArea {
     experimentalGroups.removeAll();
     experimentalGroups.add(experimentalGroupsCollection);
   }
-
-
-  //humanly readable order comparator (a1 < a2 < a10)
-  public class HumanReadableComparator implements Comparator<String> {
-
-    @Override
-    public int compare(String a, String b) {
-      int la = a.length();
-      int lb = b.length();
-      int ka = 0;
-      int kb = 0;
-      while (true) {
-        if (ka == la)
-          return kb == lb ? 0 : -1;
-        if (kb == lb)
-          return 1;
-        if (a.charAt(ka) >= '0' && a.charAt(ka) <= '9' && b.charAt(kb) >= '0'
-            && b.charAt(kb) <= '9') {
-          int na = 0;
-          int nb = 0;
-          while (ka < la && a.charAt(ka) == '0')
-            ka++;
-          while (ka + na < la && a.charAt(ka + na) >= '0' && a.charAt(ka + na) <= '9')
-            na++;
-          while (kb < lb && b.charAt(kb) == '0')
-            kb++;
-          while (kb + nb < lb && b.charAt(kb + nb) >= '0' && b.charAt(kb + nb) <= '9')
-            nb++;
-          if (na > nb)
-            return 1;
-          if (nb > na)
-            return -1;
-          if (ka == la)
-            return kb == lb ? 0 : -1;
-          if (kb == lb)
-            return 1;
-
-        }
-        if (a.charAt(ka) != b.charAt(kb))
-          return a.charAt(ka) - b.charAt(kb);
-        ka++;
-        kb++;
-      }
-    }
-  }
-
 
 }
