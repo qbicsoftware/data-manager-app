8d9d4360d044052941183bcb120c789d30f2202b
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2024-06-25 10:52:55 +0200

    Extend existing OidcUserService

--- user-interface/src/main/java/life/qbic/datamanager/security/OidcUserDetailsService.java
+++ user-interface/src/main/java/life/qbic/datamanager/security/OidcUserDetailsService.java
@@ -2,7 +2,6 @@ package life.qbic.datamanager.security;
 
 import static java.util.Objects.requireNonNull;
 
-import java.util.ArrayList;
 import java.util.Optional;
 import life.qbic.identity.api.UserInfo;
 import life.qbic.identity.api.UserInformationService;
@@ -10,10 +9,8 @@ import life.qbic.projectmanagement.application.authorization.QbicOidcUser;
 import life.qbic.projectmanagement.application.authorization.authorities.UserAuthorityProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
-import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Component;
 
@@ -26,7 +23,7 @@ import org.springframework.stereotype.Component;
  * @since <version tag>
  */
 @Component
-public class OidcUserDetailsService implements OAuth2UserService<OidcUserRequest, OidcUser> {
+public class OidcUserDetailsService extends OidcUserService {
 
   private final UserAuthorityProvider userAuthorityProvider;
   private final UserInformationService userInformationService;
@@ -43,16 +40,16 @@ public class OidcUserDetailsService implements OAuth2UserService<OidcUserRequest
 
   @Override
   public OidcUser loadUser(OidcUserRequest userRequest) throws OAuth2AuthenticationException {
-    OidcUserInfo userInfo = new OidcUserInfo(userRequest.getIdToken().getClaims());
-    var defaultOidcUser = new DefaultOidcUser(new ArrayList<>(), userRequest.getIdToken());
+    OidcUser defaultOidcUser = super.loadUser(userRequest);
+
     Optional<UserInfo> localUser = userInformationService.findByOidc(defaultOidcUser.getName(),
         defaultOidcUser.getIssuer().toString());
-
     if (localUser.isPresent()) {
       var user = localUser.get();
       var authorities = userAuthorityProvider.getAuthoritiesByUserId(
           user.id());
-      return new QbicOidcUser(authorities, userRequest.getIdToken(), userInfo, user.id());
+      return new QbicOidcUser(authorities, userRequest.getIdToken(), defaultOidcUser.getUserInfo(),
+          user.id());
     }
     return defaultOidcUser;
   }
