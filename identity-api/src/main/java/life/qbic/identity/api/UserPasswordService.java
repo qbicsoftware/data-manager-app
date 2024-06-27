0e79deefbdcff148e21ac57faae73534e8a8d144
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2024-06-27 10:41:42 +0200

    add javadoc

--- identity-api/src/main/java/life/qbic/identity/api/UserPasswordService.java
+++ identity-api/src/main/java/life/qbic/identity/api/UserPasswordService.java
@@ -3,15 +3,16 @@ package life.qbic.identity.api;
 import java.util.Optional;
 
 /**
- * TODO!
- * <b>short description</b>
+ * A service for retrieving encrypted user passwords.
  *
- * <p>detailed description</p>
- *
- * @since <version tag>
+ * @since 1.2.0
  */
 public interface UserPasswordService {
 
-  public Optional<UserPassword> findForUser(String userId);
+  /**
+   * @param userId the identifier of the user in the datamanager user management
+   * @return the {@link UserPassword} for a user with the given identifier
+   */
+  Optional<UserPassword> findForUser(String userId);
 
 }
