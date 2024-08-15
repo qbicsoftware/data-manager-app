1ae76157a3cf25d1935aa29e59a55d25588436bb
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2024-08-15 08:56:47 +0200

    Add interface for terminology select API

---
+++ project-management/src/main/java/life/qbic/projectmanagement/application/ontology/TerminologySelect.java
@@ -0,0 +1,12 @@
+package life.qbic.projectmanagement.application.ontology;
+
+/**
+ * <b><interface short description - 1 Line!></b>
+ *
+ * <p><More detailed description - When to use, what it solves, etc.></p>
+ *
+ * @since <version tag>
+ */
+public interface TerminologySelect {
+
+}
