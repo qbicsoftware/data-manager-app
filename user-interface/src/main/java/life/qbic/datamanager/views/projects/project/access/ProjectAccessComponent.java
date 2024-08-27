0f83ff46b0e6b3d9eb61b8f7b84171536600917f
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2024-08-27 17:16:46 +0200

    Fix broken ProjectAccessComponent
    Due to the way the grid constructor initializes the grid, the grid is now generated with columns for each bean property.
    As we add the columns ourselves, this leads to every column already being present (duplicate column key) and the ProjectAccessComponent.java not being instantiated.
    Using the no-args constructor of the Grid class solves this issue.

--- user-interface/src/main/java/life/qbic/datamanager/views/projects/project/access/ProjectAccessComponent.java
+++ user-interface/src/main/java/life/qbic/datamanager/views/projects/project/access/ProjectAccessComponent.java
@@ -152,7 +152,7 @@ public class ProjectAccessComponent extends PageArea {
   }
 
   private Grid<ProjectUser> createProjectUserGrid() {
-    Grid<ProjectUser> pUserGrid = new Grid<>(ProjectUser.class);
+    Grid<ProjectUser> pUserGrid = new Grid<>();
     Editor<ProjectUser> editor = pUserGrid.getEditor();
     Binder<ProjectUser> binder = new Binder<>(ProjectUser.class);
     editor.setBinder(binder);
