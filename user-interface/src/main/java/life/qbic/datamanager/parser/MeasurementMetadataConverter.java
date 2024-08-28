d037c4dbc8d5a208f7e9a2b21d6bbbb9108fd478
Sven F. <9976560+sven1103@users.noreply.github.com>
2024-08-28 09:15:24 +0200

    Support xlsx upload for measurement metadata registration (#803)
    Adds XLSX file upload support for measurement metadata registration.
    
    Solves #677
    
    Co-authored-by: KochTobi <tobias.koch@qbic.uni-tuebingen.de>
    (cherry picked from commit 5de2e988344384d06534b754a556dae706aab9c7)

---
+++ user-interface/src/main/java/life/qbic/datamanager/parser/MeasurementMetadataConverter.java
@@ -0,0 +1,52 @@
+package life.qbic.datamanager.parser;
+
+import java.util.List;
+import life.qbic.projectmanagement.application.measurement.MeasurementMetadata;
+
+/**
+ * <b>Measurement Metadata Converter</b>
+ * <p>
+ * Measurement metadata converter enable the client to process a {@link ParsingResult} object and
+ * convert them into known implementations of the {@link MeasurementMetadata} interface.
+ *
+ * @since 1.4.0
+ */
+public interface MeasurementMetadataConverter {
+
+  /**
+   * Takes an instance of {@link ParsingResult} and tries to convert it to known implementations of
+   * the {@link MeasurementMetadata} interface.
+   * <p>
+   * Currently supported implementations are:
+   *
+   * <ul>
+   *   <li>NGS Measurement Metadata {@link life.qbic.projectmanagement.application.measurement.NGSMeasurementMetadata}</li>
+   *   <li>Proteomics Measurement Metadata {@link life.qbic.projectmanagement.application.measurement.ProteomicsMeasurementMetadata}</li>
+   * </ul>
+   *
+   * @param parsingResult       the parsing result to take as input for the conversion.
+   * @param ignoreMeasurementId weather to ignore the measurement identifier or not
+   * @return a list of converted implementations of {@link MeasurementMetadata}.
+   * @throws UnknownMetadataTypeException if no matching implementation of
+   *                                      {@link MeasurementMetadata} can be associated from the
+   *                                      provided {@link ParsingResult#keys()}.
+   * @since 1.4.0
+   */
+  List<? extends MeasurementMetadata> convert(ParsingResult parsingResult,
+      boolean ignoreMeasurementId)
+      throws UnknownMetadataTypeException;
+
+  class UnknownMetadataTypeException extends RuntimeException {
+
+    public UnknownMetadataTypeException(String message) {
+      super(message);
+    }
+  }
+
+  class MissingSampleIdException extends RuntimeException {
+    public MissingSampleIdException(String message) {
+      super(message);
+    }
+  }
+
+}
