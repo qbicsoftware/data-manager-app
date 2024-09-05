c7e4e10d7d57190af58ecbd5d6d7b87161fbf7c7
Steffengreiner <Steffen.Greiner@gmx.de>
2024-09-05 17:13:41 +0200

    Update JD For cardlayout

--- user-interface/src/main/java/life/qbic/datamanager/views/general/CardLayout.java
+++ user-interface/src/main/java/life/qbic/datamanager/views/general/CardLayout.java
@@ -6,8 +6,10 @@ import com.vaadin.flow.component.html.Div;
 /**
  * Card Layout
  * <p>
- * Card styled Layout employed within all {@link Main} in the
- * {@link life.qbic.datamanager.views.login.LoginLayout}
+ * Card styled Layout employed within all Components of the {@link Main} classes in the
+ * {@link life.qbic.datamanager.views.login.LoginLayout}.
+ * It's intended purpose is to give a harmonized style via css based on the native {@link Div}
+ * for all components related to user login, registration and password reset.
  */
 public class CardLayout extends Div {
 
