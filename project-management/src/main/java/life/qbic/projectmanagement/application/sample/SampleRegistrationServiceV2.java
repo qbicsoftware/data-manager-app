e63e98309d9c438beaaa989fc4ec2ef02fa442a3
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2024-09-23 10:05:38 +0200

    Improve registration api

--- project-management/src/main/java/life/qbic/projectmanagement/application/sample/SampleRegistrationServiceV2.java
+++ project-management/src/main/java/life/qbic/projectmanagement/application/sample/SampleRegistrationServiceV2.java
@@ -1,9 +1,12 @@
 package life.qbic.projectmanagement.application.sample;
 
 import java.util.Collection;
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
+import life.qbic.projectmanagement.application.batch.BatchRegistrationService;
 import life.qbic.projectmanagement.domain.model.experiment.ExperimentId;
 import life.qbic.projectmanagement.domain.model.project.ProjectId;
+import life.qbic.projectmanagement.domain.repository.SampleRepository;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 /**
@@ -15,9 +18,18 @@ import org.springframework.security.access.prepost.PreAuthorize;
  */
 public class SampleRegistrationServiceV2 {
 
+  private final BatchRegistrationService batchRegistrationService;
+  private final SampleRepository sampleRepository;
+
+  public SampleRegistrationServiceV2(BatchRegistrationService batchRegistrationService, SampleRepository sampleRepository) {
+    this.batchRegistrationService = Objects.requireNonNull(batchRegistrationService);
+    this.sampleRepository = Objects.requireNonNull(sampleRepository);
+  }
+
   @PreAuthorize("hasPermission(#projectId, 'life.qbic.projectmanagement.domain.model.project.Project', 'WRITE')")
   public CompletableFuture<Void> registerNewSamples(Collection<SampleMetadata> sampleMetadata,
-      ExperimentId experimentId, ProjectId projectId) {
+      ExperimentId experimentId, ProjectId projectId, String batchLabel, Boolean batchIsPilot) {
+
     throw new UnsupportedOperationException("Not implemented yet");
   }
 
