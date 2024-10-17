247f2b883c8b528d5b40f2fcdb549872870624fe
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2024-10-17 10:11:29 +0200

    Fix mandatory property for ngs measurements

--- user-interface/src/main/java/life/qbic/datamanager/parser/measurement/NGSMeasurementRegisterColumn.java
+++ user-interface/src/main/java/life/qbic/datamanager/parser/measurement/NGSMeasurementRegisterColumn.java
@@ -13,18 +13,18 @@ import java.util.Arrays;
 public enum NGSMeasurementRegisterColumn {
 
   SAMPLE_ID("QBiC Sample Id", 0, false, true),
-  SAMPLE_NAME("Sample Name", 1, false, true),
-  POOL_GROUP("Sample Pool Group", 2, false, true),
+  SAMPLE_NAME("Sample Name", 1, false, false),
+  POOL_GROUP("Sample Pool Group", 2, false, false),
   ORGANISATION_ID("Organisation ID", 3, false, true),
   FACILITY("Facility", 4, false, true),
   INSTRUMENT("Instrument", 5, false, true),
   SEQUENCING_READ_TYPE("Sequencing Read Type", 6, false, true),
-  LIBRARY_KIT("Library Kit", 7, false, true),
-  FLOW_CELL("Flow Cell", 8, false, true),
-  SEQUENCING_RUN_PROTOCOL("Sequencing Run Protocol", 11, false, true),
-  INDEX_I7("Index i7", 9, false, true),
-  INDEX_I5("Index i5", 10, false, true),
-  COMMENT("Comment", 11, false, true),
+  LIBRARY_KIT("Library Kit", 7, false, false),
+  FLOW_CELL("Flow Cell", 8, false, false),
+  SEQUENCING_RUN_PROTOCOL("Sequencing Run Protocol", 11, false, false),
+  INDEX_I7("Index i7", 9, false, false),
+  INDEX_I5("Index i5", 10, false, false),
+  COMMENT("Comment", 11, false, false),
   ;
 
   private final String headerName;
