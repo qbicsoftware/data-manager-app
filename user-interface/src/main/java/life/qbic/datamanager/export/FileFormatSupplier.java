80c6f9aadfad6a03da927b051fb2e4b7966ed03d
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2024-10-18 14:50:31 +0200

    Fix exception

--- user-interface/src/main/java/life/qbic/datamanager/export/FileFormatSupplier.java
+++ user-interface/src/main/java/life/qbic/datamanager/export/FileFormatSupplier.java
@@ -17,9 +17,11 @@ public interface FileFormatSupplier {
   class FormatException extends RuntimeException {
 
     public FormatException(String message) {
+      super(message);
     }
 
     public FormatException(String message, Throwable cause) {
+      super(message, cause);
     }
   }
 
