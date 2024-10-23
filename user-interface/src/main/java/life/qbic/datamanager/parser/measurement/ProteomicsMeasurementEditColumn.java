458e9139c7272ac78f38200b049227254d6fd809
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2024-10-23 12:27:14 +0200

    Add Proteomics Measurement Edit Column helper

--- user-interface/src/main/java/life/qbic/datamanager/parser/measurement/ProteomicsMeasurementEditColumn.java
+++ user-interface/src/main/java/life/qbic/datamanager/parser/measurement/ProteomicsMeasurementEditColumn.java
@@ -2,6 +2,7 @@ package life.qbic.datamanager.parser.measurement;
 
 import java.util.Optional;
 import life.qbic.datamanager.parser.Column;
+import life.qbic.datamanager.parser.ExampleProvider;
 import life.qbic.datamanager.parser.ExampleProvider.Helper;
 
 /**
@@ -40,6 +41,56 @@ public enum ProteomicsMeasurementEditColumn implements Column {
   private final int columnIndex;
   private final boolean readOnly;
   private final boolean mandatory;
+  private static final ExampleProvider exampleProvider = new ExampleProvider() {
+    @Override
+    public Helper getHelper(Column column) {
+      if (column instanceof ProteomicsMeasurementEditColumn proteomicsMeasurementEditColumn) {
+        return switch (proteomicsMeasurementEditColumn) {
+          case MEASUREMENT_ID -> null;
+          case SAMPLE_ID -> new Helper("QBiC sample IDs, e.g. Q29866",
+              "The sample(s) that will be linked to the measurement.");
+          case SAMPLE_NAME -> new Helper("Free text, e.g. MySample 01",
+              "A visual aid to simplify sample navigation for the person managing the metadata. Will be ignored after upload.");
+          case POOL_GROUP -> new Helper("Free text, e.g. pool group 1",
+              "A group of samples that are pooled together for a measurement. All samples in a pool group should have the same label.");
+          case TECHNICAL_REPLICATE_NAME -> null;
+          case ORGANISATION_ID -> new Helper("ROR URL (e.g. https://ror.org/03a1kwz48)",
+              "A unique identifier of the organisation where the measurement has been conducted.");
+          case ORGANISATION_NAME -> null;
+          case FACILITY -> new Helper("Free text, e.g. Quantitative Biology Center",
+              "The facility's name within the organisation.");
+          case MS_DEVICE -> new Helper("CURIE (ontology), e.g. NCIT:C12434",
+              "The MS device model that has been used for the measurement.  To avoid ambiguities, we expect an ontology term CURIE. You can use our ontology look up search online to query available terms and CURIEs we currently support.");
+          case MS_DEVICE_NAME -> null;
+          case CYCLE_FRACTION_NAME -> new Helper("Free text, e.g. Fraction01, AB",
+              "Sometimes a sample is fractionated and all fractions are measured. With this property you can indicate which fraction it is.");
+          case DIGESTION_METHOD -> new Helper("Enumeration, Select a value from the dropdown",
+              "Method that has been used to break proteins into peptides. Please use the dropdown menu to select one of the values.");
+          case DIGESTION_ENZYME ->
+              new Helper("Free text", "Information about the enzymes used for the proteolytic.");
+          case ENRICHMENT_METHOD -> new Helper("Free text",
+              "Enrichment of proteins or peptides of different characteristics.");
+          case INJECTION_VOLUME -> new Helper("Whole number, e.g. 1, 6, 8",
+              "The sample volume injected into the LC column in microliter.");
+          case LC_COLUMN -> new Helper("Free text, can be a commercial name or brand",
+              "The type of column that has been used.");
+          case LCMS_METHOD -> new Helper("Free text",
+              "Laboratory specific methods that have been used for LCMS measurement.");
+          case LABELING_TYPE -> new Helper("Free text",
+              "The label type that has been used to label the sample for measurement.");
+          case LABEL ->
+              new Helper("Free text", "The label value for the label type that has been used.");
+          case COMMENT ->
+              new Helper("Free text", "Notes about the measurement. (Max 500 characters)");
+        };
+      } else {
+
+        throw new IllegalArgumentException(
+            "Column not of class " + NGSMeasurementEditColumn.class.getName() + " but is "
+                + column.getClass().getName());
+      }
+    }
+  };
 
   ProteomicsMeasurementEditColumn(String headerName, int columnIndex, boolean readOnly,
       boolean mandatory) {
@@ -67,6 +118,6 @@ public enum ProteomicsMeasurementEditColumn implements Column {
 
   @Override
   public Optional<Helper> getFillHelp() {
-    return Optional.empty();
+    return Optional.ofNullable(exampleProvider.getHelper(this));
   }
 }
