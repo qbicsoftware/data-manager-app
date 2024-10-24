e4e49f9b8a2184af821e2d768b26f29c2188aa3c
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2024-10-24 15:32:58 +0200

    Checkin progress

---
+++ user-interface/src/main/java/life/qbic/datamanager/views/projects/edit/EditProjectDesignDialog.java
@@ -0,0 +1,9 @@
+package life.qbic.datamanager.views.projects.edit;/**
+ * <b><class short description - 1 Line!></b>
+ * 
+ * <p><More detailed description - When to use, what it solves, etc.></p>
+ *
+ * @since <version tag>
+ */
+public class EditProjectDesignDialog {
+}
