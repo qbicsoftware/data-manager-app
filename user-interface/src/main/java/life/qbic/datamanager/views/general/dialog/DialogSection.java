73a49cfb7cfcbd8697a1d0ae8ec316ff17e2a979
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2024-11-28 08:46:08 +0100

    Adjust section header and description margins

--- user-interface/src/main/java/life/qbic/datamanager/views/general/dialog/DialogSection.java
+++ user-interface/src/main/java/life/qbic/datamanager/views/general/dialog/DialogSection.java
@@ -22,9 +22,9 @@ public class DialogSection extends Div {
   private DialogSection(String title, String description) {
     addClassName("dialog-section");
     this.title = new Div(title);
-    this.title.addClassName("heading-4");
+    this.title.addClassNames("heading-4", "text-margin-bottom-03");
     this.description = new Div(description);
-    this.description.addClassName("normal-body-text");
+    this.description.addClassNames("normal-body-text", "text-margin-bottom-04");
     this.content = new Div();
     this.content.addClassName("dialog-content");
 
