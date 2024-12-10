7563a0bc04bc0d4ef942f6a5ae7ea98c3ffff459
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2024-12-10 09:04:25 +0100

    More refinemenets

--- user-interface/src/main/java/life/qbic/datamanager/views/general/dialog/stepper/StepperDialogFooter.java
+++ user-interface/src/main/java/life/qbic/datamanager/views/general/dialog/stepper/StepperDialogFooter.java
@@ -5,13 +5,20 @@ import java.util.Objects;
 import life.qbic.datamanager.views.general.dialog.ButtonFactory;
 
 /**
- * <b><class short description - 1 Line!></b>
+ * <b>Stepper Dialog Footer</b>
  *
- * <p><More detailed description - When to use, what it solves, etc.></p>
+ * <p>A more specialised footer compared to the
+ * {@link life.qbic.datamanager.views.general.dialog.DialogFooter}</p>. This footer can be used in
+ * the context of {@link StepperDialog} and implements the {@link NavigationListener} interface to
+ * get notified about navigation changes in the stepper.
  *
- * @since <version tag>
+ * @since 1.7.0
  */
 public class StepperDialogFooter implements NavigationListener {
+
+  private static final String FLEX_HORIZONTAL = "flex-horizontal";
+  private static final String GAP_04 = "gap-04";
+  private static final String CANCEL = "Cancel";
 
   private final StepperDialog dialog;
 
@@ -23,27 +30,21 @@ public class StepperDialogFooter implements NavigationListener {
     onNavigationChange(dialog.currentNavigation()); // we want to init the footer properly.
   }
 
+  /**
+   * Creates a {@link StepperDialogFooter} that wires to the provided {@link StepperDialog}. During
+   * instantiation, the footer component of the {@link StepperDialog} is set automatically and also
+   * the footer subscribes to navigation changes of the {@link StepperDialog}.
+   *
+   * @param dialog the stepper dialog to wire into
+   * @return the fully set-up stepper dialog footer
+   * @since 1.7.0
+   */
   public static StepperDialogFooter with(StepperDialog dialog) {
     return new StepperDialogFooter(Objects.requireNonNull(dialog));
   }
 
   private static void updateFooter(StepperDialog dialog, Div footer) {
     dialog.setFooter(footer);
-  }
-
-  @Override
-  public void onNavigationChange(NavigationInformation navigationInformation) {
-    int currentStep = navigationInformation.currentStep();
-    int totalSteps = navigationInformation.totalSteps();
-    if (isIntermediateStep(currentStep, totalSteps)) {
-      updateFooter(dialog,footerFactory.createIntermediate(dialog));
-      return;
-    }
-    if (currentStep == totalSteps) {
-      updateFooter(dialog,footerFactory.createLast(dialog));
-    } else {
-      updateFooter(dialog, footerFactory.createFirst(dialog));
-    }
   }
 
   private static boolean hasNextStep(int currentStep, int numberOfSteps) {
@@ -56,6 +57,21 @@ public class StepperDialogFooter implements NavigationListener {
 
   private static boolean isIntermediateStep(int currentStep, int numberOfSteps) {
     return hasNextStep(currentStep, numberOfSteps) && hasPreviousStep(currentStep);
+  }
+
+  @Override
+  public void onNavigationChange(NavigationInformation navigationInformation) {
+    int currentStep = navigationInformation.currentStep();
+    int totalSteps = navigationInformation.totalSteps();
+    if (isIntermediateStep(currentStep, totalSteps)) {
+      updateFooter(dialog, footerFactory.createIntermediate(dialog));
+      return;
+    }
+    if (currentStep == totalSteps) {
+      updateFooter(dialog, footerFactory.createLast(dialog));
+    } else {
+      updateFooter(dialog, footerFactory.createFirst(dialog));
+    }
   }
 
   private static class FooterFactory {
@@ -76,12 +92,14 @@ public class StepperDialogFooter implements NavigationListener {
 
   private static class FirstFooter extends Div {
 
+
+
     FirstFooter(StepperDialog dialog) {
-      addClassNames("flex-horizontal", "gap-04", "footer");
+      addClassNames(FLEX_HORIZONTAL, GAP_04, "footer");
       var buttonFactory = new ButtonFactory();
-      var cancelButton = buttonFactory.createCancelButton("Cancel");
+      var cancelButton = buttonFactory.createCancelButton(CANCEL);
       var nextButton = buttonFactory.createNavigationButton("Next");
-      cancelButton.addClickListener(listener-> dialog.cancel());
+      cancelButton.addClickListener(listener -> dialog.cancel());
       nextButton.addClickListener(listener -> dialog.next());
       add(cancelButton, nextButton);
     }
@@ -90,16 +108,16 @@ public class StepperDialogFooter implements NavigationListener {
   private static class IntermediateFooter extends Div {
 
     public IntermediateFooter(StepperDialog dialog) {
-      addClassNames("flex-horizontal", "footer-intermediate");
+      addClassNames(FLEX_HORIZONTAL, "footer-intermediate");
       var buttonFactory = new ButtonFactory();
-      var cancelButton = buttonFactory.createCancelButton("Cancel");
+      var cancelButton = buttonFactory.createCancelButton(StepperDialogFooter.CANCEL);
       var nextButton = buttonFactory.createNavigationButton("Next");
       var previousButton = buttonFactory.createNavigationButton("Previous");
-      previousButton.addClickListener(listener-> dialog.previous());
-      cancelButton.addClickListener(listener-> dialog.cancel());
+      previousButton.addClickListener(listener -> dialog.previous());
+      cancelButton.addClickListener(listener -> dialog.cancel());
       nextButton.addClickListener(listener -> dialog.next());
       var containerRight = new Div();
-      containerRight.addClassNames("flex-horizontal", "gap-04");
+      containerRight.addClassNames(FLEX_HORIZONTAL, GAP_04);
       containerRight.add(cancelButton, nextButton);
       add(previousButton, containerRight);
     }
@@ -108,16 +126,16 @@ public class StepperDialogFooter implements NavigationListener {
   private static class LastFooter extends Div {
 
     public LastFooter(StepperDialog dialog) {
-      addClassNames("flex-horizontal", "footer-intermediate");
+      addClassNames(FLEX_HORIZONTAL, "footer-intermediate");
       var buttonFactory = new ButtonFactory();
-      var cancelButton = buttonFactory.createCancelButton("Cancel");
+      var cancelButton = buttonFactory.createCancelButton(StepperDialogFooter.CANCEL);
       var confirmButton = buttonFactory.createConfirmButton("Submit");
       var previousButton = buttonFactory.createNavigationButton("Previous");
-      previousButton.addClickListener(listener-> dialog.previous());
-      cancelButton.addClickListener(listener-> dialog.cancel());
+      previousButton.addClickListener(listener -> dialog.previous());
+      cancelButton.addClickListener(listener -> dialog.cancel());
       confirmButton.addClickListener(listener -> dialog.confirm());
       var containerRight = new Div();
-      containerRight.addClassNames("flex-horizontal", "gap-04");
+      containerRight.addClassNames(FLEX_HORIZONTAL, GAP_04);
       containerRight.add(cancelButton, confirmButton);
       add(previousButton, containerRight);
     }
