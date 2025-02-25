f962ca966c166055fca6e071b8e5b8ef39e7957a
Steffengreiner <Steffen.Greiner@gmx.de>
2025-02-25 11:39:47 +0100

    Add oidc and oidcissuer to contact within application and domain

--- user-interface/src/main/java/life/qbic/datamanager/views/general/contact/Contact.java
+++ user-interface/src/main/java/life/qbic/datamanager/views/general/contact/Contact.java
@@ -6,7 +6,6 @@ import java.util.Objects;
 import life.qbic.application.commons.ApplicationException;
 
 /**
- *
  * <b>A contact person</b>
  *
  * <p>Describes a contact person with a name and email.</p>
@@ -19,10 +18,14 @@ public final class Contact implements Serializable {
   private static final long serialVersionUID = 8233688580418521324L;
   private String fullName;
   private String email;
+  private String oidc;
+  private String oidcIssuer;
 
-  public Contact(String fullName, String email) {
+  public Contact(String fullName, String email, String oidc, String oidcIssuer) {
     this.fullName = fullName;
     this.email = email;
+    this.oidc = oidc;
+    this.oidcIssuer = oidcIssuer;
   }
 
   public void setFullName(String fullName) {
@@ -33,27 +36,51 @@ public final class Contact implements Serializable {
     this.email = email;
   }
 
-  public String getFullName() {
+  public void setOidc(String oidc) {
+    this.oidc = oidc;
+  }
+
+  public void setOidcIssuer(String oidcIssuer) {
+    this.oidcIssuer = oidcIssuer;
+  }
+
+  public String fullName() {
     return fullName;
   }
 
-  public String getEmail() {
+  public String email() {
     return email;
   }
 
+  public String oidc() {
+    return oidc;
+  }
+
+  public String oidcIssuer() {
+    return oidcIssuer;
+  }
+
   public boolean isEmpty() {
-    return (fullName == null || fullName.isBlank()) && (email == null || email.isBlank());
+    return (fullName == null || fullName.isBlank()) && (email == null
+        || email.isBlank() && oidc == null || oidc.isBlank() && oidcIssuer.isBlank()
+        || oidcIssuer == null);
   }
 
   public boolean isComplete() {
+    return fullName != null && !fullName.isBlank() && email != null && !email.isBlank() && oidc
+        != null && !oidc.isBlank() && oidcIssuer != null && !oidcIssuer.isBlank();
+  }
+
+  public boolean hasMinimalInformation() {
     return fullName != null && !fullName.isBlank() && email != null && !email.isBlank();
   }
 
   public life.qbic.projectmanagement.domain.model.project.Contact toDomainContact() {
-    if (!isComplete()) {
+    if (!hasMinimalInformation()) {
       throw new ApplicationException("Contact is not complete and cannot be converted: " + this);
     }
-    return new life.qbic.projectmanagement.domain.model.project.Contact(getFullName(), getEmail());
+    return new life.qbic.projectmanagement.domain.model.project.Contact(fullName(), email(), oidc,
+        oidcIssuer);
   }
 
 
