db241a54a570575d8f626128e0b2d33f02b6d8e4
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2025-03-03 10:01:22 +0100

    Introduce Linked List as cache implementation

--- user-interface/src/main/java/life/qbic/datamanager/RequestCache.java
+++ user-interface/src/main/java/life/qbic/datamanager/RequestCache.java
@@ -2,6 +2,7 @@ package life.qbic.datamanager;
 
 import com.vaadin.flow.server.VaadinSession;
 import com.vaadin.flow.spring.annotation.SpringComponent;
+import java.util.LinkedList;
 import java.util.Optional;
 import life.qbic.projectmanagement.application.api.AsyncProjectService.CacheableRequest;
 
@@ -13,6 +14,8 @@ import life.qbic.projectmanagement.application.api.AsyncProjectService.Cacheable
  */
 @SpringComponent
 public class RequestCache {
+
+  private static final int CACHE_SIZE = 10;
 
   /**
    * Stores a {@link CacheableRequest} in the current {@link VaadinSession}.
@@ -28,10 +31,25 @@ public class RequestCache {
   public void store(CacheableRequest request) throws CacheException {
     VaadinSession session = VaadinSession.getCurrent();
     if (session != null) {
-      session.setAttribute(CacheableRequest.class.getName(), request);
+      if (session.getAttribute(RequestCache.class.getName()) == null) {
+        session.setAttribute(RequestCache.class.getName(), new LinkedList<CacheableRequest>());
+      }
+      var entry = session.getAttribute(RequestCache.class.getName());
+      if (entry instanceof LinkedList) {
+        var list = (LinkedList<CacheableRequest>) entry;
+        store(list, request);
+      }
       return;
     }
     throw new CacheException("No Vaadin session found");
+  }
+
+
+  private static void store(LinkedList<CacheableRequest> list, CacheableRequest request) {
+    if (list.size() == CACHE_SIZE) {
+      list.removeFirst();
+    }
+    list.add(request);
   }
 
   /**
@@ -42,13 +60,28 @@ public class RequestCache {
    *                        ({@link VaadinSession#getSession()} returned <code>null</code>)
    * @since 1.9.0
    */
-  public Optional<CacheableRequest> get() throws CacheException {
+  public Optional<CacheableRequest> get(String id) throws CacheException {
     VaadinSession session = VaadinSession.getCurrent();
     if (session != null) {
-      return Optional.ofNullable(
-          (CacheableRequest) session.getAttribute(CacheableRequest.class.getName()));
+      var cache = session.getAttribute(RequestCache.class.getName());
+      if (cache == null) {
+        return Optional.empty();
+      }
+      if (cache instanceof LinkedList) {
+        return Optional.ofNullable(findRequest((LinkedList<CacheableRequest>) cache, id));
+      }
+      throw new CacheException("Unknown cache type");
     }
     throw new CacheException("No Vaadin session found");
+  }
+
+  private CacheableRequest findRequest(LinkedList<CacheableRequest> cache, String id) {
+    for (CacheableRequest request : cache) {
+      if (request.requestId().equals(id)) {
+        return request;
+      }
+    }
+    return null;
   }
 
   public static class CacheException extends RuntimeException {
