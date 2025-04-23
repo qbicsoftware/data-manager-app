0de23e3fccfce5d243978c0ba72088bb74da4c8d
KochTobi <kochtobi@users.noreply.github.com>
2025-04-23 15:07:19 +0200

    remove input check and forward to the service to fail
    We simply accept that for invalid input, an error is returned. This way we do not need to keep in sync with the parsing on the side of the orcid repository.

--- project-management/src/main/java/life/qbic/projectmanagement/application/contact/PersonLookupService.java
+++ project-management/src/main/java/life/qbic/projectmanagement/application/contact/PersonLookupService.java
@@ -32,18 +32,11 @@ public class PersonLookupService {
   public List<OrcidEntry> queryPersons(String filter, int offset, int limit) {
     // returned by JPA -> UnmodifiableRandomAccessList
     List<OrcidEntry> orcidEntries = new ArrayList<>();
-    //Orcid Repository will return an Error 500 if the search string starts with a special character
-    // or contains one of the non-listed characters here (such as % & or :)
-    String validRegex = "^[A-Za-z0-9][A-Za-z0-9.@_-]*$";
     //Return empty result if no query was provided by the user
     if (filter.isBlank()) {
       return orcidEntries;
     }
-    //Return empty result if an invalid query was provided by the user
-    if (!filter.matches(validRegex)) {
-      return orcidEntries;
-    }
-    orcidEntries = personRepository.findAll(filter, limit, offset);
+    orcidEntries.addAll(personRepository.findAll(filter, limit, offset));
     // the list must be modifiable for spring security to filter it
     return orcidEntries;
   }
