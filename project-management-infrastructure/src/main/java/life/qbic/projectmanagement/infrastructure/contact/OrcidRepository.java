e889b0c56e534aed739c3fb152f6baf9894aacc0
KochTobi <kochtobi@users.noreply.github.com>
2025-04-23 15:12:11 +0200

    Only search for complete records
    Co-authored-by: steffengreiner <steffen.greiner@qbic.uni-tuebingen.de>

--- project-management-infrastructure/src/main/java/life/qbic/projectmanagement/infrastructure/contact/OrcidRepository.java
+++ project-management-infrastructure/src/main/java/life/qbic/projectmanagement/infrastructure/contact/OrcidRepository.java
@@ -4,6 +4,8 @@ import static life.qbic.logging.service.LoggerFactory.logger;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.net.URI;
@@ -14,11 +16,11 @@ import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.StringJoiner;
 import java.util.stream.Collectors;
 import life.qbic.logging.api.Logger;
 import life.qbic.projectmanagement.application.contact.OrcidEntry;
@@ -39,7 +41,7 @@ import org.springframework.stereotype.Repository;
 public class OrcidRepository implements PersonRepository {
 
   private static final Logger log = logger(OrcidRepository.class);
-  private static final String PAGINATED_QUERY = "https://pub.orcid.org/v3.0/expanded-search/?start=%s&rows=%s&q=%s";
+  private final String paginatedQuery;
   private static final String OIDC_ISSUER = "https://orcid.org";
   private final String token;
   private final String refreshToken;
@@ -47,9 +49,11 @@ public class OrcidRepository implements PersonRepository {
 
   @Autowired
   public OrcidRepository(
-      @Value("${spring.security.oauth2.client.registration.orcid.client-id}") String clientID,
-      @Value("${spring.security.oauth2.client.registration.orcid.client-secret}") String clientSecret,
-      @Value("${spring.security.oauth2.client.provider.orcid.token-uri}") String tokenEndpoint) {
+      @Value("${qbic.external-service.person-search.orcid.client-id}") String clientID,
+      @Value("${qbic.external-service.person-search.orcid.client-secret}") String clientSecret,
+      @Value("${qbic.external-service.person-search.orcid.token-uri}") String tokenEndpoint,
+      @Value("${qbic.external-service.person-search.orcid.extended-search-uri}") String paginatedQuery) {
+    this.paginatedQuery = paginatedQuery;
     httpClient = createHttpClient();
     var authResponse = authenticate(httpClient, clientID, clientSecret, tokenEndpoint);
     this.refreshToken = authResponse.refresh_token();
@@ -115,42 +119,59 @@ public class OrcidRepository implements PersonRepository {
 
   @Override
   public List<OrcidEntry> findAll(String query, int limit, int offset) {
-    var queryUrl = String.format(PAGINATED_QUERY, offset, limit, query);
-    HttpRequest request = HttpRequest.newBuilder().uri(URI.create(queryUrl))
+    String url = paginatedQuery;
+    url += "+AND+email:*"; //require an email to be present
+    url += "+AND+given-names:*"; //require an email to be present
+    url += "+AND+family-name:*"; //require an email to be present
+    var queryUrl = String.format(url, offset, limit, query);
+    URI uri;
+    try {
+      uri = URI.create(queryUrl);
+    } catch (IllegalArgumentException invalidQuery) {
+      log.error(invalidQuery.getMessage(), invalidQuery);
+      return List.of();
+    }
+    HttpRequest request = HttpRequest.newBuilder().uri(uri)
         .headers("Authorization", "Bearer " + token, "Accept", "application/json").GET()
         .build();
+
+    HttpResponse<String> response;
     try {
-      var response = httpClient.send(request, BodyHandlers.ofString());
-      ObjectMapper mapper = new ObjectMapper();
-      var node = mapper.readTree(response.body());
-      var value = node.get("expanded-result");
-      if (response.statusCode() == HttpStatus.OK.value()) {
-        if (!value.isEmpty()) {
-          var foundRecords = new ArrayList<>(
-              Arrays.stream(mapper.convertValue(value, OrcidRecord[].class))
-                  .map(OrcidRepository::convert).toList());
-          var invalidRecords = foundRecords.stream().filter(Objects::isNull).toList();
-          //Filter null values resulting from invalid records on OrcIds side
-          var validRecords = foundRecords.stream().filter(Objects::nonNull).toList();
-          log.info(
-              "From a total of %d parsed orcid records, %d were valid and %d were invalid".formatted(
-                  foundRecords.size(), validRecords.size(), invalidRecords.size()));
-          return validRecords;
-        } else {
-          return new ArrayList<>();
-        }
-      } else {
-        log.error("Error getting orcid records due to " + response.statusCode());
-        throw new OrcidRepository.QueryException(
-            "Orcid Public repository does not seem to be available",
-            new Throwable(response.body()));
-      }
-    } catch (IOException | InterruptedException e) {
-      if (e instanceof InterruptedException) {
-        Thread.currentThread().interrupt();
-      }
-      log.error("Error sending orcid request", e);
-      throw new OrcidRepository.QueryException("Person repository seems not available", e);
+      response = httpClient.send(request, BodyHandlers.ofString());
+    } catch (IOException e) {
+      log.error(e.getMessage(), e);
+      return List.of();
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+      log.error(e.getMessage(), e);
+      return List.of();
+    }
+    ObjectMapper mapper = new ObjectMapper();
+    JsonNode node;
+    try {
+      node = mapper.readTree(response.body());
+    } catch (JsonProcessingException e) {
+      log.error(e.getMessage(), e);
+      return List.of();
+    }
+    if (!node.has("expanded-result")) {
+      log.error("Could not parse orcid response: " + node);
+      return List.of();
+    }
+    var value = node.get("expanded-result");
+    HttpStatus httpStatus = HttpStatus.resolve(response.statusCode());
+    if (value.isEmpty()) {
+      return List.of();
+    }
+    if (httpStatus != null && httpStatus.is2xxSuccessful()) {
+      List<OrcidEntry> foundRecords = Arrays.stream(mapper.convertValue(value, OrcidRecord[].class))
+          .map(OrcidRepository::convert).toList();
+      return foundRecords.stream()
+          .filter(Objects::nonNull)
+          .toList();
+    } else {
+      log.error("Error getting orcid records due to " + response.statusCode());
+      return List.of();
     }
   }
 
@@ -182,6 +203,17 @@ public class OrcidRepository implements PersonRepository {
     @Override
     public int hashCode() {
       return Objects.hash(orcidID, givenName, familyName, creditName, Arrays.hashCode(email));
+    }
+
+    @Override
+    public String toString() {
+      return new StringJoiner(", ", OrcidRecord.class.getSimpleName() + "[", "]")
+          .add("orcidID='" + orcidID + "'")
+          .add("givenName='" + givenName + "'")
+          .add("familyName='" + familyName + "'")
+          .add("creditName='" + creditName + "'")
+          .add("email=" + Arrays.toString(email))
+          .toString();
     }
   }
 
