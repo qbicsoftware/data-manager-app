bd7b8a14cb84b514a976f1b1ba50b1fefa9283e0
KochTobi <kochtobi@users.noreply.github.com>
2025-06-23 15:11:50 +0200

    Replace if-else with Optional methods

--- project-management/src/main/java/life/qbic/projectmanagement/application/experiment/ExperimentInformationService.java
+++ project-management/src/main/java/life/qbic/projectmanagement/application/experiment/ExperimentInformationService.java
@@ -422,9 +422,10 @@ public class ExperimentInformationService {
     for (AsyncProjectService.ExperimentalVariable experimentalVariable : experimentalVariables) {
       List<ExperimentalValue> experimentalValues = new ArrayList<>();
       for (String level : experimentalVariable.levels()) {
-        ExperimentalValue experimentalValue = experimentalVariable.optionalUnit().isPresent()
-            ? ExperimentalValue.create(level, experimentalVariable.unit())
-            : ExperimentalValue.create(level);
+        ExperimentalValue experimentalValue =
+            experimentalVariable.optionalUnit()
+                .map(unit -> ExperimentalValue.create(level, unit))
+                .orElse(ExperimentalValue.create(level));
         experimentalValues.add(experimentalValue);
       }
       experiment.addVariableToDesign(experimentalVariable.name(), experimentalValues);
