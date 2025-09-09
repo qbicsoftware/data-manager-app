d317da13c0ad8283954a8358319c616c0c3d5bcc
KochTobi <kochtobi@users.noreply.github.com>
2025-09-09 12:47:21 +0200

    add close call to vaadin session on logout.

--- user-interface/src/main/java/life/qbic/datamanager/security/LogoutService.java
+++ user-interface/src/main/java/life/qbic/datamanager/security/LogoutService.java
@@ -1,6 +1,7 @@
 package life.qbic.datamanager.security;
 
 import com.vaadin.flow.server.VaadinServletRequest;
+import com.vaadin.flow.server.VaadinSession;
 import java.io.Serial;
 import java.io.Serializable;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
@@ -13,6 +14,7 @@ public class LogoutService implements Serializable {
   private static final long serialVersionUID = 5199220688136926750L;
 
   public void logout() {
+    VaadinSession.getCurrent().close();
     SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
     logoutHandler.logout(VaadinServletRequest.getCurrent().getHttpServletRequest(), null, null);
   }
