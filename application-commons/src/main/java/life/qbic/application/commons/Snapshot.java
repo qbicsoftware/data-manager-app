67031e3e0f87a82fcd8344b2bcf7e1a371f3a76e
KochTobi <kochtobi@users.noreply.github.com>
2025-09-10 15:32:37 +0200

    require all snapshots to be serializable

--- application-commons/src/main/java/life/qbic/application/commons/Snapshot.java
+++ application-commons/src/main/java/life/qbic/application/commons/Snapshot.java
@@ -1,9 +1,11 @@
 package life.qbic.application.commons;
 
+import java.io.Serializable;
+
 /**
  * Interface for the Memento in the <a
  * href="https://refactoring.guru/design-patterns/memento">Memento Pattern</a>
  */
-public interface Snapshot {
+public interface Snapshot extends Serializable {
 
 }
