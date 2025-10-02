c7375390818c0cc0646930e478cb15e97356802c
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2025-10-02 17:25:50 +0200

    Extend JD

--- user-interface/src/main/java/life/qbic/datamanager/views/UiUtil.java
+++ user-interface/src/main/java/life/qbic/datamanager/views/UiUtil.java
@@ -20,6 +20,7 @@ public class UiUtil {
    * <p>
    * The wrapper contains safeguards like UI null-checks, attachment checks, session lock checks and
    * runs the task accordingly in a UI thread-safe manner.
+   * The {@link Runnable#run()} will only be invoked, if the UI is attached.
    *
    * @param task the task to run
    * @since 1.12.0
