f0bf245ec83a9c114850db2776f073629ba5bef8
KochTobi <kochtobi@users.noreply.github.com>
2025-11-17 14:00:23 +0100

    Introduce hot flux with announcements

--- datamanager-app/src/main/java/life/qbic/datamanager/announcements/AnnouncementService.java
+++ datamanager-app/src/main/java/life/qbic/datamanager/announcements/AnnouncementService.java
@@ -1,6 +1,7 @@
 package life.qbic.datamanager.announcements;
 
 import java.time.Instant;
+import java.util.List;
 import reactor.core.publisher.Flux;
 
 /**
@@ -18,11 +19,40 @@ public interface AnnouncementService {
   Flux<Announcement> loadActiveAnnouncements(Instant timePoint);
 
   /**
+   * A {@link Flux} containing announcements. Each bundle contains announcements active at the given time. The Flux is a hot source.
+   * Every subscriber gets the latest 1 AnnouncementBundle and all following bundles. At least one subscription is required for the flux to connect.
+   *
+   * @return a hot flux publishing bundled announcements
+   */
+  Flux<AnnouncementBundle> activeAnnouncements();
+
+  /**
    * An announcement with a given message.
    *
    * @param message
    */
   record Announcement(String message) {
 
+  }
+
+  /**
+   * A bundle of announcements. The announcement list contained within is unmodifiable.
+   *
+   * @param announcements a list of announcements. The provided list is copied into an unmodifiable
+   *                      list.
+   */
+  record AnnouncementBundle(List<Announcement> announcements) {
+
+    public AnnouncementBundle {
+      announcements = List.copyOf(announcements);
+    }
+
+    static AnnouncementBundle empty() {
+      return new AnnouncementBundle(List.of());
+    }
+
+    public boolean isEmpty() {
+      return announcements.isEmpty();
+    }
   }
 }
--- datamanager-app/src/main/java/life/qbic/datamanager/announcements/AnnouncementServiceImpl.java
+++ datamanager-app/src/main/java/life/qbic/datamanager/announcements/AnnouncementServiceImpl.java
@@ -1,8 +1,11 @@
 package life.qbic.datamanager.announcements;
 
+import java.time.Duration;
 import java.time.Instant;
+import java.util.List;
 import java.util.Objects;
 import life.qbic.projectmanagement.application.concurrent.VirtualThreadScheduler;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
@@ -10,9 +13,15 @@ import reactor.core.publisher.Flux;
 public class AnnouncementServiceImpl implements AnnouncementService {
 
   private final AnnouncementRepository announcementRepository;
+  private final Duration initialDelay;
+  private final Duration refreshInterval;
 
-  public AnnouncementServiceImpl(AnnouncementRepository announcementRepository) {
+  public AnnouncementServiceImpl(AnnouncementRepository announcementRepository,
+      @Value(value = "${announcement.initial-delay}") Duration initialDelay,
+      @Value(value = "${announcement.refresh-interval}") Duration refreshInterval) {
     this.announcementRepository = announcementRepository;
+    this.initialDelay = initialDelay;
+    this.refreshInterval = refreshInterval;
   }
 
   @Override
@@ -23,6 +32,24 @@ public class AnnouncementServiceImpl implements AnnouncementService {
         .distinctUntilChanged(Objects::hashCode) //avoid unnecessary work
         .map(AnnouncementServiceImpl::toApiObject)
         .subscribeOn(VirtualThreadScheduler.getScheduler());
+  }
+
+  private List<Announcement> foobar(Instant timePoint) {
+    return announcementRepository.getAnnouncementByDisplayStartTimeBeforeAndDisplayEndTimeAfterOrderByDisplayStartTimeAsc(
+            timePoint, timePoint)
+        .stream()
+        .map(AnnouncementServiceImpl::toApiObject)
+        .distinct()
+        .toList();
+  }
+
+  @Override
+  public Flux<AnnouncementBundle> activeAnnouncements() {
+    return Flux.interval(initialDelay, refreshInterval)
+        .map(ignored -> foobar(Instant.now()))
+        .map(AnnouncementBundle::new)
+        .replay(1) // every subscriber gets the last bundle directly
+        .autoConnect();
   }
 
   private static Announcement toApiObject(AnnouncementRepository.Announcement announcement) {
