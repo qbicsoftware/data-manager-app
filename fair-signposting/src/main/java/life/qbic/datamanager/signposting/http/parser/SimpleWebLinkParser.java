5184a89d33e39183f4f66f6803a598a0047ea685
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2025-11-21 09:53:50 +0100

    Provide java docs

--- fair-signposting/src/main/java/life/qbic/datamanager/signposting/http/parser/SimpleWebLinkParser.java
+++ fair-signposting/src/main/java/life/qbic/datamanager/signposting/http/parser/SimpleWebLinkParser.java
@@ -3,12 +3,9 @@ package life.qbic.datamanager.signposting.http.parser;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import life.qbic.datamanager.signposting.http.WebLinkParser;
-import life.qbic.datamanager.signposting.http.WebLinkParser.StructureException;
 import life.qbic.datamanager.signposting.http.lexer.WebLinkToken;
 import life.qbic.datamanager.signposting.http.lexer.WebLinkTokenType;
 
@@ -45,23 +42,47 @@ public class SimpleWebLinkParser implements WebLinkParser {
   }
 
 
+  /**
+   * Parses a list of lexed web link tokens to a raw link header value. The parser only performs
+   * structural validation, not semantic validation.
+   * <p>
+   * The template for structural validation is the serialisation description in ABNF for RFC 8288
+   * Section 3.
+   *
+   * <p>
+   * Parser contract:
+   *
+   * <ul>
+   *   <li>The token list must contain an EOF token</li>
+   *   <li>The last token item must be an EOF token, based on ascending sorting by position</li>
+   * </ul>
+   * <p>
+   * In case the contract is violated, a structure exception is thrown.
+   *
+   * @param tokens a list of tokens to parse as raw web link header
+   * @return a raw web link header, structurally validated against RFC 8288
+   * @throws NullPointerException if the tokens list is {@code null}
+   * @throws StructureException   if the tokens violate the structure of a valid web link token
+   */
   @Override
   public RawLinkHeader parse(List<WebLinkToken> tokens)
       throws NullPointerException, StructureException {
     Objects.requireNonNull(tokens);
+
     if (tokens.isEmpty()) {
       throw new StructureException(
           "A link header entry must have at least one web link. Tokens were empty.");
     }
 
+    // Always reset the internal state on every parse() call
+    reset();
+
     this.tokens = tokens.stream()
         .sorted(Comparator.comparingInt(WebLinkToken::position))
         .toList();
 
+    // Validate contract
     ensureEOF("Lexer did not append EOF token");
-
-    // reset the current the parser state
-    currentPosition = 0;
 
     if (this.tokens.get(currentPosition).type() == WebLinkTokenType.EOF) {
       throw new StructureException(
@@ -76,7 +97,8 @@ public class SimpleWebLinkParser implements WebLinkParser {
     while (current().type() == WebLinkTokenType.COMMA) {
       next();
       if (currentIsEof()) {
-        throw new StructureException("Unexpected trailing comma: expected another link-value after ','.");
+        throw new StructureException(
+            "Unexpected trailing comma: expected another link-value after ','.");
       }
       collectedLinks.add(parseLinkValue());
     }
@@ -87,12 +109,39 @@ public class SimpleWebLinkParser implements WebLinkParser {
     return new RawLinkHeader(collectedLinks);
   }
 
+  /**
+   * Resets the internal state of the parser instance
+   */
+  private void reset() {
+    currentPosition = 0;
+  }
+
+  /**
+   * Checks if the last token in the token list is an EOF token. To keep the parser robust and
+   * simple, this is part of the contract and the parser shall fail early if the contract is
+   * violated.
+   *
+   * @param errorMessage the message to provide in the exception
+   * @throws IllegalStateException if the last token of the list ist not an EOF token
+   */
   private void ensureEOF(String errorMessage) throws IllegalStateException {
     if (tokens.getLast().type() != WebLinkTokenType.EOF) {
       throw new IllegalStateException(errorMessage);
     }
   }
 
+  /**
+   * Parses a single web link value, which must contain a target (URI). Optionally, the web link can
+   * have one or more parameters.
+   * <p>
+   * If the target has a trailing ',' (COMMA), no further parameters are expected.
+   * <p>
+   * The correctness of the parameter structure with a precedent ';' (SEMICOLON) after the target is
+   * concern of the {@link #parseParameters()} method, since it is part of the parameter list
+   * description.
+   *
+   * @return a raw web link value with target and optionally one or more parameters
+   */
   private RawLink parseLinkValue() {
     var parsedLinkValue = parseUriReference();
     if (current().type() != WebLinkTokenType.COMMA) {
@@ -101,6 +150,18 @@ public class SimpleWebLinkParser implements WebLinkParser {
     return new RawLink(parsedLinkValue, List.of());
   }
 
+  /**
+   * Parses parameters beginning from the current token position (inclusive).
+   * <p>
+   * Based on the serialisation description of RFC 8288 for link-values, params must have a
+   * precedent ';' (SEMICOLON). If the start position on method call is not a semicolon, an
+   * exception will be thrown.
+   * <p>
+   * In case the link-value has no parameters at all (e.g. multiple web links with targets (URI)
+   * only), this method should not be called in the first place.
+   *
+   * @return a list of raw parameters with param name and value
+   */
   private List<RawParam> parseParameters() {
     var parameters = new ArrayList<RawParam>();
     if (currentIsEof()) {
@@ -111,7 +172,7 @@ public class SimpleWebLinkParser implements WebLinkParser {
     next();
 
     // now one or more parameters can follow
-    while(current().type() != WebLinkTokenType.COMMA) {
+    while (current().type() != WebLinkTokenType.COMMA) {
       RawParam parameter = parseParameter();
       parameters.add(parameter);
       // If the current token is no ';' (SEMICOLON), no additional parameters are expected
@@ -151,6 +212,11 @@ public class SimpleWebLinkParser implements WebLinkParser {
     return RawParam.withValue(paramName, rawParamValue);
   }
 
+  /**
+   * Evaluates if the current token is an EOF token.
+   *
+   * @return {@code true}, if the current token is an EOF token, else {@code false}
+   */
   private boolean currentIsEof() {
     return current().type() == WebLinkTokenType.EOF;
   }
@@ -169,6 +235,15 @@ public class SimpleWebLinkParser implements WebLinkParser {
     }
   }
 
+  /**
+   * Checks if the current token matches any (at least one) expected token.
+   * <p>
+   * If no expected type is provided, the method will throw a
+   * {@link life.qbic.datamanager.signposting.http.WebLinkParser.StructureException}.
+   *
+   * @param expected zero or more expected token types.
+   * @throws StructureException if the current token does not match any expected token
+   */
   private void expectCurrentAny(WebLinkTokenType... expected) throws StructureException {
     var matches = Arrays.stream(expected)
         .anyMatch(type -> type.equals(current().type()));
@@ -210,19 +285,26 @@ public class SimpleWebLinkParser implements WebLinkParser {
     return uriValue;
   }
 
+  /**
+   * Returns the token on the current position.
+   *
+   * @return the token on the current position.
+   */
   private WebLinkToken current() {
     return tokens.get(currentPosition);
   }
 
+  /**
+   * Returns the next token from the current position. If the current position is already the last
+   * token of the token list, the last token will be returned.
+   * <p>
+   * By contract, the parser expects the last item to be an EOF token (see
+   * {@link WebLinkTokenType#EOF}). So the last item in the token list will always be an EOF token.
+   */
   private WebLinkToken next() {
     if (currentPosition < tokens.size() - 1) {
       currentPosition++;
     }
     return current();
-  }
-
-  private WebLinkToken peek() {
-    var nextPosition = currentPosition < tokens.size() - 1 ? currentPosition + 1 : currentPosition;
-    return tokens.get(nextPosition);
   }
 }
