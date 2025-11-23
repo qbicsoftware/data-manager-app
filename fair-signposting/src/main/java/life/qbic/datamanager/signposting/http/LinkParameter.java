3083c6d69aaf464053ec2f0d98c8c0c7882503cd
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2025-11-23 21:13:37 +0100

    Refine validation

---
+++ fair-signposting/src/main/java/life/qbic/datamanager/signposting/http/LinkParameter.java
@@ -0,0 +1,10 @@
+package life.qbic.datamanager.signposting.http;
+
+/**
+ * <record short description>
+ *
+ * @since <version tag>
+ */
+public record WebLinkParameter(String name, String value) {
+
+}
---
+++ fair-signposting/src/main/java/life/qbic/datamanager/signposting/http/validation/RfcLinkParameter.java
@@ -0,0 +1,9 @@
+package life.qbic.datamanager.signposting.http.validation;
+
+/**
+ * <enum short description>
+ *
+ * @since <version tag>
+ */
+public enum LinkParameters {
+}
