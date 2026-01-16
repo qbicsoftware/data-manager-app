e9bfe9163978d9e4f4a3c01d184c20bd34971758
KochTobi <kochtobi@users.noreply.github.com>
2026-01-16 18:13:10 +0100

    Provide read-only measurement repositories

---
+++ project-management-infrastructure/src/main/java/life/qbic/projectmanagement/infrastructure/experiment/measurement/foobar/NgsMeasurementJpaRepository.java
@@ -0,0 +1,379 @@
+package life.qbic.projectmanagement.infrastructure.experiment.measurement.foobar;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.SecondaryTable;
+import jakarta.persistence.Table;
+import jakarta.persistence.criteria.Join;
+import java.io.IOException;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.StringJoiner;
+import life.qbic.projectmanagement.domain.model.measurement.MeasurementId;
+import life.qbic.projectmanagement.infrastructure.PreventAnyUpdateEntityListener;
+import life.qbic.projectmanagement.infrastructure.experiment.measurement.foobar.NgsMeasurementJpaRepository.Instrument.InstrumentReadConverter;
+import life.qbic.projectmanagement.infrastructure.experiment.measurement.foobar.NgsMeasurementJpaRepository.NgsMeasurementInformation;
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.data.convert.ReadingConverter;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NgsMeasurementJpaRepository extends
+    PagingAndSortingRepository<NgsMeasurementInformation, MeasurementId>,
+    JpaSpecificationExecutor<NgsMeasurementInformation> {
+
+  final class NgsMeasurementFilter {
+
+    private final String experimentId;
+    private final String searchTerm;
+
+    public NgsMeasurementFilter(@NonNull String experimentId,
+        @NonNull String searchTerm) {
+      this.searchTerm = Objects.requireNonNull(searchTerm);
+      this.experimentId = Objects.requireNonNull(experimentId);
+    }
+
+    public Specification<NgsMeasurementInformation> asSpecification() {
+      return matchesExperiment(experimentId)
+          .and(containsSearchTerm(searchTerm));
+    }
+
+    private static Specification<NgsMeasurementInformation> matchesExperiment(String experimentId) {
+      return (root, query, criteriaBuilder) ->
+      {
+        query.distinct(true);
+        return criteriaBuilder.equal(root.join("sampleInfos").get("experimentId"), experimentId);
+      };
+    }
+
+    private static Specification<NgsMeasurementInformation> containsSearchTerm(String searchTerm) {
+      var cleanedLowerCaseSearchTerm = searchTerm.strip().toLowerCase();
+      if (Objects.isNull(searchTerm) || searchTerm.isEmpty()) {
+        return Specification.unrestricted();
+      }
+      return (root, query, criteriaBuilder) -> {
+        query.distinct(true);
+        var measurementCodeContains = criteriaBuilder.like(
+            criteriaBuilder.lower(root.get("measurementCode")),
+            "%" + cleanedLowerCaseSearchTerm + "%");
+        var measurementNameContains = criteriaBuilder.like(
+            criteriaBuilder.lower(root.get("measurementName")),
+            "%" + cleanedLowerCaseSearchTerm + "%");
+        Join<Object, String> sampleInfos = root.joinList("sampleInfos");
+        var anySampleLabelContains = criteriaBuilder.like(
+            criteriaBuilder.lower(sampleInfos.get("sampleLabel")),
+            "%" + cleanedLowerCaseSearchTerm + "%");
+        var sampleCommentContains = criteriaBuilder.like(
+            criteriaBuilder.lower(sampleInfos.get("comment")),
+            "%" + cleanedLowerCaseSearchTerm + "%");
+        return
+            criteriaBuilder.or(measurementCodeContains,
+                measurementNameContains,
+                anySampleLabelContains,
+                sampleCommentContains);
+      };
+    }
+
+    @Override
+    public String toString() {
+      return new StringJoiner(", ", NgsMeasurementFilter.class.getSimpleName() + "[", "]")
+          .add("experimentId='" + experimentId + "'")
+          .add("searchTerm='" + searchTerm + "'")
+          .toString();
+    }
+  }
+
+  record Instrument(String label, String oboId, String iri) implements Serializable {
+
+    @JsonComponent
+    static class InstrumentJsonDeserializer extends JsonDeserializer<Instrument> {
+
+      @Override
+      public Instrument deserialize(JsonParser jsonParser, DeserializationContext ctxt)
+          throws IOException, JacksonException {
+        JsonNode tree = jsonParser.readValueAsTree();
+        String oboId = Optional.ofNullable(tree.get("name"))
+            .map(JsonNode::asText) //e.g. EFO_0008633
+            .map(it -> it.replace("_", ":")) //e.g. EFO:0008633
+            .orElseThrow(() -> new JsonParseException("Could not parse instrument oboId."));
+        String label = Optional.ofNullable(tree.get("label"))
+            .map(JsonNode::asText)
+            .orElseThrow(() -> new JsonParseException("Could not parse instrument label."));
+        String iri = Optional.ofNullable(tree.get("classIri"))
+            .map(JsonNode::asText)
+            .orElseThrow(() -> new JsonParseException("Could not parse instrument iri."));
+        return new Instrument(label, oboId, iri);
+      }
+    }
+
+    @ReadingConverter
+    static class InstrumentReadConverter implements AttributeConverter<Instrument, String> {
+
+      private final ObjectMapper objectMapper;
+
+      public InstrumentReadConverter(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+      }
+
+      @Override
+      public String convertToDatabaseColumn(Instrument attribute) {
+        return "";
+      }
+
+      @Override
+      public Instrument convertToEntityAttribute(String dbData) {
+        try {
+          return objectMapper.readValue(dbData, Instrument.class);
+        } catch (JsonProcessingException e) {
+          throw new RuntimeException(e);
+        }
+      }
+    }
+  }
+
+  record Organisation(String label, String iri) {
+
+  }
+
+  @Entity
+  @Table(name = "specific_measurement_metadata_ngs")
+  @SecondaryTable(name = "sample", pkJoinColumns = @PrimaryKeyJoinColumn(
+      name = "sample_id", referencedColumnName = "sample_id"
+  ))
+  final class NgsSampleInfo {
+
+    @Id
+    @Column(name = "sample_id")
+    private String sampleId;
+    @ManyToOne
+    @JoinColumn(name = "measurement_id")
+    private NgsMeasurementInformation measurement;
+
+    @Column(table = "sample", name = "experiment_id")
+    private String experimentId;
+    @Column(table = "sample", name = "code")
+    private String sampleCode;
+    @Column(table = "sample", name = "label")
+    private String sampleLabel;
+    @Column(name = "comment")
+    private String comment;
+
+    protected NgsSampleInfo() {
+
+    }
+
+    public String sampleCode() {
+      return sampleCode;
+    }
+
+    public String sampleLabel() {
+      return sampleLabel;
+    }
+
+    public String comment() {
+      return comment;
+    }
+
+    String experimentId() {
+      return experimentId;
+    }
+
+    @Override
+    public String toString() {
+      return new StringJoiner(", ", NgsSampleInfo.class.getSimpleName() + "[", "]")
+          .add("sampleId='" + sampleId + "'")
+          .add("measurement=" + measurement.measurementId)
+          .add("sampleCode='" + sampleCode + "'")
+          .add("sampleLabel='" + sampleLabel + "'")
+          .add("comment='" + comment + "'")
+          .toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (!(o instanceof NgsSampleInfo that)) {
+        return false;
+      }
+
+      return Objects.equals(sampleId, that.sampleId) && Objects.equals(measurement,
+          that.measurement) && Objects.equals(sampleCode, that.sampleCode)
+          && Objects.equals(sampleLabel, that.sampleLabel) && Objects.equals(
+          comment, that.comment);
+    }
+
+    @Override
+    public int hashCode() {
+      int result = Objects.hashCode(sampleId);
+      result = 31 * result + Objects.hashCode(measurement);
+      result = 31 * result + Objects.hashCode(sampleCode);
+      result = 31 * result + Objects.hashCode(sampleLabel);
+      result = 31 * result + Objects.hashCode(comment);
+      return result;
+    }
+  }
+
+
+  @Entity(name = "ngs_measurement_metadata")
+  @Table(name = "ngs_measurements")
+  @EntityListeners(PreventAnyUpdateEntityListener.class)
+  final class NgsMeasurementInformation {
+
+
+    @Id
+    @Column(name = "measurement_id")
+    private String measurementId;
+
+    @Column(name = "projectId", nullable = false)
+    private String projectId;
+
+    @Column(name = "measurementCode")
+    private String measurementCode;
+
+    @Column(name = "measurementName")
+    private String measurementName;
+
+    @Column(name = "facility")
+    private String facility;
+
+    @Embedded
+    @AttributeOverride(name = "label", column = @Column(name = "label"))
+    @AttributeOverride(name = "iri", column = @Column(name = "IRI"))
+    private Organisation organisation;
+
+    @Column(name = "instrument")
+    @Convert(converter = InstrumentReadConverter.class)
+    private Instrument instrument;
+
+    @Column(name = "samplePool")
+    private String samplePool;
+
+    @Column(name = "registrationTime")
+    private Instant registeredAt;
+
+    @Column(name = "readType")
+    private String sequencingReadType;
+
+    @Column(name = "libraryKit")
+    private String libraryKit;
+
+    @Column(name = "flowcell")
+    private String flowCell;
+
+    @Column(name = "runProtocol")
+    private String sequencingRunProtocol;
+
+    @OneToMany(mappedBy = "measurement", fetch = FetchType.EAGER)
+    private List<NgsSampleInfo> sampleInfos;
+
+    protected NgsMeasurementInformation() {
+
+    }
+
+    public String measurementId() {
+      return measurementId;
+    }
+
+    public String measurementCode() {
+      return measurementCode;
+    }
+
+    public String measurementName() {
+      return measurementName;
+    }
+
+    public String facility() {
+      return facility;
+    }
+
+    public Organisation organisation() {
+      return organisation;
+    }
+
+    public Instrument instrument() {
+      return instrument;
+    }
+
+    public String samplePool() {
+      return samplePool;
+    }
+
+    public Instant registeredAt() {
+      return registeredAt;
+    }
+
+    public String sequencingReadType() {
+      return sequencingReadType;
+    }
+
+    public String libraryKit() {
+      return libraryKit;
+    }
+
+    public String flowCell() {
+      return flowCell;
+    }
+
+    public String sequencingRunProtocol() {
+      return sequencingRunProtocol;
+    }
+
+    public String experimentId() {
+      return sampleInfos().getFirst().experimentId();
+    }
+
+    public List<NgsSampleInfo> sampleInfos() {
+      return Collections.unmodifiableList(sampleInfos);
+    }
+
+    @Override
+    public String toString() {
+      return new StringJoiner(", ", NgsMeasurementInformation.class.getSimpleName() + "[",
+          "]")
+          .add("experimentID=" + experimentId())
+          .add("measurementId=" + measurementId)
+          .add("projectId='" + projectId + "'")
+          .add("measurementCode='" + measurementCode + "'")
+          .add("measurementName='" + measurementName + "'")
+          .add("facility='" + facility + "'")
+          .add("organisation=" + organisation)
+          .add("instrument=" + instrument)
+          .add("samplePool='" + samplePool + "'")
+          .add("registeredAt=" + registeredAt)
+          .add("sequencingReadType='" + sequencingReadType + "'")
+          .add("libraryKit='" + libraryKit + "'")
+          .add("flowCell='" + flowCell + "'")
+          .add("sequencingRunProtocol='" + sequencingRunProtocol + "'")
+          .add("sampleInfos=" + sampleInfos)
+          .toString();
+    }
+  }
+
+
+}
---
+++ project-management-infrastructure/src/main/java/life/qbic/projectmanagement/infrastructure/experiment/measurement/foobar/PxpMeasurementJpaRepository.java
@@ -0,0 +1,424 @@
+package life.qbic.projectmanagement.infrastructure.experiment.measurement.foobar;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.SecondaryTable;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import jakarta.persistence.criteria.Join;
+import java.io.IOException;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.StringJoiner;
+import life.qbic.projectmanagement.domain.model.measurement.MeasurementId;
+import life.qbic.projectmanagement.infrastructure.PreventAnyUpdateEntityListener;
+import life.qbic.projectmanagement.infrastructure.experiment.measurement.foobar.PxpMeasurementJpaRepository.MsDevice.MsDeviceReadConverter;
+import life.qbic.projectmanagement.infrastructure.experiment.measurement.foobar.PxpMeasurementJpaRepository.PxpMeasurementInformation;
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.data.convert.ReadingConverter;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PxpMeasurementJpaRepository extends
+    PagingAndSortingRepository<PxpMeasurementInformation, MeasurementId>,
+    JpaSpecificationExecutor<PxpMeasurementInformation> {
+
+  final class PxpMeasurementFilter {
+
+    private final String experimentId;
+    private final String searchTerm;
+
+    public PxpMeasurementFilter(@NonNull String experimentId,
+        @NonNull String searchTerm) {
+      this.searchTerm = Objects.requireNonNull(searchTerm);
+      this.experimentId = Objects.requireNonNull(experimentId);
+    }
+
+    public Specification<PxpMeasurementInformation> asSpecification() {
+      return matchesExperiment(experimentId)
+          .and(containsSearchTerm(searchTerm));
+    }
+
+    private static Specification<PxpMeasurementInformation> matchesExperiment(String experimentId) {
+      return (root, query, criteriaBuilder) ->
+      {
+        query.distinct(true);
+        return criteriaBuilder.equal(root.join("sampleInfos").get("experimentId"), experimentId);
+      };
+    }
+
+    private static Specification<PxpMeasurementInformation> containsSearchTerm(String searchTerm) {
+      var cleanedLowerCaseSearchTerm = searchTerm.strip().toLowerCase();
+      if (Objects.isNull(searchTerm) || searchTerm.isEmpty()) {
+        return Specification.unrestricted();
+      }
+      return (root, query, criteriaBuilder) -> {
+        query.distinct(true);
+        var measurementCodeContains = criteriaBuilder.like(
+            criteriaBuilder.lower(root.get("measurementCode")),
+            "%" + cleanedLowerCaseSearchTerm + "%");
+        var measurementNameContains = criteriaBuilder.like(
+            criteriaBuilder.lower(root.get("measurementName")),
+            "%" + cleanedLowerCaseSearchTerm + "%");
+        Join<Object, String> sampleInfos = root.joinList("sampleInfos");
+        var anySampleLabelContains = criteriaBuilder.like(
+            criteriaBuilder.lower(sampleInfos.get("sampleLabel")),
+            "%" + cleanedLowerCaseSearchTerm + "%");
+        var sampleCommentContains = criteriaBuilder.like(
+            criteriaBuilder.lower(sampleInfos.get("comment")),
+            "%" + cleanedLowerCaseSearchTerm + "%");
+        return
+            criteriaBuilder.or(measurementCodeContains,
+                measurementNameContains,
+                anySampleLabelContains,
+                sampleCommentContains);
+      };
+    }
+
+    @Override
+    public String toString() {
+      return new StringJoiner(", ", PxpMeasurementFilter.class.getSimpleName() + "[", "]")
+          .add("experimentId='" + experimentId + "'")
+          .add("searchTerm='" + searchTerm + "'")
+          .toString();
+    }
+  }
+
+  record MsDevice(String label, String oboId, String iri) implements Serializable {
+
+    @JsonComponent
+    static class MsDeviceJsonDeserializer extends JsonDeserializer<MsDevice> {
+
+      @Override
+      public MsDevice deserialize(JsonParser jsonParser, DeserializationContext ctxt)
+          throws IOException, JacksonException {
+        JsonNode tree = jsonParser.readValueAsTree();
+        String oboId = Optional.ofNullable(tree.get("name"))
+            .map(JsonNode::asText) //e.g. EFO_0008633
+            .map(it -> it.replace("_", ":")) //e.g. EFO:0008633
+            .orElseThrow(() -> new JsonParseException("Could not parse msDevice oboId."));
+        String label = Optional.ofNullable(tree.get("label"))
+            .map(JsonNode::asText)
+            .orElseThrow(() -> new JsonParseException("Could not parse msDevice label."));
+        String iri = Optional.ofNullable(tree.get("classIri"))
+            .map(JsonNode::asText)
+            .orElseThrow(() -> new JsonParseException("Could not parse msDevice iri."));
+        return new MsDevice(label, oboId, iri);
+      }
+    }
+
+    @ReadingConverter
+    static class MsDeviceReadConverter implements AttributeConverter<MsDevice, String> {
+
+      private final ObjectMapper objectMapper;
+
+      public MsDeviceReadConverter(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+      }
+
+      @Override
+      public String convertToDatabaseColumn(MsDevice attribute) {
+        return "";
+      }
+
+      @Override
+      public MsDevice convertToEntityAttribute(String dbData) {
+        try {
+          return objectMapper.readValue(dbData, MsDevice.class);
+        } catch (JsonProcessingException e) {
+          throw new RuntimeException(e);
+        }
+      }
+    }
+  }
+
+  record Organisation(String label, String iri) {
+
+  }
+
+  @Entity
+  @Table(name = "specific_measurement_metadata_pxp")
+  @SecondaryTable(name = "sample", pkJoinColumns = @PrimaryKeyJoinColumn(
+      name = "sample_id", referencedColumnName = "sample_id"
+  ))
+  final class PxpSampleInfo {
+
+    @Id
+    @Column(name = "sample_id")
+    private String sampleId;
+    @ManyToOne
+    @JoinColumn(name = "measurement_id")
+    private PxpMeasurementInformation measurement;
+
+    @Column(table = "sample", name = "experiment_id")
+    private String experimentId;
+    @Column(table = "sample", name = "code")
+    private String sampleCode;
+    @Column(table = "sample", name = "label")
+    private String sampleLabel;
+    @Column(name = "comment")
+    private String comment;
+
+    protected PxpSampleInfo() {
+
+    }
+
+    public String sampleCode() {
+      return sampleCode;
+    }
+
+    public String sampleLabel() {
+      return sampleLabel;
+    }
+
+    public String comment() {
+      return comment;
+    }
+
+    String experimentId() {
+      return experimentId;
+    }
+
+    @Override
+    public String toString() {
+      return new StringJoiner(", ", PxpSampleInfo.class.getSimpleName() + "[", "]")
+          .add("sampleId='" + sampleId + "'")
+          .add("measurement=" + measurement.measurementId)
+          .add("sampleCode='" + sampleCode + "'")
+          .add("sampleLabel='" + sampleLabel + "'")
+          .add("comment='" + comment + "'")
+          .toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (!(o instanceof PxpSampleInfo that)) {
+        return false;
+      }
+
+      return Objects.equals(sampleId, that.sampleId) && Objects.equals(measurement,
+          that.measurement) && Objects.equals(sampleCode, that.sampleCode)
+          && Objects.equals(sampleLabel, that.sampleLabel) && Objects.equals(
+          comment, that.comment);
+    }
+
+    @Override
+    public int hashCode() {
+      int result = Objects.hashCode(sampleId);
+      result = 31 * result + Objects.hashCode(measurement);
+      result = 31 * result + Objects.hashCode(sampleCode);
+      result = 31 * result + Objects.hashCode(sampleLabel);
+      result = 31 * result + Objects.hashCode(comment);
+      return result;
+    }
+  }
+
+
+  @Entity(name = "pxp_measurement_metadata")
+  @Table(name = "proteomics_measurement")
+  @EntityListeners(PreventAnyUpdateEntityListener.class)
+  final class PxpMeasurementInformation {
+
+    @Id
+    @Column(name = "measurement_id")
+    private String measurementId;
+
+    @Column(name = "projectId", nullable = false)
+    private String projectId;
+
+    @Column(name = "measurementCode")
+    private String measurementCode;
+
+    @Column(name = "measurementName")
+    private String measurementName;
+
+    @Column(name = "facility")
+    private String facility;
+
+    @Embedded
+    @AttributeOverride(name = "label", column = @Column(name = "label"))
+    @AttributeOverride(name = "iri", column = @Column(name = "IRI"))
+    private Organisation organisation;
+
+    @Column(name = "instrument")
+    @Convert(converter = MsDeviceReadConverter.class)
+    private MsDevice msDevice;
+
+    @Column(name = "samplePool")
+    private String samplePool;
+
+    @Column(name = "registration")
+    private Instant registeredAt;
+
+    @Column(name = "digestionEnzyme")
+    private String digestionEnzyme;
+
+    @Column(name = "digestionMethod")
+    private String digestionMethod;
+
+    @Column(name = "enrichmentMethod")
+    private String enrichmentMethod;
+
+    @Column(name = "injectionVolume")
+    private double injectionVolume;
+
+    @Column(name = "labelType")
+    private String labelType;
+
+    @Column(name = "label", insertable = false, updatable = false)
+    private String label;
+
+    @Column(name = "technicalReplicateName")
+    private String technicalReplicateName;
+
+    @Column(name = "lcmsMethod")
+    private String lcmsMethod;
+
+    @Column(name = "lcColumn")
+    private String lcColumn;
+
+
+    @OneToMany(mappedBy = "measurement", fetch = FetchType.EAGER)
+    private List<PxpSampleInfo> sampleInfos;
+
+    protected PxpMeasurementInformation() {
+
+    }
+
+    public String measurementId() {
+      return measurementId;
+    }
+
+    public String measurementCode() {
+      return measurementCode;
+    }
+
+    public String measurementName() {
+      return measurementName;
+    }
+
+    public String facility() {
+      return facility;
+    }
+
+    public Organisation organisation() {
+      return organisation;
+    }
+
+    public MsDevice msDevice() {
+      return msDevice;
+    }
+
+    @Transient
+    public String getMsDeviceLabel() {
+      return msDevice.label();
+    }
+
+    public String samplePool() {
+      return samplePool;
+    }
+
+    public Instant registeredAt() {
+      return registeredAt;
+    }
+
+    public String digestionEnzyme() {
+      return digestionEnzyme;
+    }
+
+    public String digestionMethod() {
+      return digestionMethod;
+    }
+
+    public String enrichmentMethod() {
+      return enrichmentMethod;
+    }
+
+    public double injectionVolume() {
+      return injectionVolume;
+    }
+
+    public String labelType() {
+      return labelType;
+    }
+
+    public String technicalReplicateName() {
+      return technicalReplicateName;
+    }
+
+    public String lcmsMethod() {
+      return lcmsMethod;
+    }
+
+    public String lcColumn() {
+      return lcColumn;
+    }
+
+    public String label() {
+      return label;
+    }
+
+    public String experimentId() {
+      return sampleInfos().getFirst().experimentId();
+    }
+
+    public List<PxpSampleInfo> sampleInfos() {
+      return Collections.unmodifiableList(sampleInfos);
+    }
+
+    @Override
+    public String toString() {
+      return new StringJoiner(", ", PxpMeasurementInformation.class.getSimpleName() + "[",
+          "]")
+          .add("measurementId='" + measurementId + "'")
+          .add("projectId='" + projectId + "'")
+          .add("measurementCode='" + measurementCode + "'")
+          .add("measurementName='" + measurementName + "'")
+          .add("facility='" + facility + "'")
+          .add("organisation=" + organisation)
+          .add("msDevice=" + msDevice)
+          .add("samplePool='" + samplePool + "'")
+          .add("registeredAt=" + registeredAt)
+          .add("digestionEnzyme='" + digestionEnzyme + "'")
+          .add("digestionMethod='" + digestionMethod + "'")
+          .add("enrichmentMethod='" + enrichmentMethod + "'")
+          .add("injectionVolume=" + injectionVolume)
+          .add("labelType='" + labelType + "'")
+          .add("label='" + label + "'")
+          .add("technicalReplicateName='" + technicalReplicateName + "'")
+          .add("lcmsMethod='" + lcmsMethod + "'")
+          .add("lcColumn='" + lcColumn + "'")
+          .add("sampleInfos=" + sampleInfos)
+          .toString();
+    }
+  }
+
+
+}
