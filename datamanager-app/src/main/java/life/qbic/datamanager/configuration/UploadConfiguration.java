da63b11101739650a7116e374e460820ea7ee98f
KochTobi <kochtobi@users.noreply.github.com>
2026-05-29 17:24:10 +0200

    Fix UploadConfiguration

--- datamanager-app/src/main/java/life/qbic/datamanager/configuration/UploadConfiguration.java
+++ datamanager-app/src/main/java/life/qbic/datamanager/configuration/UploadConfiguration.java
@@ -2,14 +2,16 @@ package life.qbic.datamanager.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
 
 @Configuration
 public class UploadConfiguration {
 
   private final long maxInMemoryBytes;
 
-  public UploadConfiguration(@Value("${qbic.upload.in-memory-limit}") long maxInMemoryBytes) {
-    this.maxInMemoryBytes = maxInMemoryBytes;
+  public UploadConfiguration(
+      @Value("${qbic.upload.in-memory-limit}") DataSize maxInMemoryThreshold) {
+    this.maxInMemoryBytes = maxInMemoryThreshold.toBytes();
   }
 
   public long maxInMemoryBytes() {
