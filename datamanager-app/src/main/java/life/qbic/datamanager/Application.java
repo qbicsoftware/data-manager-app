919b640fa58622da52f622fbb45565a5e956c577
sven1103-agent <261423644+sven1103-agent@users.noreply.github.com>
2026-08-04 11:38:41 +0200

    Provide style sheets

--- datamanager-app/src/main/java/life/qbic/datamanager/Application.java
+++ datamanager-app/src/main/java/life/qbic/datamanager/Application.java
@@ -1,12 +1,14 @@
 package life.qbic.datamanager;
 
 import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.shared.communication.PushMode;
 import com.vaadin.flow.shared.ui.Transport;
 import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 import java.io.Serial;
 import life.qbic.identity.domain.registry.DomainRegistry;
 import life.qbic.identity.domain.repository.UserRepository;
@@ -27,6 +29,8 @@ import org.springframework.context.annotation.ComponentScan;
  * browsers.
  */
 @SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+@StyleSheet(Lumo.STYLESHEET)
+@StyleSheet(Lumo.UTILITY_STYLESHEET)
 @Theme(value = "datamanager")
 @PWA(
     name = "Data Manager",
