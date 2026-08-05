4a6cb0c63357736f5b881becf3708799de281bc7
KochTobi <kochtobi@users.noreply.github.com>
2026-08-05 15:49:22 +0200

    Fix auto open drawer

--- datamanager-app/src/main/java/life/qbic/datamanager/views/DataManagerLayout.java
+++ datamanager-app/src/main/java/life/qbic/datamanager/views/DataManagerLayout.java
@@ -37,7 +37,7 @@ public class DataManagerLayout extends AppLayout implements RouterLayout {
     mainLayout.setId("main-layout");
     setContent(mainLayout);
     // Vaadin 25: Ensure drawer is closed by default when no content is added
-    getElement().setAttribute("drawer-opened", false);
+    setDrawerOpened(false);
   }
 
   /**
