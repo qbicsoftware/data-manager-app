a421711f5c0ccf9ce9ad2cc4c652a83b19bb9047
KochTobi <kochtobi@users.noreply.github.com>
2026-08-06 18:17:54 +0200

    remove commented-out code

--- datamanager-app/src/main/java/life/qbic/datamanager/views/general/oidc/OidcLogo.java
+++ datamanager-app/src/main/java/life/qbic/datamanager/views/general/oidc/OidcLogo.java
@@ -18,22 +18,7 @@ public class OidcLogo extends Image {
 
   private DownloadHandler getLogoResource() {
     String oidcLogoSrc = oidcType.getLogoPath();
-//    //Image source cannot contain a "/" so we look for the actual file name independent in which folder path it is contained.
-//    if (oidcLogoSrc.contains("/")) {
-//      oidcLogoSrc = oidcType.getLogoPath().substring(oidcType.getLogoPath().lastIndexOf('/') + 1);
-//    }
 //    https://docs.oracle.com/javase/8/docs/technotes/guides/lang/resources.html
     return DownloadHandler.forClassResource(getClass(), oidcLogoSrc);
-
-//        event ->
-//            new DownloadResponse(
-//                getClass().getClassLoader().getResourceAsStream(oidcType.getLogoPath()),
-//                "orcid_logo.svg",
-//                "image/svg+xml",
-//                -1L
-//            ));
-
-//            oidcLogoSrc,
-//            () -> getClass().getClassLoader().getResourceAsStream(oidcType.getLogoPath()));
   }
 }
