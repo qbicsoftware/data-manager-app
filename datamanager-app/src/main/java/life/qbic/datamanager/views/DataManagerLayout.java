386bc974b41af5faceb7f0b3b95a1ff285a26d3f
Steffengreiner <steffen.greiner@qbic.uni-tuebingen.de>
2026-09-03 17:11:24 +0200

    Provide Method which persists drawer state within Layouts

--- datamanager-app/src/main/java/life/qbic/datamanager/views/DataManagerLayout.java
+++ datamanager-app/src/main/java/life/qbic/datamanager/views/DataManagerLayout.java
@@ -7,6 +7,7 @@ import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.RouterLayout;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.flow.server.VaadinSession;
 import java.util.Objects;
 import life.qbic.datamanager.announcements.AnnouncementComponent;
 import life.qbic.datamanager.announcements.AnnouncementService;
@@ -24,6 +25,8 @@ public class DataManagerLayout extends AppLayout implements RouterLayout {
 
   private final Div contentArea;
 
+  private static final String DRAWER_STATE_KEY = "drawerOpened";
+
   protected DataManagerLayout(FooterComponentFactory footerComponentFactory,
       AnnouncementService announcementService) {
     Objects.requireNonNull(footerComponentFactory);
@@ -35,6 +38,7 @@ public class DataManagerLayout extends AppLayout implements RouterLayout {
     // Add content area and footer to the main layout
     Div mainLayout = new Div(announcementComponent, contentArea, footerComponentFactory.get());
     mainLayout.setId("main-layout");
+    consistDrawerStateBetweenLayouts();
     setContent(mainLayout);
     // Vaadin 25: Ensure drawer is closed by default when no content is added
     setDrawerOpened(false);
@@ -50,5 +54,22 @@ public class DataManagerLayout extends AppLayout implements RouterLayout {
   public void showRouterLayoutContent(HasElement content) {
     contentArea.removeAll();
     contentArea.getElement().appendChild(content.getElement());
+  }
+
+  /**
+   * While Vaadin drawer states are persistent within the views of an individual layout
+   * but not between Layouts resetting to the default state independent if a drawer was opened previously or not.
+   * This method persists the drawer stages of all layouts which inherit from the {@link DataManagerLayout}
+   * {@link life.qbic.datamanager.views.projects.project.ProjectMainLayout},
+   * {@link life.qbic.datamanager.views.projects.project.experiments.ExperimentMainLayout}
+   */
+  public void consistDrawerStateBetweenLayouts(){
+    Boolean wasOpen = (Boolean) VaadinSession.getCurrent()
+        .getAttribute(DRAWER_STATE_KEY);
+    if (wasOpen != null) {
+      setDrawerOpened(wasOpen);
+    }
+    setDrawerOpened(wasOpen == null || wasOpen); // default true/open
+    getElement().addPropertyChangeListener(DRAWER_STATE_KEY, event -> VaadinSession.getCurrent().setAttribute(DRAWER_STATE_KEY, isDrawerOpened()));
   }
 }
